48d65a8b44ad8b6a8c8559d4440ceba892c6bcbc
Thomas Ochman <thomas@nocebo.se>
2014-01-08 18:24:47 +0100

    New app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebsiteOne::Application.config.secret_key_base = 'a2f102441c72b7998cb9c8e6671df591fbfb12ff848c8ffaedc5b680d0a4e56d4e2d4e643f8bac61f7a391ab0f8bcf84800a2e7b782df1903d16a20aabe8beb9'
