472bdd97f25b779ae9d7ab5fa1b3883379fff43c
Thomas Ochman <thomas@nocebo.se>
2014-01-10 06:12:49 +0100

    modified visitors controller spec

--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe VisitorsController do
-
+    it 'assignes the welcome message' do
+      get :index
+      assigns(:message).should eql 'Hello World'
+      expect(response).to render_template('index')
+    end
 end
