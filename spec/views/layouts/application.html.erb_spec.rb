0a97bfdd26648a13157ffdb0393fc8d0cc350b68
Thomas Ochman <thomas@nocebo.se>
2014-01-10 07:48:20 +0100

    took out test for navbar from the contexts of user sign-in or out

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,34 +1,41 @@
 require 'spec_helper'
 
 describe 'layouts/application.html.erb' do
+  it 'should include jQuery and bootstrap 3.0+ css & js files' do
+    render
+    rendered.should have_xpath("//link[contains(@href, '.css')]")
+    rendered.should have_xpath("//script[contains(@src, '.js')]")
+    #rendered.should have_xpath("//script[contains(@src, 'jquery')]")
+  end
+
+  it 'should render a navbar' do
+    rendered.should have_selector('div.navbar-wrapper')
+  end
+
+  it 'should render links to site features' do
+    render
+    rendered.should have_link 'Our projects', :href => '#'
+  end
+
   context 'not signed in as registred user' do
     before :each do
       view.stub(:user_signed_in?).and_return(false)
     end
-
-    it 'should include jQuery and bootstrap 3.0+ css & js files' do
-      render
-      rendered.should have_xpath("//link[contains(@href, '.css')]")
-      rendered.should have_xpath("//script[contains(@src, '.js')]")
-      #rendered.should have_xpath("//script[contains(@src, 'jquery')]")
-    end
-
     it 'should render a navigation bar with links' do
       render
-        rendered.should have_selector('div.navbar-wrapper')
-        rendered.should have_link 'Check in', :href => new_user_session_path
-        rendered.should have_link 'Sign up', :href => new_user_registration_path
+      rendered.should have_link 'Check in', :href => new_user_session_path
+      rendered.should have_link 'Sign up', :href => new_user_registration_path
     end
   end
+
   context 'user is signed in' do
     before :each do
       view.stub(:user_signed_in?).and_return(true)
     end
     it 'should render a navigation bar with links' do
       render
-      rendered.should have_selector('div.navbar-wrapper')
-      rendered.should_not have_link 'Check in'
-      rendered.should_not have_link 'Sign up'
+      rendered.should_not have_link 'Check in', :href => new_user_session_path
+      rendered.should_not have_link 'Sign up', :href => new_user_registration_path
       rendered.should have_link 'My Account', :href => edit_user_registration_path
     end
   end
