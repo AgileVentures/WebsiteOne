a4724a6da80ce1d528431e054f97ca1357d743cf
Thomas Ochman <thomas@nocebo.se>
2014-01-10 07:25:32 +0100

    layouts/application.html.erb_spec

--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe VisitorsController do
-    it 'assignes the welcome message' do
+    it 'assignes the welcome message and renders index template' do
       get :index
       assigns(:message).should eql 'Hello World'
       expect(response).to render_template('index')
