35c13c33332f59fd99b1596b3259df1344be06da
Bryan Yap <bcollie115@gmail.com>
2014-01-13 18:24:25 +0000

    merged view specs with

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,9 +2,6 @@ require 'spec_helper'
 
 describe ProjectsController do
 
-  let(:valid_attributes) { { "title" => "Title" } }
-
-  let(:valid_session) { {} }
   #TODO split specs into 'logged in' vs 'not logged in'
   before :each do
     user = double('user')
@@ -28,10 +25,10 @@ describe ProjectsController do
 
   describe '#show' do
 
-    #TODO YA refactor with doubles
     it 'assigns the requested project as @project' do
-      project = Project.create! valid_attributes
-      get :show, { :id => project.to_param }, valid_session
+      project = double(Project)
+      Project.stub(:find).and_return(project)
+      get :show, { :id => project.to_param }
       assigns(:project).should eq(project)
     end
   end
@@ -68,7 +65,8 @@ describe ProjectsController do
       #TODO YA add a show view_spec to check if flash is actually displayed
       expect(flash[:notice]).to eql('Project was successfully created.')
     end
-    it 'renders failure message on unsuccessful save' do
+
+    it 'renders new template with failure message on unsuccessful save' do
       @project.stub(:save).and_return(false)
 
       post :create, @params
@@ -77,41 +75,10 @@ describe ProjectsController do
       expect(flash[:alert]).to eql('Project was not saved. Please check the input.')
     end
 
-    describe 'POST create' do
-      describe 'with valid params' do
-        it 'creates a new Project' do
-          expect {
-            post :create, { :project => valid_attributes }, valid_session
-          }.to change(Project, :count).by(1)
-        end
-
-        it 'assigns a newly created project as @project' do
-          post :create, { :project => valid_attributes }, valid_session
-          assigns(:project).should be_a(Project)
-          assigns(:project).should be_persisted
-        end
-
-        it 'redirects to the created project' do
-          post :create, { :project => valid_attributes }, valid_session
-          response.should redirect_to(Project.last)
-        end
-      end
-
-      describe 'with invalid params' do
-        it 'assigns a newly created but unsaved project as @project' do
-          # Trigger the behavior that occurs when invalid params are submitted
-          Project.any_instance.stub(:save).and_return(false)
-          post :create, { :project => { "title" => "invalid value" } }, valid_session
-          assigns(:project).should be_a_new(Project)
-        end
-
-        it 're-renders the new template' do
-          # Trigger the behavior that occurs when invalid params are submitted
-          Project.any_instance.stub(:save).and_return(false)
-          post :create, { :project => { "title" => "invalid value" } }, valid_session
-          response.should render_template("new")
-        end
-      end
+    it 'assigns a newly created project as @project' do
+      @project.stub(:save)
+      post :create, @params
+      expect(assigns(:project)).to eq @project
     end
   end
 
