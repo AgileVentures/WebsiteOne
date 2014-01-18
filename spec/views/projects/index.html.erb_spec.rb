3cf364351469072c3c8e3e382d7ceac72c115435
Thomas Ochman <thomas@nocebo.se>
2014-01-18 06:57:51 +0100

    view spec for project is green after restyling

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'projects/index.html.erb' do
   before :each do
     #TODO Y use factoryGirl
-    Project.create(id: 1, title: "Title 1", description: "Description 1", status: "Status 1")
-    Project.create(id: 2, title: "Title 2", description: "Description 2", status: "Status 2")
-    Project.create(id: 3, title: "Title 3", description: "Description 3", status: "Status 3")
+    @project_one = Project.create(id: 1, title: "Title 1", description: "Description 1", status: "Status 1")
+    @project_two = Project.create(id: 2, title: "Title 2", description: "Description 2", status: "Status 2")
+    @project_three = Project.create(id: 3, title: "Title 3", description: "Description 3", status: "Status 3")
 
     assign(:projects, Project.all)
   end
@@ -18,10 +18,14 @@ describe 'projects/index.html.erb' do
 
       rendered.within('table#projects thead') do |table_row|
         table_row.should have_css('legend', :text => 'List of Projects')
-        table_row.should have_css('th', :text => 'Title')
-        table_row.should have_css('th', :text => 'Description')
-        table_row.should have_css('th', :text => 'Created')
-        table_row.should have_css('th', :text => 'Status')
+      end
+    end
+
+    it 'should display content' do
+      render
+      rendered.within('table#projects tbody') do |table_row|
+        table_row.should have_content 'Created:'
+        table_row.should have_content 'Status:'
       end
     end
 
@@ -34,13 +38,10 @@ describe 'projects/index.html.erb' do
       end
     end
 
-    it 'should render a link Show' do
+    it 'should render a link' do
       render
-      #TODO Y refactor to a smarter traversing
-      i = 0
       rendered.within('table#projects tbody') do |table_row|
-        i += 1
-        expect(table_row).to have_link('Show', href: project_path(i))
+        expect(table_row).to have_link(@project_one.title , href: project_path(@project_one.id))
       end
     end
 
@@ -111,15 +112,11 @@ describe 'projects/index.html.erb' do
 
   describe 'content formatting' do
     it 'renders format in short style' do
-
       render
-      # checking the date of project_id: 1 on row 1, column 4
-      rendered.within('table tr#1 td[4]') do |rendered_date|
+      rendered.within('table tr#1 td[1]') do |rendered_date|
         correct_date = Project.find(1).created_at.strftime("%Y-%m-%d")
-        expect(rendered_date.text).to eq(correct_date)
+        expect(rendered_date.text).to contain(correct_date)
       end
     end
-
   end
-
 end
