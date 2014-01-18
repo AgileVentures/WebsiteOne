6e4060d30c6ee552514924052a5585d574cb6ac0
Paul McCulloch <hrfeels@gmail.com>
2014-01-17 20:30:25 -0800

    specs passing, changed template spec project_document_path

--- spec/views/documents/new.html.erb_spec.rb
+++ spec/views/documents/new.html.erb_spec.rb
@@ -2,21 +2,28 @@ require 'spec_helper'
 
 describe "documents/new" do
   before(:each) do
-    assign(:document, stub_model(Document,
+    @project = stub_model(Project, {:id => 1})
+    @document = assign(:document, stub_model(Document,
+      :id => 1,
       :title => "MyString",
       :body => "MyText",
       :project_id => 1
-    ).as_new_record)
+    ))
   end
 
-  it "renders new document form" do
-    render
+  context 'user is signed in' do
+    before do
+      view.stub(:user_signed_in?).and_return(true)
+    end
+    it "renders new document form" do
+      render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", documents_path, "post" do
-      assert_select "input#document_title[name=?]", "document[title]"
-      assert_select "textarea#document_body[name=?]", "document[body]"
-      assert_select "input#document_project_id[name=?]", "document[project_id]"
+      # Run the generator again with the --webrat flag if you want to use webrat matchers
+      assert_select "form[action=?][method=?]", project_document_path(@project, @document), "post" do
+        assert_select "input#document_title[name=?]", "document[title]"
+        assert_select "textarea#document_body[name=?]", "document[body]"
+        assert_select "input#document_project_id[name=?]", "document[project_id]"
+      end
     end
   end
 end
