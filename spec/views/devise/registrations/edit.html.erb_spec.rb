5a55e1c70af022fbe1ab9023bb649a211e7d3e9c
Yaro Apletov <yaro@mail.ru>
2014-01-19 21:23:55 +0000

    Avatar feature:  in progress (iteration 1)

--- spec/views/devise/registrations/edit.html.erb_spec.rb
+++ spec/views/devise/registrations/edit.html.erb_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
-describe 'users/registrations/edit.html.erb' do
-  it 'fsdfs' do
-    view.should_receive(:resource).at_least(1).times.and_return(User.new)
-    view.should_receive(:resource_name).at_least(1).times.and_return('user')
-    view.should_receive(:devise_mapping).and_return(Devise.mappings[:user])
-
-    render
-  end
+describe 'devise/registrations/edit.html.erb' do
+  before(:each)
+  #stubbing out devise methods
+  view.should_receive(:resource).at_least(1).times.and_return(User.new)
+  view.should_receive(:resource_name).at_least(1).times.and_return('user')
+  view.should_receive(:devise_mapping).and_return(Devise.mappings[:user])
 end
+
+it 'shows required labels' do
+
+  render
+  expect(rendered).to have_text('Title')
+  end
+
+it 'shows required user fields' do
+
+  render
+end
+
+it 'shows avatar image' do
+
+end
+
+it 'shows Update button' do
+
+end
+
+it 'shows Cancel my account button' do
+  rendered.should have_link('Back', :href => projects_path)
+end
+
+it 'shows Back button' do
+  rendered.should have_button('Submit')
+end
+
+
+
+
+
