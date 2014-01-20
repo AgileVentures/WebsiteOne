487fcac9545efaffa90969c4afcbb8e3bb485a56
Yaro Apletov <yaro@mail.ru>
2014-01-20 03:07:44 +0000

    UserManagement: fixed devise to work with new fields of User model  - overrode devise sanitizer in application.rb  - created custom sanitizer in user_sanitizer.rb  - initialised custom sanitizer in initializers/sanitizers.rb
     Followed guide: http://blog.12spokes.com/web-design-development/adding-custom-fields-to-your-devise-user-model-in-rails-4/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  # overriding the devise sanitizer class to allow for custom fields to be permitted for mass assignment
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super
+    end
+  end
 end
