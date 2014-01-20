fd6dd36b65563aa7995fbc608e33f247e0efae0a
Yaro Apletov <yaro@mail.ru>
2014-01-20 00:58:24 +0000

    UserManagement: added redirects to routes to handle malicious paths for user management  - redirects to 404 for requests not handled by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 WebsiteOne::Application.routes.draw do
-
+  root 'visitors#index'
   mount Mercury::Engine => '/'
 
   devise_for :users
-  root 'visitors#index'
+  # devise does not provide some GET routes, which causes routing exceptions
+  get 'users' => redirect('/404.html')
+  get 'users/sign_out' => redirect('/404.html')
+  get 'users/password' => redirect('/404.html')
 
   resources :projects do
     resources :documents do
