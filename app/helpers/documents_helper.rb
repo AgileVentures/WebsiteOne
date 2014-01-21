057cbbef826659f5a62d4f19a85d4c63e00e5b55
Bryan Yap <bcollie115@gmail.com>
2014-01-21 01:28:03 +0000

    982890-63061202 Add Mercury for Document editing Added a few more tags to sanitization filter

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,7 +2,7 @@ module DocumentsHelper
   # TODO Bryan: After being accepted as safe, this should move to the document
   # mercury update to sanitize html before it saves into the database
   def clean_html(html)
-    raw sanitize html, tags: %w(h1 h2 h3 b i pre span iframe div img br),
+    raw sanitize html, tags: %w(h1 h2 h3 h4 h5 h6 b i pre span iframe div img br blockquote p),
                  attributes: %w(src frameborder allowfullscreen style)
   end
 
