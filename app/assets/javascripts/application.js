193afed1d5d672900bf31d52c8b5e9bf82f5ec43
Bryan Yap <bcollie115@gmail.com>
2014-01-22 18:30:36 +0000

    quick fix for javascript animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,80 +18,64 @@
 // Bryan: removed require_tree . because mercury causes problems if loaded on every page
 
 
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
+$(function() {
+    function ready() {
+        var TxtRotate = function(el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+        };
 
-TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+        TxtRotate.prototype.tick = function() {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+            if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-    var that = this;
-    var delta = 300 - Math.random() * 100;
+            var that = this;
+            var delta = 300 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+            if (this.isDeleting) { delta /= 2; }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
+            if (!this.isDeleting && this.txt === fullTxt) {
+                delta = this.period;
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                this.loopNum++;
+                delta = 500;
+            }
 
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
+            setTimeout(function() {
+                that.tick();
+            }, delta);
+        };
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-rotate');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        var elements = document.getElementsByClassName('txt-rotate');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-rotate');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
         }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".txt-rotate > .wrap";
+        document.body.appendChild(css);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap";
-    document.body.appendChild(css);
-};
 
-$(function(){
-    var onClass = "on";
-    var showClass = "show";
-
-    $("input").bind("checkval",function(){
-        var label = $(this).prev("label");
-        if(this.value !== ""){
-            label.addClass(showClass);
-        } else {
-            label.removeClass(showClass);
-        }
-    }).on("keyup",function(){
-            $(this).trigger("checkval");
-        }).on("focus",function(){
-            $(this).prev("label").addClass(onClass);
-        }).on("blur",function(){
-            $(this).prev("label").removeClass(onClass);
-        }).trigger("checkval");
+    $(document).ready(ready);
+    $(document).on('page:load', ready);
 });
-
