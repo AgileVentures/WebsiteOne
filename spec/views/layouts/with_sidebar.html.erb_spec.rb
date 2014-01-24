916be0fe1ebe86055f51c241d45af05b58f3dbab
marcelo <almagro@comcast.net>
2014-01-24 10:20:28 -0800

    wrote test for presence of sidebar

--- spec/views/layouts/with_sidebar.html.erb_spec.rb
+++ spec/views/layouts/with_sidebar.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe 'layouts/application.html.erb' do
-  
+describe 'layouts/with_sidebar.html.erb' do
+  it 'render sidebar' do
+    render
+    rendered.should have_css('nav#sidebar')
+    rendered.should have_css('h3', :text => 'Current Projects')
+  end
 end
