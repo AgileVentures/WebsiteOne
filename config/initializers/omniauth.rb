c2eb3d1684c1d5089877f1f3dece07e970b77d37
Bryan Yap <bcollie115@gmail.com>
2014-01-24 23:57:50 +0000

    override update in registrations controller, fixed routes.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # TODO Bryan: register for a proper key and secret with Sam?
+  # TODO register for a proper authentication key https://github.com/settings/applications
   provider :github, '992af46d7af9fbc6f551', 'fb8967ce60af9f2b2b4ba4bc5e9da8ed31d04d22'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ WebsiteOne::Application.routes.draw do
   root 'visitors#index'
   mount Mercury::Engine => '/'
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => 'registrations'}
   # devise does not provide some GET routes, which causes routing exceptions
   get 'users' => redirect('/404.html')
   get 'users/sign_out' => redirect('/404.html')
   get 'users/password' => redirect('/404.html')
 
   get '/auth/:provider/callback' => 'authentications#create'
-  devise_for :users, :controllers => {:registrations => 'registrations'}
 
   resources :projects do
     resources :documents do
