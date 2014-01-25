58528dbfbf427a026a9129fc10eca5f5bf967bfc
Bryan Yap <bcollie115@gmail.com>
2014-01-25 17:25:36 +0000

    started some controller tests

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe AuthenticationsController do
+  before(:all) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.add_mock(:github, { uid: '123456' })
+    @user = FactoryGirl.create(:user)
+    @auth = @user.authentications.create(provider: 'github', uid: '123456')
+  end
 
+  let(:user) { @user }
+  let(:auth) { @auth }
+
+  context 'using Github authentication' do
+    before(:all) do
+      @request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+    end
+
+    it 'should work' do
+      post :create
+
+      current_user.should eq user
+    end
+  end
 end
