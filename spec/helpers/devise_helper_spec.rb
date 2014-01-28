c4fa069c317bdddb50a493a3fa6509e9a54d7a93
Ismael <ismael@ismael-laptop.(none)>
2014-01-28 14:35:01 +0100

    spec for devise_error_simple_message

--- spec/helpers/devise_helper_spec.rb
+++ spec/helpers/devise_helper_spec.rb
@@ -1,49 +1,53 @@
 require 'spec_helper'
 
+
 describe DeviseHelper do
-  before(:each) do
 
-  end
-  describe 'devise_error_messages_flash' do
+  describe 'when no error' do
 
-    it 'when no error' do
-      acts_well_when_no_error
+    before(:each) do
+      helper.stub_chain(:resource, :errors, :empty?).and_return true
     end
 
-    it 'when there are errors' do
-      messages = ["error 1","error 2" ]
-      sentence = "There are 2 errors"
+    it '#devise_error_messages_flash' do
+      acts_well_when_no_error?('devise_error_messages_flash')
+    end
+
+    it '#devise_error_simple_message' do
+      acts_well_when_no_error?('devise_error_simple_message')
+    end
+
+  end
+
+  describe 'when there are errors' do
+    before(:each) do
+      @messages = ["error 1","error 2" ]
       helper.stub_chain(:resource, :errors, :empty?).and_return false
-      helper.stub_chain(:resource, :errors, :full_messages).and_return messages
-      helper.stub_chain(:resource, :errors, :count).and_return messages.size
+      helper.stub_chain(:resource, :errors, :full_messages).and_return @messages
+    end
+
+    it '#devise_error_messages_flash' do
+      sentence = "There are 2 errors"
+      helper.stub_chain(:resource, :errors, :count).and_return @messages.size
       helper.stub_chain(:resource, :class, :model_name, :human).and_return "devise"
       I18n.should_receive(:t).and_return sentence
       result = helper.devise_error_messages_flash
-      expect(result).to have_css ('.alert.alert-danger')
       expect(result).to have_text (sentence)
-      messages.each { |msg| expect(result).to have_text (msg) }
-    end
-  end
+      expect(result).to have_css ('.alert.alert-danger')
+      @messages.each { |msg| expect(result).to have_text (msg) }
 
-  describe 'devise_error_simple_message' do
-    it 'when no error' do
-      acts_well_when_no_error
     end
-    
-    it 'when there are errors' do
-      messages = ["error 1","error 2" ]
-      helper.stub_chain(:resource, :errors, :empty?).and_return false
-      helper.stub_chain(:resource, :errors, :full_messages).and_return messages
+    it '#devise_error_simple_message' do
       result = helper.devise_error_simple_message
       expect(result).to have_css ('.alert.alert-danger')
-      messages.each { |msg| expect(result).to have_text (msg) }
-    end
+      @messages.each { |msg| expect(result).to have_text (msg) }
 
+    end
   end
+
 end
 
-def acts_well_when_no_error
-  helper.stub_chain(:resource, :errors, :empty?).and_return true
-  result = helper.devise_error_messages_flash
+def acts_well_when_no_error?(method_name)
+  result = helper.send(method_name)
   expect(result).to eq ""
 end
