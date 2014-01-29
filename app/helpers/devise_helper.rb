bf9f7e0f5d4b147b36f90ddc5d6edeb8c7fe1851
Dmitry Sukhikh <china@pozitive.biz>
2014-01-29 15:05:50 +0700

    DeviseHelper was refactored

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,29 +1,29 @@
 module DeviseHelper
-  def devise_error_messages_flash
-    return "" if resource.errors.empty?
-    messages = resource.errors.full_messages.map { |msg| msg + '<br/>' }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
-    html = <<-HTML
-  <div class="alert alert-danger">
-    <strong>#{sentence}</strong>
-    <p>#{messages}</p>
-  </div>
-    HTML
-    html.html_safe
-  end
 
   def devise_error_simple_message
     return "" if resource.errors.empty?
     messages = resource.errors.full_messages.map { |msg| msg + '<br/>' }.join
-    html = <<-HTML
-      <div class="alert alert-danger">
+    html = yield(messages) if block_given?
+    html ||= <<-HTML
+        <div class="alert alert-danger">
         <p>#{messages}</p>
-      </div>
-    HTML
+        </div>
+      HTML
     html.html_safe
   end
 
+  def devise_error_messages_flash
+    devise_error_simple_message do |messages|
+      sentence = I18n.t("errors.messages.not_saved",
+                        :count => resource.errors.count,
+                        :resource => resource.class.model_name.human.downcase)
+      <<-HTML
+        <div class="alert alert-danger">
+        <strong>#{sentence}</strong>
+        <p>#{messages}</p>
+        </div>
+      HTML
+    end
+  end
 
 end
