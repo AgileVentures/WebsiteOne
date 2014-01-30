63d1dbde97b0bfda3e8878d9b518669303d8d965
Thomas Ochman <thomas@nocebo.se>
2014-01-30 20:55:37 +0100

    refactored specs to new attributes in Documents and Projects

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
+
 describe UsersHelper do
   describe '#user_display_name' do
     it 'should return Anon when first name and last name are empty' do
