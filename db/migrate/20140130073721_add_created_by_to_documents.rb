68d4f2601ad2388a9ce7fba38359a8d42905116d
mtc2013 <mtc2013opensource@gmail.com>
2014-01-30 06:30:44 -0500

    Fixed migrations for created by on documents and projects

--- db/migrate/20140130073721_add_created_by_to_documents.rb
+++ db/migrate/20140130073721_add_created_by_to_documents.rb
@@ -1,5 +1,10 @@
 class AddCreatedByToDocuments < ActiveRecord::Migration
   def self.up
-    add_index :documents, :user_id,
+    add_column :documents, :user_id, :integer
+    add_index :documents, :user_id
+  end
+  def self.down
+    remove_index :documents, :user_id
+    remove_column :documents, :user_id
   end
 end
