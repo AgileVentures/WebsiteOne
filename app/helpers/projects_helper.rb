9da7804d540189eae6d024daa2bfbd1407816508
Thomas Ochman <thomas@nocebo.se>
2014-01-30 15:13:44 +0100

    added helper methods fpr created_date and created_by in both projects and documents helpers. Modified views to show created_by

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,20 @@
 module ProjectsHelper
+  def created_by
+    if @project.user_id.present?
+      user = User.find_by_id(@project.user_id)
+      if user.first_name.present?
+        ['by:', ([user.first_name, user.last_name].join(' '))].join(' ')
+      else
+       [ 'by:', (user.email)].join(' ')
+      end
+    else
+      'No author'
+    end
+  end
+
+  def created_date
+    date = @project.created_at.strftime("Created: %Y-%m-%d")
+    return(date)
+  end
+
 end
