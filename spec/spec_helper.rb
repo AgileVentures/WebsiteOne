7a94c63ab7f7e1d2af9c3880e0ec0f9d1c602b9f
Thomas Ochman <thomas@nocebo.se>
2014-02-02 07:32:19 +0100

    added Coverall test api

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
+require 'coveralls'
+Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
