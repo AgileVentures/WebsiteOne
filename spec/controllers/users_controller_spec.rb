6e48f9ab5eb972bcdeeb178b43930df076e05c8c
Pete Boucher <petebouch@gmail.com>
2014-02-04 11:51:04 +0100

    added controller specs and played with columb widths in show view

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,29 @@ require 'spec_helper'
 describe UsersController do
 
   describe "GET 'index'" do
+  	# before :all do
+  	# 	@user = mock_model(:user)
+
+  	# end
+
     it "returns http success" do
       get 'index'
       response.should be_success
     end
+
+    it "assigns all users" do
+    	pending
+    	get 'index'
+    	# assigns(:users).should
+    end
+  end
+
+  describe "GET 'show'" do
+  	it "assigns a user instance" do
+  		pending
+  		get 'show'
+  		assigns(:user).should_not be_nil
+  	end
   end
 
 end
