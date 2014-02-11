559c128d4547dab06d5f3298e84ef831cf7a8a77
Yaro Apletov <yaro@mail.ru>
2014-02-11 11:45:07 +0000

    user videos: view + supporting js + specs

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -13,35 +13,70 @@ describe "users/show.html.erb" do
         {
             url: "http://www.youtube.com/100",
             title: "Random",
-            published: '01/01/2015'.to_date
+            published: '01/02/2015'.to_date
         },
         {
             url: "http://www.youtube.com/340",
             title: "Stuff",
-            published: '01/01/2015'.to_date
+            published: '01/03/2015'.to_date
         },
         {
             url: "http://www.youtube.com/2340",
             title: "Here's something",
-            published: '01/01/2015'.to_date
+            published: '01/04/2015'.to_date
         }
     ]
     assign :youtube_videos, @youtube_videos
 	end
 
+  it 'renders a table wih video links if there are videos' do
+    render
+    expect(render).to have_text('Title', 'Published')
+  end
+
+  it 'renders an embedded player' do
+    expect(render).to have_selector('iframe#ytplayer')
+  end
+
   it 'renders list of youtube links and published dates if user has videos' do
     render
-    @youtube_videos.each do |link|
-      expect(rendered).to have_link(link[:title], :href => link[:url])
+    @youtube_videos.each do |video|
+      expect(rendered).to have_link(video[:title], :href => video[:url])
+      expect(rendered).to have_text(video[:published])
     end
   end
   it 'renders "no available videos" if user has no videos' do
     assign(:youtube_videos, nil)
     render
-    expect(rendered).to have_text("Eric Els has no publicly viewable Youtube videos.")
+    expect(rendered).to have_text('Eric Els has no publicly viewable Youtube videos.')
   end
 
-  it 'renders "connect youtube channel" when user views his profile and it is not yet connected'
+  it 'renders "connect youtube channel" when user views his profile and it is not yet connected' do
+    @user.stub(youtube_id: nil)
+    assign(:youtube_videos, nil)
+    view.stub(current_user: @user)
+
+    render
+    expect(rendered).to have_link('Link your YouTube channel')
+  end
+
+  it 'renders "disconnect youtube channel" when user views his profile and is connected' do
+    @user.stub(youtube_id: 'test')
+    assign(:youtube_videos, nil)
+    view.stub(current_user: @user)
+
+    render
+    expect(rendered).to have_link('Unlink your YouTube channel')
+  end
+
+  it 'does not render "connect youtube channel" when user views other profile' do
+    @user.stub(youtube_id: nil)
+    assign(:youtube_videos, nil)
+    view.stub(current_user: mock_model(User, id: 'test'))
+
+    render
+    expect(rendered).not_to have_text('Link your YouTube channel')
+  end
 
   it 'renders big user avatar' do
     expect(view).to receive(:gravatar_for).with(@user.email ,size: 275).and_return('img_link')
