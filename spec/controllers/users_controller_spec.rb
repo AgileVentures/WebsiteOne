fb04af4c693ed5ca972014253aa12c2385263319
Pete Boucher <petebouch@gmail.com>
2014-02-11 13:37:43 +0100

    completed user_controller specs; all tests passing (apart from that dodgy selenium timeout)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,34 +3,38 @@ require 'spec_helper'
 describe UsersController do
 
   describe "GET 'index'" do
-
     it "returns http success" do
       get 'index'
       response.should be_success
     end
 
     it "assigns all users" do
-    	pending
     	get 'index'
-    	# assigns(:users).should
+    	expect(assigns(:users)).not_to be_nil
     end
   end
 
   describe "GET 'show'" do
     before :each do
-      #@user = double('User')
     	@user = double('User', id: 1,
                      first_name: 'Hermionie',
                      last_name: 'Granger',
                      email: 'hgranger@hogwarts.ac.uk',
                     )
+      @projects = [
+          mock_model(Project, friendly_id: 'title-1', title: 'Title 1'),
+          mock_model(Project, friendly_id: 'title-2', title: 'Title 2'),
+          mock_model(Project, friendly_id: 'title-3', title: 'Title 3')
+      ]
       User.stub(find: @user)
-      @user.stub(folloing_by_type: @projects)
+      @user.stub(:following_by_type).and_return(@projects)
     end
+
     it "assigns a user instance" do
   		get 'show', id: @user.id
   		expect(assigns(:user)).not_to be_nil
     end
+
     it "renders the show view" do
       get 'show', id: @user.id
       expect(response).to render_template :show
@@ -38,16 +42,12 @@ describe UsersController do
 
     context "with followed projects" do
       before :each do
-        @projects = [
-          mock_model(Project, friendly_id: 'title-1', title: 'Title 1'),
-          mock_model(Project, friendly_id: 'title-2', title: 'Title 2'),
-          mock_model(Project, friendly_id: 'title-3', title: 'Title 3')
-        ]
+
       end
 
       it "assigns a list of project being followed" do
         get 'show', id: @user.id
-        expect(assigns(:users_projects).to eq(@fake_projects))
+        expect(assigns(:users_projects)).to eq(@projects)
       end
     end
   end
