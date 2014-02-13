7f1c3474da80fa51f4d43468b951152dde5e22fa
Bryan Yap <bcollie115@gmail.com>
2014-02-13 23:21:54 +0000

    refactoring auth controller spec

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -12,19 +12,23 @@ class AuthenticationsController < ApplicationController
     path = request.env['omniauth.origin'] || root_path
 
     if authentication.present?
-      if current_user.present? and authentication.user != current_user
-        flash[:alert] = 'An account already exists with these credentials!'
+      if user_signed_in? and authentication.user.id != current_user.id
+        # Bryan: TESTED
+        flash[:alert] = 'Another account is already associated with these credentials!'
         redirect_to path
       else
+        # Bryan: TESTED
         flash[:notice] = 'Signed in successfully.'
         sign_in_and_redirect(:user, authentication.user)
       end
     elsif current_user
       auth = current_user.authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
       if auth.save
+        # Bryan: TESTED
         flash[:notice] = 'Authentication successful.'
         redirect_to path
       else
+        # Bryan: TESTED
         flash[:alert] = 'Unable to create additional profiles.'
         redirect_to path
       end
@@ -32,6 +36,7 @@ class AuthenticationsController < ApplicationController
       user = User.new
       user.apply_omniauth(omniauth)
       if user.save
+        # Bryan: TESTED
         flash[:notice] = 'Signed in successfully.'
         sign_in_and_redirect(:user, user)
       else
@@ -71,6 +76,7 @@ class AuthenticationsController < ApplicationController
     @authentication = current_user.authentications.find(params[:id])
     if @authentication
       if current_user.authentications.count == 1 and current_user.encrypted_password.blank?
+        # Bryan: TESTED
         flash[:alert] = 'Bad idea!'
       elsif @authentication.destroy
         flash[:notice] = 'Successfully removed profile.'
--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -1,6 +1,128 @@
 require 'spec_helper'
 
 describe AuthenticationsController do
+
+  before(:each) do
+    OmniAuth.config.mock_auth[:agileventures] = {
+        'provider' => :agileventures,
+        'uid'      => '12345678',
+        'info'     => { 'email' => 'foo@agileventures.org' }
+    }
+    @provider = :agileventures
+  end
+
+  it 'should render a failure message on unsuccessful authentication' do
+    get :failure
+    expect(flash[:alert]).to eq 'Authentication failed.'
+    expect(response).to redirect_to root_path
+  end
+
+  context 'destroying authentications' do
+    before(:each) do
+      @user = double(User, id: 1, friendly_id: 'my-id', encrypted_password: 'i-am-encrypted')
+      request.env['warden'].stub :authenticate! => @user
+      controller.stub :current_user => @user
+
+      @auth = double(Authentication)
+      @auths = [ @auth ]
+      @user.should_receive(:authentications).at_least(1).and_return @auths
+      @auths.should_receive(:find).and_return @auth
+    end
+
+    it 'should be removable for users with a password' do
+      @auths.should_receive(:count).and_return 2
+      @auth.should_receive(:destroy).and_return true
+      get :destroy, id: 1
+      expect(flash[:notice]).to eq 'Successfully removed profile.'
+    end
+
+    it 'should not be allowed for users without any other means of authentication' do
+      @auths.should_receive(:count).and_return 1
+      @user.should_receive(:encrypted_password).and_return nil
+      controller.should_receive(:authenticate_user!).and_return true
+      get :destroy, id: 1
+      expect(flash[:alert]).to eq 'Bad idea!'
+    end
+  end
+
+  before(:each) do
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[@provider]
+    request.env['omniauth.params'] = { }
+  end
+
+  context 'for unregistered users' do
+
+    before(:each) do
+    end
+  end
+
+  context 'for not signed in users' do
+
+    before(:each) do
+      Authentication.should_receive(:find_by_provider_and_uid).and_return nil
+      @user = double(User)
+      @user.stub(:apply_omniauth)
+    end
+
+    it 'should work with valid credentials' do
+      User.should_receive(:new).and_return(@user)
+      @user.should_receive(:save).and_return(true)
+      get :create, provider: @provider
+      expect(flash[:notice]).to eq 'Signed in successfully.'
+    end
+
+    it 'should sign in the correct user' do
+      Authentication.should_receive(:find_by_provider_and_uid).and_return @auth
+      controller.should_receive(:sign_in_and_redirect)
+      @auth.should_receive(:user).at_least(1).and_return @user
+      get :create, provider: @provider
+      expect(flash[:notice]).to eq 'Signed in successfully.'
+    end
+  end
+
+  context 'for signed in users' do
+
+    before(:each) do
+      @user = double(User, id: 1, friendly_id: 'my-id', encrypted_password: 'i-am-encrypted')
+      request.env['warden'].stub :authenticate! => @user
+      controller.stub :current_user => @user
+      @auth = double(Authentication, user: @user)
+      controller.stub :user_signed_in? => true
+    end
+
+    it 'should not allow connecting to a profile when the profile already exists under a different user' do
+      Authentication.stub(:find_by_provider_and_uid).and_return @auth
+      other_user = double(User, id: @user.id + 1)
+      controller.stub :current_user => other_user
+      get :create, provider: @provider
+      expect(flash[:alert]).to eq 'Another account is already associated with these credentials!'
+      expect(response).to redirect_to root_path
+    end
+
+    context 'connecting to a new profile' do
+
+      before(:each) do
+        Authentication.stub(:find_by_provider_and_uid).and_return nil
+        @user.stub_chain(:authentications, :build).and_return @auth
+      end
+
+      it 'should be able to create other profiles' do
+        other_auths = %w( Glitter Smoogle HitPub )
+        other_auths.each do |p|
+          @auth.should_receive(:save).and_return true
+          get :create, provider: p
+          expect(flash[:notice]).to eq 'Authentication successful.'
+        end
+      end
+
+      it 'should not accept multiple profiles from the same source' do
+        @auth.should_receive(:save).and_return false
+        get :create, provider: @provider
+        expect(flash[:alert]).to eq 'Unable to create additional profiles.'
+      end
+    end
+  end
+
   it 'calls #link_to_youtube' do
 
 
