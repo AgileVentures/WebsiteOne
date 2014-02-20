d46183444766ce77d183265adc0ec1d3d103797f
Thomas Ochman <thomas@nocebo.se>
2014-02-20 10:07:56 +0100

    commented out code that will not be used for now in users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -47,14 +47,15 @@ module Youtube
       end
     end
 
-    def project_videos(user)
-      parse_response(open("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=#{user_id}&order=date&q=#{search_term}&type=video&key=#{api_key}").read)
-    end
+    #def project_videos(user)
+    #  parse_response(open("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=#{user_id}&order=date&q=#{search_term}&type=video&key=#{api_key}").read)
+    #end
 
     def parse_response(response, user)
       begin
         json = JSON.parse(response)
         videos = json['feed']['entry']
+        return if videos.nil?
         videos.map do |hash|
           {
               id:         hash['id']['$t'].split('/').last,
