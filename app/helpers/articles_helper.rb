afe3b3fb6931a7b915746d8c3fffb086c6d13145
Bryan Yap <bcollie115@gmail.com>
2014-02-20 14:16:34 +0000

    refactored markdown renderer

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -13,23 +13,28 @@ module ArticlesHelper
     end
   end
 
-  renderer = CodeRayify.new filter_html: true,
-                            hard_wrap: true,
-                            with_toc_data: true,
-                            link_attributes: { target: '_blank', rel: 'nofollow' }
+  renderer_options = {
+      filter_html: true,
+      hard_wrap: true,
+      with_toc_data: true,
+      link_attributes: { target: '_blank', rel: 'nofollow' }
+  }
 
-  @@markdown_engine = Redcarpet::Markdown.new(renderer,
-                                              autolink: true,
-                                              fenced_code_blocks: true,
-                                              no_intra_emphasis: true,
-                                              superscript: true,
-                                              footnotes: true)
+  engine_options = {
+      autolink: true,
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      superscript: true,
+      footnotes: true
+  }
+
+  @@markdown_engine = Redcarpet::Markdown.new(CodeRayify.new(renderer_options), engine_options)
 
   def from_markdown(markdown)
     raw @@markdown_engine.render markdown
   end
 
   def markdown_preview(markdown)
-    raw sanitize(@@markdown_engine.render(markdown), tags: %w( p pre )).truncate(250, separator: ' ')
+    raw sanitize(@@markdown_engine.render(markdown), tags: %w( p pre )).truncate(100, separator: ' ')
   end
 end
