4b802511c9012b704dabfb661f5223852c54c4f5
Bryan Yap <bcollie115@gmail.com>
2014-02-22 16:17:33 +0000

    fix for jasmine:ci task

--- spec/javascripts/hoa_button_spec.js
+++ spec/javascripts/hoa_button_spec.js
@@ -1,24 +1,19 @@
 
-var oldGapi = gapi;
+var gapi = (typeof gapi === "undefined") ? {
+  hangout: {
+    render: function() {
+      // dummy
+    }
+  }
+} : gapi;
 
 describe('HOA button works with turbolinks', function () {
   var placeholder, apiCall;
   beforeEach(function () {
-    gapi = {
-      hangout: {
-        render: function() {
-          // dummy
-        }
-      }
-    };
     setFixtures(sandbox({id: 'HOA-placeholder', 'data-hoa-title': 'HOA-title'}));
     placeholder = $('#HOA-placeholder');
     apiCall = spyOn(gapi.hangout, 'render');
     $(document).trigger('page:load');
-  });
-
-  afterEach(function() {
-    gapi = oldGapi;
   });
 
   it('scrolling causes heights to be calculated', function() {
