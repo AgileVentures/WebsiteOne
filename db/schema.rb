340fff2c5d6c5da1a4f4eb9b9e55d67a0426dc37
Bryan Yap <bcollie115@gmail.com>
2014-03-14 09:50:14 +0000

    dropped, created and migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -56,9 +56,9 @@ ActiveRecord::Schema.define(version: 20140313161712) do
     t.string   "name"
     t.string   "category"
     t.text     "description"
-    t.date     "event_date",                                default: '2014-03-06',          null: false
-    t.time     "start_time",                                default: '2000-01-01 14:55:03', null: false
-    t.time     "end_time",                                  default: '2000-01-01 15:25:03', null: false
+    t.date     "event_date",                                null: false
+    t.time     "start_time",                                null: false
+    t.time     "end_time",                                  null: false
     t.string   "repeats"
     t.integer  "repeats_every_n_weeks"
     t.integer  "repeats_weekly_each_days_of_the_week_mask"
@@ -138,7 +138,6 @@ ActiveRecord::Schema.define(version: 20140313161712) do
     t.string   "city"
     t.string   "region"
     t.string   "youtube_user_name"
-    t.string   "github_profile_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
