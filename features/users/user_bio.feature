3b697280d7ad3cc4cfceab0de5cad1e90c5a5606
Jeff Thomas <subskriptions@gmail.com>
2014-03-19 17:15:45 -0600

    'bio on profile' feature. Ping to Marcelo to refactor no bio present scenario.

--- features/users/user_bio.feature
+++ features/users/user_bio.feature
@@ -9,11 +9,23 @@ Background:
     | Alice      | Jones     | alicejones@hotmail.com  | Lives on a farm with many sheep and goats. |
     | Bob        | Butcher   | bobb112@hotmail.com     |                                            |
 
+@jeff
 Scenario: View user's bio details
   When I visit Alice's profile page
   Then I should see "Bio"
   And I should see "Lives on a farm with many sheep and goats."
 
+@jeff
 Scenario: User does not have a bio.
   When I visit Bob's profile page
   Then I should see "No Bio"
+
+@jeff2 @javascript
+Scenario: Add bio content to profile
+  Given I am logged in
+  And I am on "profile" page for user "me"
+  And I click the "Edit" button
+  And I fill in "Bio" with "Lives in the city"
+  And I click "Update"
+  Then I should see "Lives in the city"
+  
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -15,7 +15,8 @@ describe "users/show.html.erb" do
                              last_name: 'Els',
                              email: 'eric@somemail.se',
                              created_at: thirty_days_ago,
-                             github_profile_url: 'http://github.com/Eric'
+                             github_profile_url: 'http://github.com/Eric',
+                             bio: 'Lonesome Cowboy'
                       )
 		assign :user, @user
     assign :users_projects, @projects
@@ -37,7 +38,7 @@ describe "users/show.html.erb" do
         }
     ]
     assign :youtube_videos, @youtube_videos
-
+    assign :bio, @user.bio
     @skills = ["rails", "ruby", "rspec"]
     assign :skills, @skills
 	end
@@ -114,6 +115,21 @@ end
     render
     expect(rendered).to have_text('Member for: about 1 month')
   end
+  
+  it 'renders a bio' do
+    #@user.stub(bio: 'Lonesome Dove')
+    render
+    expect(renered).to have_text 'Bio'
+    expect(rendered).to have_text 'Lonesome Cowboy'
+  end
+
+  it 'renders "No Bio"' do
+    @user.stub(bio: nil)
+    assign :bio, @user.bio
+    render
+    expect(rendered).not_to have_text 'No Bio'
+    #not_have_css
+  end
 
   it 'displays GitHub profile if it is linked' do
     @user.stub(github_profile_url: nil)
@@ -150,6 +166,4 @@ end
     render
     expect(rendered).to have_css("#skills-show")
   end
-
-  it 'renders user statistics'
 end
