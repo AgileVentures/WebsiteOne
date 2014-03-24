d6823bb6fc131aef83ada7607dd297e25970339d
Sampriti Panda <sampdingo@hotmail.com>
2014-03-24 10:09:51 -0400

    Static Pages Mercury Edit working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,16 @@ class StaticPagesController < ApplicationController
   def show
     @page = StaticPage.friendly.find(params[:id])
   end
+
+  def mercury_update
+    @page = StaticPage.friendly.find(params[:id])
+    if @page.update_attributes(title: params[:content][:static_page_title][:value],
+                                   body: params[:content][:static_page_body][:value])
+      render text: '' # So mercury knows it is successful
+    end
+  end
+
+  def mercury_saved
+    redirect_to static_page_path(params[:id]), notice: 'The page has been successfully updated.'
+  end
 end
