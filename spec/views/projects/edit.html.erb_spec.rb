fd07e338f47eba27f9d76a557b92bd1c17544c68
marcelo <almagro@comcast.net>
2014-03-27 10:26:27 -0700

    pong to marian:spec failing

--- spec/views/projects/edit.html.erb_spec.rb
+++ spec/views/projects/edit.html.erb_spec.rb
@@ -12,8 +12,8 @@ describe 'projects/edit.html.erb' do
 
     expect(rendered).to have_text('Title')
     expect(rendered).to have_text('Description')
-    expect(rendered).to have_text('Github url')
-    expect(rendered).to have_text('Pivotaltracker url')
+    expect(rendered).to have_text('GitHub link')
+    expect(rendered).to have_text('PivotalTracker link')
     expect(rendered).to have_text('Status')
   end
 
@@ -22,8 +22,8 @@ describe 'projects/edit.html.erb' do
 
     expect(rendered).to have_field('Title')
     expect(rendered).to have_field('Description')
-    expect(rendered).to have_field('GitHub link')
-    expect(rendered).to have_field('PivotalTracker link')
+    expect(rendered).to have_field('Github url')
+    expect(rendered).to have_field('Pivotaltracker url')
     expect(rendered).to have_field('Status')
   end
 
