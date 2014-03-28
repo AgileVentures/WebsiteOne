07a51f086258fde8c649ced4cfe21cc8b9677527
Sampriti Panda <sampdingo@hotmail.com>
2014-03-28 10:32:44 -0400

    Tests for StaticPage url for me

--- spec/models/static_page_spec.rb
+++ spec/models/static_page_spec.rb
@@ -24,4 +24,39 @@ describe StaticPage do
       expect(@page).to be_valid
     end
   end
+
+  context 'correct urls for static pages' do
+    context 'with ancestors' do
+      before(:each) do
+        @page_child = StaticPage.create!(valid_attributes_for(:static_page).merge(parent_id: @page.id))
+        @page_url = "#{@page.slug}/#{@page_child.slug}"
+      end
+
+      it 'returns url for static page object' do
+        expect(StaticPage.url_for_me(@page_child)).to eq @page_url
+      end
+      it 'returns url for static page title' do
+        expect(StaticPage.url_for_me(@page_child.title)).to eq @page_url
+      end
+      it 'returns url for static page slug' do
+        expect(StaticPage.url_for_me(@page_child.slug)).to eq @page_url
+      end
+    end
+
+    context 'without ancestors' do
+      it 'returns url for static page object' do
+        expect(StaticPage.url_for_me(@page)).to eq @page.slug
+      end
+      it 'returns url for static page title' do
+        expect(StaticPage.url_for_me(@page.title)).to eq @page.slug
+      end
+      it 'returns url for static page slug' do
+        expect(StaticPage.url_for_me(@page.slug)).to eq @page.slug
+      end
+    end
+
+    it 'returns url for non-existant static page' do
+      expect(StaticPage.url_for_me('does not exist')).to eq 'does-not-exist'
+    end
+  end
 end
