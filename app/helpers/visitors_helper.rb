38e14fd6cc104d51547a2c0be184576cebcfb0f0
Sampriti Panda <sampdingo@hotmail.com>
2014-03-28 11:52:22 -0400

    Fixed -1 hours bug on Events Countdown

--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -1,6 +1,6 @@
 module VisitorsHelper
   def display_countdown(event)
-    time_to_next_event_instance = Time.now.to_datetime.distance_to(event.next_occurrence_time.to_datetime)
+    time_to_next_event_instance = seconds_to_datetime(event.next_occurrence_time.to_i - Time.now.to_datetime.to_i)
     minutes_left = time_to_next_event_instance[:minutes]
     hours_left = time_to_next_event_instance[:hours]
     days_left = time_to_next_event_instance[:days]
@@ -10,5 +10,12 @@ module VisitorsHelper
     else
       "#{days_left} days #{hours_left} hours #{minutes_left} minutes"
     end
+  end
+
+  def seconds_to_datetime(s)
+    mm, ss = s.divmod(60)
+    hh, mm = mm.divmod(60)
+    dd, hh = hh.divmod(24)
+    return {:minutes=>mm, :hours=>hh, :days=>dd}
   end
 end
