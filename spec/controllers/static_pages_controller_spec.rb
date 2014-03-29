5af509d112e09132e43d28831313192063b9594e
Sampriti Panda <sampdingo@hotmail.com>
2014-03-29 10:23:19 -0400

    Controller Specs for ancestry show feature

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -28,5 +28,15 @@ describe StaticPagesController do
     it 'renders the show template' do
       expect(response).to render_template 'show'
     end
+
+    it 'assigns the requested page ancestry as @ancestry' do
+      assigns(:ancestry).should eq([page.title])
+    end
+
+    it 'assigns the requested child page ancestry as @ancestry' do
+      page_child = FactoryGirl.create(:static_page, parent_id: @page.id)
+      get :show, {:id => page_child.to_param}, valid_session
+      assigns(:ancestry).should eq([page.title, page_child.title])
+    end
   end
 end
