971180e8ef85db27924423163b82246062776909
Thomas Ochman <thomas@nocebo.se>
2014-03-30 13:26:35 +0200

    small updates to view spec and method

--- spec/views/visitors/index.html.erb_spec.rb
+++ spec/views/visitors/index.html.erb_spec.rb
@@ -57,7 +57,7 @@ describe 'visitors/index.html.erb' do
       render
       expect(rendered).to have_link @event.name, event_path(@event)
       expect(rendered).to have_text [@event.name, 'in'].join(' ')
-      expect(rendered).to_not have_text '0 days'
+      expect(rendered).to_not have_text '-1 hours'
       expect(rendered).to have_text '23 hours'
       expect(rendered).to have_text '45 minutes'
     end
