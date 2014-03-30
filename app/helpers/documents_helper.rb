7eb2a70ca868412149d2244804934879cc121f7e
Sampriti Panda <sampdingo@hotmail.com>
2014-03-30 06:53:24 -0400

    Readded Deleted Pages

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -3,7 +3,7 @@ module DocumentsHelper
   # mercury update to sanitize html before it saves into the database
   def clean_html(html)
     raw sanitize html, tags: %w(h1 h2 h3 h4 h5 h6 b i ul li pre span iframe div img br blockquote p a),
-                 attributes: %w(src target frameborder allowfullscreen style href)
+                 attributes: %w(id class src target frameborder allowfullscreen style href)
   end
 
   def clean_html_summary(html)
