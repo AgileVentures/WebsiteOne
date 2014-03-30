ce1577ea7c26ccfdabbb2f80b622883e0fac85ae
Sampriti Panda <sampdingo@hotmail.com>
2014-03-30 01:19:14 -0400

    Registration Controller#create unsuccessful save spec.

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -5,7 +5,7 @@ describe RegistrationsController do
     context 'successful save' do
       before(:each) do
         request.env["devise.mapping"] = Devise.mappings[:user]
-        post :create, 'user' => {'email' => 'random@random.com', 'password' => 'randomrandom', 'password_confirmation' => 'randomrandom'}
+        post :create, user: {email: 'random@random.com', password: 'randomrandom', password_confirmation: 'randomrandom'}
       end
 
       it 'redirects to index' do
@@ -27,6 +27,31 @@ describe RegistrationsController do
     end
 
     context 'unsuccessful save' do
+      before(:each) do
+        request.env["devise.mapping"] = Devise.mappings[:user]
+      end
+
+      it 'does not email upon failure to register' do
+        post :create, user: {email: 'random@random.com', password: 'randomrando', password_confirmation: 'randomrandom'}
+        expect(ActionMailer::Base.deliveries.size).to eq 0
+      end
+
+      it 'sets omniauth session to nil' do
+        post :create, user: {email: 'random2@random.com', password: 'randomrando', password_confirmation: 'randomrandom'}
+        session[:omniauth].should eq nil
+      end
+
+      it 'has an active record error message in the user instance variable when registration fails due to email already being in db' do
+        FactoryGirl.create :user
+        post :create, user: {email: 'example@example.com', password: 'randomrandom', password_confirmation: 'randomrandom'}
+        expect(assigns(:user).errors.full_messages).to include "Email has already been taken"
+      end
+
+      # Deal with it later
+      xit 'has an active record error message in the user instance variable when registration fails due to non matching passwords' do
+        post :create, 'user' => {'email' => 'example2@example.com', 'password' => 'pppppppp', 'password_confirmation' => 'aaaaaaaaaa'}
+        expect(assigns(:user).errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
     end
   end
 
