b5d90efb0046ea2f6429857e5cdb8d5be2c9a0fc
Sampriti Panda <sampdingo@hotmail.com>
2014-03-30 12:49:20 -0400

    Projects Helper Spec

--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
-describe ProjectsHelper do
+include ProjectsHelper
 
+describe ProjectsHelper do
+  it 'returns time in correct format' do
+    @project = mock_model(Project, created_at: DateTime.new(2000, 1, 1))
+    created_date.should eq 'Created: 2000-01-01'
+  end
 end
