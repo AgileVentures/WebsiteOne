4fd5416c207e2ca728bf8f71a67344ddbeb43a67
marcelo <almagro@comcast.net>
2014-04-05 13:34:51 -0700

    another switch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ WebsiteOne::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   config.action_dispatch.rack_cache = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
