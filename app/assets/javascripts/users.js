f4e966581c990efc292c9fecfeaa36f9e744f305
Bryan Yap <bcollie115@gmail.com>
2014-04-11 14:30:32 +0100

    add scrollto behaviour when clicking user videos, adjusted css in event show/index and users show page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,9 +4,13 @@ WSO.define('Users', function() {
 
   function selectVideo(event) {
     event.preventDefault();
-    player = $('#ytplayer').get(0);
-    player.src = 'http://www.youtube.com/v/' + this.id + '?version=3&enablejsapi=1';
+    player = $('#ytplayer');
+    player.attr('src', 'http://www.youtube.com/v/' + this.id + '?version=3&enablejsapi=1');
     $('#video_contents').text($(this).data('content'));
+
+    $('html, body').animate({
+      scrollTop: player.offset().top - 100
+    }, 300);
   }
 
   function init() {
