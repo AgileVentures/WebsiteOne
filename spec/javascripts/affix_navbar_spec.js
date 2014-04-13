87162d23110cf816d2db68b1a1b3c9d4682126e1
johnnymo87 <johnnymo87@gmail.com>
2014-04-13 04:48:28 +0100

    finished wso_spec, working on affix_navbar_spec

--- spec/javascripts/affix_navbar_spec.js
+++ spec/javascripts/affix_navbar_spec.js
@@ -1,64 +1,74 @@
-xdescribe('wso/affixed_navbar.js', function () {
-  var affixedNav, header, main, footer, height, scrollTop, script;
-  beforeEach(function () {
-    setFixtures(sandbox({id: 'main_header'}));
-    appendSetFixtures(sandbox({id: 'nav'}));
-    appendSetFixtures(sandbox({id: 'main'}));
-    appendSetFixtures(sandbox({id: 'footer'}));
-    affixedNav = $('#nav');
-    header = $('#main_header');
-    main = $('#main');
-    footer = $('#footer');
-    height = spyOn($.prototype, 'height').and.callFake(function () {
-      return 50
-    });
-    scrollTop = spyOn($.prototype, 'scrollTop');
-//        WSO.define('AffixedNavbar')
-  });
-//    it('', function() {
-//        var win = $(window);
-//        WSO.define('AffixedNavbar');
-//        spyOnEvent(win, 'scroll');
-//        WSO.AffixedNavbar.init();
-//        expect('scroll').toHaveBeenTriggeredOn(win)
-//    });
-
-
-
-  it('scrolling causes heights to be calculated', function() {
-    expect(height).toHaveBeenCalled();
-    $(window).scroll();
-    expect(scrollTop).toHaveBeenCalled();
-  });
-  describe('scrolling down', function () {
+describe('Affixed Navbar', function () {
+    var affixedNav, header, main, footer, height, scrollTop, scroll, hasClass;
     beforeEach(function () {
-      scrollTop.and.callFake(function () {
-        return 150
-      });
-      var parseInt = spyOn(window, 'parseInt').and.callFake(function () {
-        return 5
-      });
-      $(window).scroll();
+        setFixtures(sandbox({id: 'main_header'}));
+        appendSetFixtures(sandbox({id: 'nav'}));
+        appendSetFixtures(sandbox({id: 'main'}));
+        appendSetFixtures(sandbox({id: 'footer'}));
+        affixedNav = $('#nav');
+        header = $('#main_header');
+        main = $('#main');
+        footer = $('#footer');
+        height = spyOn($.prototype, 'height').and.callFake(function () {
+            return 50
+        });
+        scrollTop = spyOn($.prototype, 'scrollTop').and.callThrough();
+        hasClass = spyOn($.prototype, 'hasClass').and.callThrough();
+        scroll = spyOn($.prototype, 'scroll').and.callThrough();
+
+        reloadScript('affix_navbar.js');
+        $(document).trigger('page:load');
     });
-    it('affixes navbar to top', function() {
-      expect(affixedNav).toHaveClass('affix');
+
+    it('should define a new WSO module called "AffixedNavbar"', function() {
+        expect(WSO.AffixedNavbar).toBeDefined();
     });
-    it('pads the margin bottom of the header appropriately', function () {
-      expect(parseInt).toHaveBeenCalled();
-      expect(header).toHaveCss({ 'margin-bottom': '55px' })
+
+    describe('AffixedNavbar.init', function() {
+        it('should measure the height of the header when initialized', function() {
+            expect(height).toHaveBeenCalled();
+        });
+
+        it('should check whether or not the navbar is already affixed', function() {
+            expect(hasClass).toHaveBeenCalledWith('affix')
+        });
     });
-  });
-  describe('scrolling back up', function() {
-    beforeEach(function() {
-      affixedNav.addClass('affix');
-      scrollTop.and.callFake(function() { return 99 });
-      $(window).scroll();
+    it('scrolling causes heights to be calculated', function () {
+//        var onScroll = spyOn(WSO.AffixedNavbar, 'onScroll');
+        $(window).scroll();
+        expect(scrollTop).toHaveBeenCalled();
     });
-    it('un-affixes navbar from top', function() {
-      expect(affixedNav).not.toHaveClass('affix');
+    describe('scrolling down', function () {
+        beforeEach(function () {
+            scrollTop.and.callFake(function () {
+                return 150
+            });
+            var parseInt = spyOn(window, 'parseInt').and.callFake(function () {
+                return 5
+            });
+            $(window).scroll();
+        });
+        it('affixes navbar to top', function () {
+            expect(affixedNav).toHaveClass('affix');
+        });
+        it('pads the margin bottom of the header appropriately', function () {
+            expect(parseInt).toHaveBeenCalled();
+            expect(header).toHaveCss({ 'margin-bottom': '55px' })
+        });
     });
-    it('sets the margin-bottom of the header back to zero', function() {
-      expect(header).toHaveCss({ 'margin-bottom': '0px' })
+    describe('scrolling back up', function () {
+        beforeEach(function () {
+            affixedNav.addClass('affix');
+            scrollTop.and.callFake(function () {
+                return 99
+            });
+            $(window).scroll();
+        });
+        it('un-affixes navbar from top', function () {
+            expect(affixedNav).not.toHaveClass('affix');
+        });
+        it('sets the margin-bottom of the header back to zero', function () {
+            expect(header).toHaveCss({ 'margin-bottom': '0px' })
+        });
     });
-  });
 });
--- spec/javascripts/wso_spec.js
+++ spec/javascripts/wso_spec.js
@@ -1,4 +1,4 @@
-describe('WebsiteOne module', function () {
+xdescribe('WebsiteOne module', function () {
     beforeEach(function () {
         WSO.define('__test__', function () {
             return jasmine.createSpyObj('factory', ['init'])
@@ -6,19 +6,23 @@ describe('WebsiteOne module', function () {
         this.module = WSO.__test__;
     });
 
+    it('should define a new WSO module called "__test__"', function () {
+        expect(this.module).toBeDefined();
+        expect(WSO.__test__).toBeDefined();
+    });
+
     describe('WSO.define', function () {
-        it('should define a new module under the WSO namespace', function () {
-            expect(this.module).toBeDefined();
-        });
         it('should have only one module defined (for testing)', function () {
             expect(WSO._modules.length).toEqual(1);
         });
+
         it('should not create a new module when defined with a repeated name', function () {
             WSO.define('__test__', function () {
                 return { init: function () {} };
             });
             expect(WSO._modules.length).toEqual(1);
         });
+
         it('should not call the init method after redefining a module when _newPageLoaded is false', function () {
             var anotherFactory = jasmine.createSpyObj('another_factory', ['init']);
             WSO._newPageLoaded = false;
@@ -34,12 +38,14 @@ describe('WebsiteOne module', function () {
             beforeEach(function () {
                 anotherFactory = jasmine.createSpyObj('another_factory', ['init']);
             });
+
             it('should not call the init method after defining a module', function () {
                 WSO.define('__another_test__', function () {
                     return anotherFactory;
                 });
                 expect(anotherFactory.init).not.toHaveBeenCalled();
             });
+
             it('should call the init method after defining a module when the _newPageLoaded is false', function () {
                 WSO._newPageLoaded = false;
                 WSO.define('__another_test__', function () {
@@ -60,9 +66,20 @@ describe('WebsiteOne module', function () {
             expect(this.module.init).toHaveBeenCalled();
             expect(anotherFactory.init).toHaveBeenCalled();
         });
+
         it('should be called on the page:load event', function () {
             $(document).trigger('page:load');
             expect(this.module.init).toHaveBeenCalled();
+        });
+    });
+
+    describe('WSO._clear', function () {
+        it('removes all the modules', function() {
+            expect(WSO.__test__).toBeDefined();
+            expect(WSO._modules.length).toEqual(1);
+            WSO._clear();
+            expect(WSO.__test__).not.toBeDefined();
+            expect(WSO._modules.length).toEqual(0)
         });
     });
 });
