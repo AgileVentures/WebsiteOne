e15ef089f0cfb5839cb73d50cc5e31312c593d57
Sampriti Panda <sampdingo@hotmail.com>
2014-04-17 10:46:31 -0400

    Remove trailing whitespaces

--- lib/agile_ventures/bulk_mailer.rb
+++ lib/agile_ventures/bulk_mailer.rb
@@ -2,7 +2,6 @@ require 'pp'
 
 module AgileVentures
   class BulkMailer
-  
     attr_reader :num_sent
     attr_reader :used_addresses
 
@@ -17,7 +16,7 @@ module AgileVentures
     #
     # <b>then call with:</b>
     #
-    # <tt>bulk = AgileVentures::BulkMailer.new(heading: 'my concern', 
+    # <tt>bulk = AgileVentures::BulkMailer.new(heading: 'my concern',
     #                               subject: 'special subject',
     #                               content: content)</tt>
     # <tt>bulk.run</tt>
@@ -40,7 +39,7 @@ module AgileVentures
       @num_sent = 0
       @used_addresses = []
       puts "about to send: #{@heading}\n#{pp(@content)}"
-      opted_in_user_in_batches   
+      opted_in_user_in_batches
       @num_sent
     end
 
@@ -48,13 +47,13 @@ module AgileVentures
     # BulkMailer
     #
     def self.help
-      help = "BulkMailer.new(heading: 'my_heading', 
-                            subject: 'my subject', 
+      help = "BulkMailer.new(heading: 'my_heading',
+                            subject: 'my subject',
                             content: 'multiline text')\n
               opts[:content]::  the content or body of the mailing\n
               opts[:heading]::  a specific headline for the mailing\n
               opts[:batch_size]:: the batch-size in which users are pulled off db\n
-              opts[:subject]::    the subject header of the mailing"    
+              opts[:subject]::    the subject header of the mailing"
       puts help
     end
 
@@ -64,20 +63,20 @@ module AgileVentures
       User.where('receive_mailings = ?', true)
       .find_in_batches(batch_size: @batch_size) do |group|
         group.each do |user|
-          ConsoleMailer.newsletter(user, {  heading: @heading, 
+          ConsoleMailer.newsletter(user, {  heading: @heading,
                                             content: @content,
                                             subject: @subject
                                          }).deliver
           @num_sent += 1
           @used_addresses.push user.email
           puts "#{user.email} - done;"
-        end 
+        end
         waiting_seconds
       end
     end
 
     # to prevent beeing blacklisted/blocked we should wait
-    # a couple seconds 
+    # a couple seconds
     #
     def waiting_seconds
       unless Rails.env == 'test'
@@ -89,6 +88,5 @@ module AgileVentures
         puts "\ncontinuing"
       end
     end
-
   end
 end
