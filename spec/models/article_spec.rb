9609e827bc9f79caf43c25da52c593140c8c7165
Sampriti Panda <sampdingo@hotmail.com>
2014-04-18 12:59:02 -0400

    Added specs for Article#url_for_me

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Article do
 
-  let(:article) { Article.new }
+  let(:article) { Article.new slug: "test-article" }
 
   it 'should respond to tag_list' do
     article.should respond_to :tag_list
@@ -14,5 +14,15 @@ describe Article do
 
   it 'should respond to friendly_id' do
     article.should respond_to :friendly_id
+  end
+
+  describe '#url_for_me' do
+    it 'returns correct url for show action' do
+      article.url_for_me('show').should eq "/articles/#{article.slug}"
+    end
+
+    it 'returns correct url for other actions' do
+      article.url_for_me('new').should eq "/articles/#{article.slug}/new"
+    end
   end
 end
