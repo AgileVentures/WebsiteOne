e881e584a6d750e708a9075ba9e17fa8e4e3bdd3
marcelo <almagro@comcast.net>
2014-05-03 08:34:38 -0700

    sync with upstream. have old code.

--- app/controllers/scrums_controller.rb
+++ app/controllers/scrums_controller.rb
@@ -1,6 +1,10 @@
 class ScrumsController < ApplicationController
   def index
-    @scrums = Scrum.all
+    #@scrums = Scrum.all
+
+    client = YouTubeIt::Client.new(:dev_key => "AIzaSyAh0CZ-jWpREV-3WtQ-4thTW0T-qU6_zrc")
+    query = client.videos_by(:query => "agile ventures scrums", :page => 10)
+    @scrums = query.videos.map(&:video_id).map{ |str| str.scan(/tag:youtube.com,\d+:video:(.+)/).last.first}
   end
 end
 
