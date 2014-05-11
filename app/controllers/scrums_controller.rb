c3ca651e0accc2144e7a4183de1a9c4165b54bff
Sampriti Panda <sampdingo@hotmail.com>
2014-05-11 12:00:13 +0530

    Adding more info to Scrum Videos

--- app/controllers/scrums_controller.rb
+++ app/controllers/scrums_controller.rb
@@ -4,21 +4,19 @@ class ScrumsController < ApplicationController
     #@scrums = Scrum.all
     client = YouTubeIt::Client.new(:dev_key => "AIzaSyAh0CZ-jWpREV-3WtQ-4thTW0T-qU6_zrc")
     query = client.videos_by(:query => "agile ventures scrums")
-    @scrums = query.videos.map(&:video_id).map{ |str| str.scan(/tag:youtube.com,\d+:video:(.+)/).last.first}
+    @scrums = query.videos.map { |video| video_data(video) }
   end
 
   private
 
   def video_data(video)
-
-    # TODO: Work in Progress
     {
         author: video.author.name,
         id: video.video_id.scan(/tag:youtube.com,\d+:video:(.+)/).last.first,
         published: video.published_at.to_date,
         title: video.title,
-        content: hash['content']['$t'],
-        url: video.url
+        content: video.title,
+        url: video.media_content[0].url
     }
   end
 end
--- app/helpers/scrums_helper.rb
+++ app/helpers/scrums_helper.rb
@@ -1,9 +1,9 @@
 module ScrumsHelper
   def scrum_link(video)
-    link_to video, video['url'], id: video, class: 'yt_link', data: { content: video['content']}
+    link_to video[:title], video[:url], id: video[:id], class: 'yt_link', data: { content: video[:content] }
   end
 
   def scrum_embed_link(video)
-    "http://www.youtube.com/embed/#{video}?enablejsapi=1"
+    "http://www.youtube.com/embed/#{video[:id]}?enablejsapi=1"
   end
 end
