3f2b1904b73a0ff9fd39d80a186bdd659edfee38
marianmosley <marian.mosley@gmail.com>
2014-05-15 12:56:43 -0700

    change box color (hardcoded in view) and play icon

--- app/helpers/scrums_helper.rb
+++ app/helpers/scrums_helper.rb
@@ -1,6 +1,6 @@
 module ScrumsHelper
   def scrum_link(video)
-    link_to raw('<i class="icon icon-large icon-play"></i>'), video[:url], id: video[:id], class: 'yt_link', data: { content: video[:content], toggle: "modal", target: "#player" }
+    link_to raw('<a class="btn btn-default btn-danger btn-xs"><span class="glyphicon glyphicon-play"></span></a>'), video[:url], id: video[:id], class: 'yt_link', data: { content: video[:content], toggle: "modal", target: "#player" }
   end
 
   def scrum_embed_link(video)
