b2cafbc83740e6dbfb0608ed18ca7a4178284343
Rafael George <george.rafael@gmail.com>
2014-05-16 21:23:50 -0400

    Add some temp methods instead of temp variables

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,28 +2,33 @@ class AuthenticationsController < ApplicationController
   before_action :authenticate_user!, only: [:destroy]
 
   def create
+    @path = request.env['omniauth.origin'] || root_path
+
     if request.env['omniauth.params']['youtube']
       link_to_youtube and return
     end
 
-    omniauth = request.env['omniauth.auth']
-    authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
-
-    @path = request.env['omniauth.origin'] || root_path
-
     if authentication.present?
-      attempt_login_with_auth(authentication, @path)
+      attempt_login_with_auth(@path)
 
     elsif current_user
-      create_new_authentication_for_current_user(omniauth, @path)
+      create_new_authentication_for_current_user(@path)
 
     else
-      create_new_user_with_authentication(omniauth)
+      create_new_user_with_authentication
     end
 
     if current_user && omniauth['provider']=='github' && current_user.github_profile_url.blank?
       link_github_profile
     end
+  end
+
+  def omniauth 
+    @omniauth ||= request.env['omniauth.auth']
+  end
+
+  def authentication 
+    @authentication ||= Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
   end
 
   def failure
@@ -77,13 +82,18 @@ class AuthenticationsController < ApplicationController
   end
 
   def link_to_youtube
+    update_youtube_id_if_token
+
+    redirect_to(request.env['omniauth.origin'] || root_path)
+  end
+
+  def update_youtube_id_if_token
     user = current_user
+
     if (token = request.env['omniauth.auth']['credentials']['token']) && !user.youtube_id
       user.youtube_id = Youtube.channel_id(token)
       user.save
     end
-
-    redirect_to(request.env['omniauth.origin'] || root_path)
   end
 
   def unlink_from_youtube
@@ -93,8 +103,8 @@ class AuthenticationsController < ApplicationController
     redirect_to(params[:origin] || root_path)
   end
 
-  def attempt_login_with_auth(authentication, path)
-    if current_user.present? and authentication.user != current_user
+  def attempt_login_with_auth(path)
+    if current_user.present? and is_current_user_same_as_auth_user?
       flash[:alert] = 'Another account is already associated with these credentials!'
       redirect_to path
     else
@@ -103,21 +113,27 @@ class AuthenticationsController < ApplicationController
     end
   end
 
-  def create_new_authentication_for_current_user(omniauth, path)
-    if auth = current_user.authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
-      if auth.save
-        # Bryan: TESTED
-        flash[:notice] = 'Authentication successful.'
-        redirect_to path
-      else
-        # Bryan: TESTED
-        flash[:alert] = 'Unable to create additional profiles.'
-        redirect_to @path
-      end
+  def is_current_user_same_as_auth_user?
+    authentication.user != current_user
+  end
+
+  def create_authentication_for_user
+    current_user.authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid']).save
+  end
+
+  def create_new_authentication_for_current_user(path)
+    if create_authentication_for_user
+      # Bryan: TESTED
+      flash[:notice] = 'Authentication successful.'
+      redirect_to path
+    else
+      # Bryan: TESTED
+      flash[:alert] = 'Unable to create additional profiles.'
+      redirect_to @path
     end
   end
 
-  def create_new_user_with_authentication(omniauth)
+  def create_new_user_with_authentication
     user = User.new
     user.apply_omniauth(omniauth)
 
