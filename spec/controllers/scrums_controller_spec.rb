595a0bad7e582c0f9f136999b0a188933e633c36
Marcelo Gobelli <marcelo@Marcelos-MacBook-Pro.local>
2014-05-27 11:06:04 -0700

    adding indexSpec file for scrums

--- spec/controllers/scrums_controller_spec.rb
+++ spec/controllers/scrums_controller_spec.rb
@@ -34,7 +34,7 @@ describe ScrumsController do
       end
 
       context 'the array of videos' do
-        subject { assigns(:scrums)}
+        subject {assigns(:scrums)}
 
         it 'the last video has an older date than the first video'  do
           expect(subject.last[:published]).to be < subject.first[:published]
