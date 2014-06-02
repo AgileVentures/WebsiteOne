1d5e2cba217a4a6ccb4a7583b442f1db19882995
Sampriti Panda <sampdingo@hotmail.com>
2014-06-02 16:34:59 +0530

    Specs for the new paramter sanitizer

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  describe '#configure_permitted_parameters' do
+    it 'should call devise_parameter_sanitizer' do
+      fake_sanitizer = stub(for: Object.new)
+      controller.should_receive(:devise_parameter_sanitizer).and_return(fake_sanitizer)
+      fake_sanitizer.should_receive(:for).with(:account_update)
+      controller.send(:configure_permitted_parameters)
+    end
+  end
 end
