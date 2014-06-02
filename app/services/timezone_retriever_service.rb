99c93e628d1ce3e5f2c90abd3454c4f3e0d05d11
johnnymo87 <johnnymo87@gmail.com>
2014-06-02 17:55:37 +0100

    simplified service, used spec_helper to get vcr working in spec

--- app/services/timezone_retriever_service.rb
+++ app/services/timezone_retriever_service.rb
@@ -3,59 +3,41 @@ require 'google_timezone'
 
 class TimezoneRetrieverService
   def self.for(user)
-    new(user).timezone
+    coordinates = Coordinates.new(user)
+    TimezoneData.new(GoogleTimezone.fetch(coordinates.latitude,
+                         coordinates.longitude))
   end
-
-  def initialize(user)
-    @user = user
-    @coordinates = Coordinates.new(
-      Geocoder.search(LastSignInIp.for(user)).first.data)
-  end
-
-  def timezone
-    Timezone.new(GoogleTimezone.fetch(@coordinates.latitude,
-                         @coordinates.longitude))
-  end
-
-  class Timezone
-    attr_reader :name, :offset
-    def initialize(timezone_data)
-      @name = timezone_data.time_zone_name
-      @offset = timezone_data.raw_offset / 3600
-    end
-
-    def offset 
-      if @offset > 0 
-        "UTC+#{@offset}"
-      else
-        "UTC#{@offset}"
-      end
-    end
-  end
-
-  class LastSignInIp
-    def self.for(user)
-      new(user.last_sign_in_ip || '0.0.0.0').ip
-    end
-
-    attr_reader :ip
-    def initialize(ip)
-      @ip = ip
-    end
-  end
-
-
 
   class Coordinates
     attr_reader :latitude, :longitude
-    def initialize(data)
-      @latitude = data.fetch('latitude')
-      @longitude = data.fetch('longitude')
+    def initialize(user)
+      @latitude = user.latitude || 0
+      @longitude = user.longitude || 0
       freeze
     end
   end
 
-  private
+  class TimezoneData
+    attr_reader :name, :offset
+    def initialize(timezone_data)
+      @timezone_data = timezone_data
+      @name = timezone_data.time_zone_name
+      @offset = timezone_data.raw_offset / 3600
+    end
 
-  attr_reader :user
+    def success?
+      @timezone_data.success?
+    end
+
+    def offset
+      if @offset > 0
+        "UTC+#{@offset}"
+      elsif @offset < 0
+        "UTC#{@offset}"
+      else
+        "UTC"
+      end
+    end
+  end
+
 end
--- spec/services/timezone_retriever_service_spec.rb
+++ spec/services/timezone_retriever_service_spec.rb
@@ -1,35 +1,50 @@
-require_relative '../../app/services/timezone_retriever_service'
-require 'active_support/all'
+#require_relative '../../app/services/timezone_retriever_service'
+#require 'active_support/all'
+#ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
 
-Geocoder.configure(:ip_lookup => :test)
-Geocoder::Lookup::Test.add_stub(
-    '127.0.0.1', [
-    {
-        ip: '127.0.0.1',
-        country_code: 'SE',
-        country_name: 'Sweden',
-        region_code: '28',
-        region_name: 'Västra Götaland',
-        city: 'Alingsås',
-        zipcode: '44139',
-        latitude: 57.9333,
-        longitude: 12.5167,
-        metro_code: '',
-        areacode: ''
-    }.as_json
-]
-)
 describe TimezoneRetrieverService do
-  let(:user) { double(:user,  last_sign_in_ip: '127.0.0.1') }
+  context 'when the user has coords (happy path)' do
+    let(:user) { double(:user, latitude: 57.9333, longitude: 12.5167) }
 
-  context '.for' do
-    it 'returns the timezone name for a given user' do
-      expect(described_class.for(user).name).to eql 'Central European Summer Time'
+    east = {cassette_name: 'timezones/query_east'}
+    context 'query East of UTC', vcr: east do
+      it 'returns the timezone name for a given user' do
+        expect(described_class.for(user).success?).to be_true
+      end
+
+      it 'returns the timezone name for a given user' do
+        expect(described_class.for(user).name).to eql 'Central European Summer Time'
+      end
+
+      it 'returns utc offset in hours for a given user' do
+        expect(described_class.for(user).offset).to eql 'UTC+1'
+      end
     end
 
-    it 'returns utc offset in hours for a given user' do
-      expect(described_class.for(user).offset).to eql 'UTC+1'
+    west = {cassette_name: 'timezones/query_west'}
+    context 'when the user has coords West of UTC (happy path)', vcr: west do
+      it 'negative offsets print correctly' do
+        user.stub latitude: 40.7127, longitude: -74.0059
+        expect(described_class.for(user).offset).to eql 'UTC-5'
+      end
     end
   end
 
+  no_coords = {cassette_name: 'timezones/query_no_coords'}
+  context 'when the user coords are unknown (sad path)', vcr: no_coords do
+    let(:user) { double(:user, latitude: 0, longitude: 0) }
+
+    it 'returns the timezone name for a given user' do
+      expect(described_class.for(user).success?).to be_false
+    end
+
+    it 'returns the timezone name for a given user' do
+      expect(described_class.for(user).name).to eql ''
+    end
+
+    it 'returns utc offset in hours for a given user' do
+      expect(described_class.for(user).offset).to eql 'UTC'
+    end
+  end
 end
