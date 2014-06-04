80a3fa7829b04df0c2756cbf53f742a29d804dd1
Marcelo Gobelli <marcelo@Marcelos-MacBook-Pro.local>
2014-06-04 11:51:36 -0700

    changes from sampriti. pong to marian

--- features/step_definitions/scrums_steps.rb
+++ features/step_definitions/scrums_steps.rb
@@ -17,11 +17,13 @@ Given(/^I play a video$/) do
 end
 
 Then(/^I should see a modal window with title "(.*?)"$/) do |header|
-  #debugger
-  #expect(page).to have_xpath("//div[@class='modal-header']"
-  expect(page.find("//div[@aria-hidden='true']"), visible: false).to be_true
+  page.evaluate_script("$('.modal').css('display')").should eq "block"
+  page.should have_selector('#playerTitle', text: header)
 end
 
+Then(/^I should not see a modal window$/) do
+  page.evaluate_script("$('.modal').css('display')").should eq "none"
+end
 
 When(/^I close the video window$/) do
     pending # express the regexp above with the code you wish you had
