8886aef46670f497170f209dfc08c33662d9fc5c
decareano <marcelo@Marcelos-MacBook-Pro.local>
2014-06-10 13:10:30 -0700

    testing the real connection

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'test'
   VCR.configure do |c|
-    c.ignore_localhost = true
+    c.ignore_localhost = false
     c.default_cassette_options = {record: :new_episodes}
     c.allow_http_connections_when_no_cassette = true
     c.configure_rspec_metadata! unless ENV['CUCUMBER']
