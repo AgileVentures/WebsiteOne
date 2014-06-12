4f781266ad36281fbfcdb20ce2062cbd9f865616
marianmosley <marian.mosley@gmail.com>
2014-06-12 12:16:45 -0700

    do not allow http requests

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -2,7 +2,7 @@ if Rails.env == 'test'
   VCR.configure do |c|
     c.ignore_localhost = true
     c.default_cassette_options = {record: :none} #:new_episodes}
-    c.allow_http_connections_when_no_cassette = true
+#    c.allow_http_connections_when_no_cassette = true
     c.configure_rspec_metadata! unless ENV['CUCUMBER']
     c.cassette_library_dir = Rails.root.join('spec', 'fixtures', 'cassettes')
     c.hook_into :webmock
