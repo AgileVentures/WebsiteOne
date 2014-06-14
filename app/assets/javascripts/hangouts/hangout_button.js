7ff9aa18449685b3b8c9312462b23dfa110d03bd
Yaro Apletov <yaro@mail.ru>
2014-06-15 00:41:09 +0100

    refactored button loading javascript

--- app/assets/javascripts/hangouts/hangout_button.js
+++ app/assets/javascripts/hangouts/hangout_button.js
@@ -1,22 +1,29 @@
-$.ajax({
-  url: 'https://apis.google.com/js/platform.js',
-  dataType: "script",
-  cache: true
-}).done(function () {
-  var placeholder = 'liveHOA-placeholder',
-  hoa_placeholder = $('#' + placeholder),
-  topic = hoa_placeholder.data('topic'),
-  app_id = hoa_placeholder.data('app-id'),
-  callback_url = hoa_placeholder.data('callback-url');
+WebsiteOne.load_hangout_button = function load() {
 
-  var status = gapi.hangout.render(placeholder, {
-    'render': 'createhangout',
-    'topic': topic,
-    'hangout_type': 'onair',
-    'initial_apps': [{'app_id' : app_id,
-      'start_data' :callback_url,
-      'app_type' : 'ROOM_APP'
-    }],
-    'widget_size': 200
-  });
-});
+  var placeholder = 'liveHOA-placeholder',
+      hoa_placeholder = $('#' + placeholder);
+
+  if (typeof(gapi) != 'undefined' && hoa_placeholder.length >0) {
+    var topic = hoa_placeholder.data('topic'),
+        app_id = hoa_placeholder.data('app-id'),
+        callback_url = hoa_placeholder.data('callback-url');
+
+    var status = gapi.hangout.render(placeholder, {
+      'render': 'createhangout',
+      'topic': topic,
+      'hangout_type': 'onair',
+      'initial_apps': [{'app_id' : app_id,
+        'start_data' :callback_url,
+        'app_type' : 'ROOM_APP'
+      }],
+      'widget_size': 200
+    });
+  };
+};
+
+if (typeof(gapi) == 'undefined') {
+  $.ajax({
+    url: 'https://apis.google.com/js/platform.js',
+    dataType: "script",
+    cache: true }).done(WebsiteOne.load_hangout_button);
+};
