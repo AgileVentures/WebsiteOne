9abdbc2770c8e7c9ef8044257841ea30706bbf14
Sampriti Panda <sampdingo@hotmail.com>
2014-06-15 21:38:29 +0530

    Test for Article#url_for_me

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -13,7 +13,7 @@ describe Document do
       @document.title = ''
       expect(@document.save).to be_false
     end
-    
+
     it 'blank project' do
       @document.project_id = nil
       expect(@document.save).to be_false
