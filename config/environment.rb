18f2a2406a1a4951a290f9302de3800122525ffd
Sampriti Panda <sampdingo@hotmail.com>
2014-06-15 21:07:03 +0530

    Moving datetime extension from config/ to a core extension

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require File.expand_path('../datetime_extension', __FILE__)
 require File.expand_path('../nested_key_extension', __FILE__)
 require File.expand_path('../uri_validator_extension', __FILE__)
 # Initialize the Rails application.
