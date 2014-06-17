9d0bdd2afd99cc20a9568876e104413308d6982d
Sampriti Panda <sampdingo@hotmail.com>
2014-06-18 00:05:33 +0530

    Reindent

--- app/helpers/youtube_helper.rb
+++ app/helpers/youtube_helper.rb
@@ -1,39 +1,39 @@
 require 'open-uri'
 
 module YoutubeHelper
-    def channel_id(token)
-      # API v3
-      response = open("https://www.googleapis.com/youtube/v3/channels?part=id&mine=true", 'Authorization' => "Bearer #{token}").read
-      json = JSON.load(response)
-      json['items'].first['id']
-    end
+  def channel_id(token)
+    # API v3
+    response = open("https://www.googleapis.com/youtube/v3/channels?part=id&mine=true", 'Authorization' => "Bearer #{token}").read
+    json = JSON.load(response)
+    json['items'].first['id']
+  end
 
-    #TODO YA add fallback is user_id not found
-    def user_id(token)
-      # API v2
-      response = open("https://gdata.youtube.com/feeds/api/users/default?alt=json", 'Authorization' => "Bearer #{token}").read
-      json = JSON.load(response)
-      json['entry']['yt$username']['$t']
-    end
+  #TODO YA add fallback is user_id not found
+  def user_id(token)
+    # API v2
+    response = open("https://gdata.youtube.com/feeds/api/users/default?alt=json", 'Authorization' => "Bearer #{token}").read
+    json = JSON.load(response)
+    json['entry']['yt$username']['$t']
+  end
 
-    def user_name(user)
-      # API v2
-      return unless user.youtube_id
-      response = open("https://gdata.youtube.com/feeds/api/users/#{user.youtube_id}?fields=title&alt=json").read
-      json = JSON.load(response)
-      json['entry']['title']['$t']
-    end
+  def user_name(user)
+    # API v2
+    return unless user.youtube_id
+    response = open("https://gdata.youtube.com/feeds/api/users/#{user.youtube_id}?fields=title&alt=json").read
+    json = JSON.load(response)
+    json['entry']['title']['$t']
+  end
 
-    #TODO YA add this to youtube connection
-    def youtube_user_name(user)
-      unless user.youtube_user_name
-        begin
-          user.youtube_user_name = user_name(user)
-          #user.save  # this is breaking everything sometimes - NEED BETTER FIX
-        rescue Exception => e
-          logger.fatal e
-        end
+  #TODO YA add this to youtube connection
+  def youtube_user_name(user)
+    unless user.youtube_user_name
+      begin
+        user.youtube_user_name = user_name(user)
+        #user.save  # this is breaking everything sometimes - NEED BETTER FIX
+      rescue Exception => e
+        logger.fatal e
       end
-      user.youtube_user_name
     end
+    user.youtube_user_name
+  end
 end
