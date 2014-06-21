46b8ef1aaf27f2ee28bb72893d654965708a3f30
Bryan Yap <bcollie115@gmail.com>
2014-06-21 21:13:27 +0100

    Removed pagination from users index page
    - because Sam didn't like it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -227,7 +227,7 @@ describe User do
 
   describe '.search' do
     subject { User.search(params) }
-    let(:params) { { per_page: 1 } }
+    let(:params) { Hash.new }
 
     before(:each) do
       FactoryGirl.create(:user, first_name: 'Bob', created_at: 5.days.ago)
@@ -235,16 +235,11 @@ describe User do
       FactoryGirl.create(:user, first_name: 'Janice', display_profile: false)
     end
 
-    it 'should return paginated results' do
-      expect(subject.size).to eq(1)
-    end
-
     it 'should be ordered by creation date' do
       expect(subject.first.first_name).to eq('Bob')
     end
 
     it 'should be filtered by the display_profile property' do
-      params.merge!(per_page: 100)
       results = subject.map(&:first_name)
       expect(results).to include('Marley')
       expect(results).not_to include('Janice')
