5493f034f8d74386a7ccb7d3c2cd43df6ea71337
Sampriti Panda <sampdingo@hotmail.com>
2014-06-21 13:50:51 +0530

    Adding in new devise parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,21 @@ class ApplicationController < ActionController::Base
   helper_method :static_page_path
 
   before_filter :get_next_event
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include ApplicationHelper
   include CustomErrors
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :bio, :password,
+               :password_confirmation, :current_password,
+               :display_email, :display_profile, :display_hire_me,
+               :receive_mailings)
+    end
+  end
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
