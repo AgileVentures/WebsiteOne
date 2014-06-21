5287904d54f6ad8691b21f8798daa9d90703bb5c
Sampriti Panda <sampdingo@hotmail.com>
2014-06-21 16:55:36 +0530

    Fixing Tests after change to YoutubeHelper

--- spec/helpers/youtube_helper_spec.rb
+++ spec/helpers/youtube_helper_spec.rb
@@ -10,22 +10,13 @@ describe YoutubeHelper do
     expect(helper.channel_id('token')).to eq('id')
   end
 
-  it 'retrieves youtube user_id for logged in user' do
-    request_string = "https://gdata.youtube.com/feeds/api/users/default?alt=json"
-    WebMock.stub_request(:get, request_string).to_return(body: 'response')
-    json = { 'entry' => { 'yt$username' => { '$t' => 'id' } } }
-
-    expect(JSON).to receive(:load).with('response').and_return(json)
-    expect(helper.user_id('token')).to eq('id')
-  end
-
-  it 'retrieves youtube user_name for a user' do
+ it 'retrieves youtube user_name for a user' do
     user = double(User, youtube_id: 'test_id')
     request_string = "https://gdata.youtube.com/feeds/api/users/test_id?fields=title&alt=json"
     WebMock.stub_request(:get, request_string).to_return(body: 'response')
     json = { 'entry' => { 'title' => { '$t' => 'Ivan Petrov' } } }
 
     expect(JSON).to receive(:load).with('response').and_return(json)
-    expect(helper.user_name(user)).to eq('Ivan Petrov')
+    expect(helper.youtube_user_name(user)).to eq('Ivan Petrov')
   end
 end
