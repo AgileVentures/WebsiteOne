8f8e064282583f956ea1531af4621892b389a195
Yaro Apletov <yaro@mail.ru>
2014-06-26 19:50:41 +0100

    added sso for disqus

--- app/helpers/disqus_helper.rb
+++ app/helpers/disqus_helper.rb
@@ -1,28 +1,25 @@
-# require 'rubygems'
-# require 'base64'
-# require 'cgi'
-# require 'openssl'
-# require "json"
-
 module DisqusHelper
   def get_disqus_sso(user)
-    data =  {
-      id: user.id,
-      username: user.display_name,
-      email: user.email,
-      #'avatar' => user['avatar'],
-      #'url' => user['url']
-    }.to_json
+    data = {}
+    present user do |presenter|
+      data =  {
+        id: user.id,
+        username: user.display_name,
+        email: user.email,
+        avatar: presenter.gravatar_src,
+        url: user_url(user)
+      }.to_json
+    end
 
     message  = Base64.encode64(data).gsub("\n", "")
     timestamp = Time.now.to_i
-    sig = OpenSSL::HMAC.hexdigest('sha1', Disqus::DISQUS_SECRET_KEY, '%s %s' % [message, timestamp])
+    sig = OpenSSL::HMAC.hexdigest('sha1', Settings.disqus.secret_key, '%s %s' % [message, timestamp])
 
-    return "<script type=\"text/javascript\">
-            var disqus_config = function() {
-            this.page.remote_auth_s3 = \"#{message} #{sig} #{timestamp}\";
-            this.page.api_key = \"#{Disqus::DISQUS_API_KEY}\";
-            }
-          </script>"
+    return %Q(<script type="text/javascript">
+              var disqus_config = function() {
+              this.page.remote_auth_s3 = '#{message} #{sig} #{timestamp}';
+              this.page.api_key = '#{Settings.disqus.api_key}';
+              }
+            </script>)
   end
 end
