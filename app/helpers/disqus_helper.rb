80e0a6ab86edf0bebe94b1607daa1cf4bfb1bae8
Yaro Apletov <yaro@mail.ru>
2014-06-26 20:30:09 +0100

    addeed sso specs for view and helper

--- app/helpers/disqus_helper.rb
+++ app/helpers/disqus_helper.rb
@@ -15,7 +15,7 @@ module DisqusHelper
     timestamp = Time.now.to_i
     sig = OpenSSL::HMAC.hexdigest('sha1', Settings.disqus.secret_key, '%s %s' % [message, timestamp])
 
-    return %Q(<script type="text/javascript">
+    return %Q(<script id="disqus-sso" type="text/javascript">
               var disqus_config = function() {
               this.page.remote_auth_s3 = '#{message} #{sig} #{timestamp}';
               this.page.api_key = '#{Settings.disqus.api_key}';
--- spec/support/shared_examples/shared_example_for_disqus.rb
+++ spec/support/shared_examples/shared_example_for_disqus.rb
@@ -1,16 +1,26 @@
 shared_examples 'commentable with Disqus' do
   let(:data_tags) { 
     data_tags = {
-      'data-disqus-shortname' => Disqus::DISQUS_SHORTNAME,
+      'data-disqus-shortname' => Settings.disqus.shortname,
       'data-disqus-identifier' => entity.friendly_id,
       'data-disqus-title' => entity.title,
       'data-disqus-url' => 'test.com'
     }
   }
 
+  before :each do
+    allow(controller.request).to receive(:url).and_return('test.com')
+  end
+
   it 'renders Disqus_thread with parameters for Document' do
-    controller.request.stub(url: 'test.com')
     render
     expect(rendered).to have_selector('#disqus_thread', data_tags)
+  end
+
+  it 'embeds single-sign-on script if user is logged in' do
+    allow(view).to receive(:current_user).and_return(stub_model(User))
+    allow(view).to receive(:user_signed_in?).and_return(true)
+    render
+    expect(rendered).to have_selector('script#disqus-sso')
   end
 end
