4cbb75fdf1f64bc19477181e2c948aa42eafb29d
Yaro Apletov <yaro@mail.ru>
2014-06-26 23:27:03 +0100

    code review refactor

--- app/helpers/disqus_helper.rb
+++ app/helpers/disqus_helper.rb
@@ -11,15 +11,15 @@ module DisqusHelper
       }.to_json
     end
 
-    message  = Base64.encode64(data).gsub("\n", "")
+    message  = Base64.strict_encode64(data)
     timestamp = Time.now.to_i
-    sig = OpenSSL::HMAC.hexdigest('sha1', Settings.disqus.secret_key, '%s %s' % [message, timestamp])
+    sig = OpenSSL::HMAC.hexdigest('sha1', Settings.disqus.secret_key, "#{message} #{timestamp}")
 
     return %Q(<script id="disqus-sso" type="text/javascript">
               var disqus_config = function() {
               this.page.remote_auth_s3 = '#{message} #{sig} #{timestamp}';
               this.page.api_key = '#{Settings.disqus.api_key}';
               }
-            </script>)
+            </script>).html_safe
   end
 end
