e18dbdeb22a30d017f98675131c8b67fe78cae38
Yaro Apletov <yaro@mail.ru>
2014-06-26 19:29:08 +0100

    added settings for Disqus

--- config/initializers/disqus.rb
+++
@@ -1,9 +0,0 @@
-module Disqus
-  if Rails.env.development? || Rails.env.test?
-    DISQUS_SHORTNAME  = 'agileventures-dev'
-    DISQUS_API_KEY = 'Mh9WLi09Ve752WaTu3xbfEpSXDHOluVNc2Cm8UnDVA5TTuKXUebEVq56cDlu0SBu'
-    DISQUS_SECRET_KEY = 'bRtQeANt2qJQ9Y6CJZtA2I2g6dioHKDuPaX5US17JmRXmvMYcG69wZXeKaFmd8qJ'
-  else
-    DISQUS_SHORTNAME  = 'agileventures'
-  end
-end
