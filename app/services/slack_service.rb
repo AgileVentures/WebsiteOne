12166d912d7966ea9cb0b3dc9e33282ed9925a2a
Yaro Apletov <yaro@mail.ru>
2014-06-26 09:16:20 +0100

    added Slack_notifications feature to Settings

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -2,6 +2,8 @@ module SlackService
   extend self
 
   def post_hangout_notification(hangout)
+    return if Features.slack_notifications.enabled == false
+
     uri = URI.parse "#{Slack::BOT_URL}/hubot/hangouts-notify"
     Net::HTTP.post_form uri, {
       title: hangout.event.name,
--- spec/services/slack_service_spec.rb
+++ spec/services/slack_service_spec.rb
@@ -4,7 +4,9 @@ describe SlackService do
    subject { SlackService }
 
    it 'sends a post request to the agile-bot with the proper data' do
+     Features.slack_notifications.enabled = true
      stub_request(:post, 'https://agile-bot.herokuapp.com/hubot/hangouts-notify')
+
      event = Event.new(name: "MockEvent", category: "PairProgramming")
      hangout = Hangout.new(hangout_url: "mock_url", event: event)
 
@@ -13,5 +15,7 @@ describe SlackService do
      assert_requested(:post, 'https://agile-bot.herokuapp.com/hubot/hangouts-notify', times: 1) do |req|
        expect(req.body).to eq 'title=MockEvent&link=mock_url&type=PairProgramming'
      end
+
+     Features.slack_notifications.enabled = false
    end
 end
