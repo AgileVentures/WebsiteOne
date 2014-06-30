872f438f9e554ded96fffbf62fd5fdcd7252cc42
Sampriti Panda <sampdingo@hotmail.com>
2014-06-30 10:40:26 +0530

    View Specs for Password Reset

--- spec/views/devise/registrations/new.html.erb_spec.rb
+++ spec/views/devise/registrations/new.html.erb_spec.rb
@@ -1,27 +1,31 @@
 require 'spec_helper'
 
-describe 'devise/registrations/new' do
+describe 'devise/registrations/new', :type => :view do
 
   before do
-    view.should_receive(:resource).at_least(1).times.and_return(User.new)
-    view.should_receive(:resource_name).at_least(1).times.and_return('user')
+    expect(view).to receive(:resource).at_least(1).times.and_return(User.new)
+    expect(view).to receive(:resource_name).at_least(1).times.and_return('user')
+    render
   end
 
   it 'renders the form to register' do
-    render
     assert_select 'form[action=?][method=?]', user_registration_path, 'post'  do
-      rendered.should have_css('input#user_email')
-      rendered.should have_css('input#user_password')
-      rendered.should have_css('input#user_password_confirmation')
-      rendered.should have_button('Sign up')
+      expect(rendered).to have_css('input#user_email')
+      expect(rendered).to have_css('input#user_password')
+      expect(rendered).to have_css('input#user_password_confirmation')
+      expect(rendered).to have_button('Sign up')
     end
   end
 
   it 'renders the social buttons' do
-    render
-    rendered.should have_css('.btn-github')
-    rendered.should have_link('GitHub', '/auth/github')
-    rendered.should have_css('.btn-gplus')
-    rendered.should have_link('Google+', '/auth/gplus')
+    expect(rendered).to have_css('.btn-github')
+    expect(rendered).to have_link('GitHub', '/auth/github')
+    expect(rendered).to have_css('.btn-gplus')
+    expect(rendered).to have_link('Google+', '/auth/gplus')
+  end
+
+  it 'renders the forgot password and sign in link' do
+    expect(rendered).to have_link('Sign in', '/users/sign_in')
+    expect(rendered).to have_link('Forgot your password?', '/users/password/new')
   end
 end
