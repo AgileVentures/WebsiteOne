e01db928196c23adcfde53f1201c031aa0735da4
Sampriti Panda <sampdingo@hotmail.com>
2014-06-30 10:40:26 +0530

    View Specs for Password Reset

---
+++ spec/views/devise/passwords/new.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'devise/passwords/new', type: :view do
+  before do
+    expect(view).to receive(:resource).at_least(1).times.and_return(User.new)
+    expect(view).to receive(:resource_name).at_least(1).times.and_return('user')
+    render
+  end
+
+  it 'renders the form to register' do
+    assert_select 'form[action=?][method=?]', user_password_path, 'post'  do
+      expect(rendered).to have_css('input#user_email')
+      expect(rendered).to have_button('Send me reset password instructions')
+    end
+  end
+end
