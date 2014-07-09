ca91b0c07ef801d1c22d1fea9c549645f09ed4a9
Yaro Apletov <yaro@mail.ru>
2014-07-09 10:38:47 +0100

    added UTC to schedule

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -17,7 +17,8 @@ module EventHelper
 
     return nil if time.nil?
 
-    time.strftime("%F at %I:%M%p")
+    event_date = time.strftime("%A, #{time.day.ordinalize} %b")
+    "#{event_date} at #{time.strftime("%I:%M%P")} (UTC)"
   end
   
   def topic(event, event_schedule)
