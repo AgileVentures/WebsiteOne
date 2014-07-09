a94ec48906c3504d66201aace543b984203fcc32
Yaro Apletov <yaro@mail.ru>
2014-07-09 19:53:47 +0100

    introduced feature settings for hangoutsApp

--- spec/support/shared_examples/shared_example_for_hangout_button.rb
+++ spec/support/shared_examples/shared_example_for_hangout_button.rb
@@ -2,7 +2,7 @@ shared_examples_for 'it has a hangout button' do
   let(:data_tags) do 
     data_tags = {
       'data-topic' => topic_name,
-      'data-app-id' => SERVICE_APP_ID,
+      'data-app-id' => Settings.hangouts.app_id,
       'data-callback-url' => hangout_url(id)
     }
   end
