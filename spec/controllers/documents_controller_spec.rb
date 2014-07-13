e0be018e1a206f91a9eea1e213e800f918347385
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-07-13 17:41:58 +0300

    Remove unused documents routes, #edit & #update in controller and corresponding tests

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -76,20 +76,6 @@ describe DocumentsController do
     end
   end
 
-  describe 'GET edit' do
-    before(:each) do
-      get :edit, {:id => document.to_param, project_id: document.project.friendly_id}, valid_session
-    end
-
-    it 'assigns the requested document as @document' do
-      assigns(:document).should eq(document)
-    end
-
-    it 'renders the edit template' do
-      expect(response).to render_template 'edit'
-    end
-  end
-
   describe 'POST create' do
     describe 'with valid params' do
       it 'creates a new Document' do
@@ -124,41 +110,6 @@ describe DocumentsController do
         Document.any_instance.stub(:save).and_return(false)
         post :create, {project_id: document.project.friendly_id, :document => { title: 'invalid value' }}, valid_session
         expect(response).to render_template 'new'
-      end
-    end
-  end
-
-  describe 'PUT update' do
-    describe 'with valid params' do
-      it 'updates the requested document' do
-        Document.any_instance.should_receive(:update).with(valid_attributes)
-        put :update, {id: document.to_param, project_id: document.project.friendly_id, document: valid_attributes}, valid_session
-      end
-
-      it 'assigns the requested document as @document' do
-        put :update, {id: document.to_param, project_id: document.project.friendly_id, document: valid_attributes}, valid_session
-        assigns(:document).should eq(document)
-      end
-
-      it 'redirects to the document' do
-        put :update, {id: document.to_param, project_id: document.project.friendly_id, document: valid_attributes}, valid_session
-        response.should redirect_to project_document_path(Document.last.project, Document.last)
-      end
-    end
-
-    describe 'with invalid params' do
-      it 'assigns the document as @document' do
-        Document.any_instance.stub(:save).and_return(false)
-        put :update, {:id => document.to_param, project_id: document.project.friendly_id, :document => { title: 'invalid value' }}, valid_session
-        assigns(:document).should eq(document)
-      end
-
-      it 're-renders the edit template' do
-        #document = Document.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Document.any_instance.stub(:save).and_return(false)
-        put :update, {:id => document.to_param, project_id: document.project.friendly_id, :document => { title: 'invalid value' }}, valid_session
-        expect(response).to render_template 'edit'
       end
     end
   end
