b21f506da719ae27cd295269cae9b1480e78d531
Christopher Milne <christopher.milne@gmail.com>
2014-07-14 19:52:44 -0400

    update articles model spec syntax

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,14 +5,14 @@ describe Article do
   let(:article) { Article.new }
 
   it 'should respond to tag_list' do
-    article.should respond_to :tag_list
+    expect(article).to respond_to :tag_list
   end
 
   it 'should respond to user' do
-    article.should respond_to :user
+    expect(article).to respond_to :user
   end
 
   it 'should respond to friendly_id' do
-    article.should respond_to :friendly_id
+    expect(article).to respond_to :friendly_id
   end
 end
