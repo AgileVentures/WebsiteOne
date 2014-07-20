9dd7691a60d755869e8828779129034deb9ff8f6
Yaro Apletov <yaro@mail.ru>
2014-07-20 21:33:03 +0100

    added uid and category to hangout model
    migrate

--- app/models/hangout.rb
+++ app/models/hangout.rb
@@ -1,5 +1,6 @@
 class Hangout < ActiveRecord::Base
   belongs_to :event
+  default_scope { order('created_at') }
 
   def started?
     hangout_url.present? && self.updated_at > 2.hours.ago
@@ -9,8 +10,8 @@ class Hangout < ActiveRecord::Base
     hangout_url.present? && self.updated_at > 15.minutes.ago
   end
 
-  # strong parameters
   def update_hangout_data(params)
-    update(title: params[:title], hangout_url: params[:hangout_url])
+    event = Event.find_by_id(params[:event_id])
+    update(title: params[:title], event: event, category: params[:category], hangout_url: params[:hangout_url])
   end
 end
