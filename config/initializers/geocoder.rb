28a9de8c31787acb6942409ace445a010ff764b2
Sam Joseph <tansaku@gmail.com>
2014-07-21 15:55:34 +0100

    trying to assess errors on production

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,5 +2,5 @@ Geocoder.configure(
     :lookup => :google,
     # IP address geocoding service (see below for supported options):
     :ip_lookup => :freegeoip,
-    :timeout => 0
+    :timeout => 2
 )
