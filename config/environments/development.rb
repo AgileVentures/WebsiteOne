161ab7f9ec0c7aa9400e8abe10353830e64a1573
Sampriti Panda <sampdingo@hotmail.com>
2014-07-21 13:27:11 +0530

    Using Settings instead of hard coded values

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,4 @@ WebsiteOne::Application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,6 @@ WebsiteOne::Application.configure do
 
   #config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'agileventures.org' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
