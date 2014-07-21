78f92527fc7d3a2519da5968ecd9fac1be331f8b
Sampriti Panda <sampdingo@hotmail.com>
2014-07-21 19:40:00 +0530

    Removing Contact Form

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,18 +1,6 @@
 class Mailer < ActionMailer::Base
   default from: 'info@agileventures.org', reply_to: 'info@agileventures.org', cc: 'support@agileventures.org'
 
-  def contact_form(contact_form)
-    @contact_form = contact_form
-    from = contact_form[:email]
-    mail(reply_to: from, to: 'info@agileventures.org', subject: 'WebsiteOne Contact Form')
-  end
-
-  def contact_form_confirmation(contact_form)
-    @contact_form = contact_form
-    to = contact_form[:email]
-    mail(to: to, subject: 'WebsiteOne Contact Form')
-  end
-
   def send_welcome_message(user)
     @user = user
     mail(to: user.email, subject: 'Welcome to AgileVentures.org')
