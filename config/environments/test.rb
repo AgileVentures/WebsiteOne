a863a02b60ebae1c076cf2a08588b6ef757b3963
Sampriti Panda <sampdingo@hotmail.com>
2014-07-21 13:27:11 +0530

    Using Settings instead of hard coded values

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,12 +25,6 @@ WebsiteOne::Application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
