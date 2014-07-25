1104399df6bbff130adab50fe4f8ac73a8adc144
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-07-25 23:48:25 +0300

    Run script through JSLint

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,19 +1,21 @@
 $('.readme-link').popover({trigger: 'focus'});
 WebsiteOne.eventTz = {
-    addEventTz: function() {
-        if (new Date().getTimezoneOffset() != 0) {
-            $('ul.occurrence').find('li').each(function() {
-                var UTCTime = new Date($(this).data('event-time'));
+    addEventTz: function () {
+        var UTCTime;
+        var localTime;
+        if (new Date().getTimezoneOffset() !== 0) {
+            $('ul.occurrence').find('li').each(function () {
+                UTCTime = new Date($(this).data('event-time'));
                 console.log(UTCTime);
                 if (!isNaN(UTCTime.getTime())) {
-                    var localTime = WebsiteOne.eventTz.getLocalTime(UTCTime);
+                    localTime = WebsiteOne.eventTz.getLocalTime(UTCTime);
                     $(this).append(' / <p class="eventLocalTime">' + localTime + '</p>');
                 }
             });
         }
     },
 
-    getLocalTime: function(eventTime) {
+    getLocalTime: function (eventTime) {
         var options = {year: "numeric", month: "2-digit", day: "numeric", hour: '2-digit', minute: '2-digit', timeZoneName: 'short'};
         return eventTime.toLocaleString(navigator.language, options);
     }
