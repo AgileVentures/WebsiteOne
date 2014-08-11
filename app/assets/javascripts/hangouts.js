d14fa51ec31f5677dc7ec2ea8f6a3cc72087ddf1
Yaro Apletov <yaro@mail.ru>
2014-08-11 22:22:25 +0100

    UI changes after code review

--- app/assets/javascripts/hangouts.js
+++ app/assets/javascripts/hangouts.js
@@ -1,5 +1,10 @@
+$('#collapse0').toggle();
 $('.user-popover').popover({trigger: 'hover'});
 $('#btn-hg-toggle').click(function (){
    $('.live-hangouts .collapse').collapse('toggle');
 });
-$('#collapse0').toggle();
+$('.panel-heading').click(function(){
+  if($(event.toElement).prop('tagName') !== 'A'){
+    $(this).closest('.panel').find('.panel-collapse').toggleClass('in');
+  }
+});
