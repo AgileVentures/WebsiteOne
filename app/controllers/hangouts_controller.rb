c7c5125c8cda0dec073b9b63c41e2b9dd208104a
Yaro Apletov <yaro@mail.ru>
2014-08-12 22:40:06 +0100

    even more specs

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -1,6 +1,6 @@
 class HangoutsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :cors_preflight_check
+  before_filter :cors_preflight_check, only: :update
 
   def update
     hangout = Hangout.find_or_create_by(uid: params[:id])
--- spec/support/shared_examples/shared_example_for_user_avatar.rb
+++ spec/support/shared_examples/shared_example_for_user_avatar.rb
@@ -4,28 +4,8 @@ shared_examples_for 'it has clickable user avatar with popover' do
     allow_any_instance_of(UserPresenter).to receive(:gravatar_image).and_return('user_gravatar')
   end
 
-  it 'renders user avatar' do
-    render
-    expect(rendered).to have_text('user_gravatar')
-  end
-
-  it 'renders link to user profile' do
-    render
-    expect(rendered).to have_link('', href: user_path(user))
-  end
-
   it 'renders a popover with user details' do
-    data_tags = { 'class' => 'user-popover',
-                  'data-html' => 'true',
-                  'data-container' => 'body',
-                  'data-toggle' => 'popover',
-                  'data-placement' => placement }
-
-    popover_content = 'Member for: <br/>User rating: <br/>PP sessions:'
-    user_tags = { 'data-title' => 'user_name',
-                  'data-content' => popover_content }
     render
-    expect(rendered).to have_selector('a', data_tags)
-    expect(rendered).to have_selector('a', user_tags)
+    expect(rendered).to match /#{user.presenter.user_avatar_with_popover({ placement: placement })}/
   end
 end
