1f5595873f2f879545d2d24f474f8f1edc16ff60
Yaro Apletov <yaro@mail.ru>
2014-08-12 19:49:54 +0100

    refactored specs to use Hangout factory

--- spec/factories/hangout.rb
+++ spec/factories/hangout.rb
@@ -1,4 +1,26 @@
 FactoryGirl.define do
+  sequence :participant do |n|
+    { name: "Participant_#{n}", gplus_id: "youtube_id_#{n}"}
+  end
+
   factory :hangout do
+    ignore do
+      created Time.now
+      updated Time.now
+    end
+
+    sequence(:title) { |n| "Hangout_#{n}"}
+    sequence(:category) { |n| "Category_#{n}"}
+    hangout_url "http://hangout.test"
+    yt_video_id "yt_video_id"
+
+    project
+    event
+    association :host, factory: :user
+
+    participants { [(generate :participant), (generate :participant)] }
+
+    created_at { Time.parse("#{created} UTC")}
+    updated_at { Time.parse("#{updated} UTC")}
   end
 end
