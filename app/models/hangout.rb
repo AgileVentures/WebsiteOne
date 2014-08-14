f7cb1ee649af747e9ed5e32b3fb42edc28fa9212
Sampriti Panda <sampdingo@hotmail.com>
2014-08-15 00:45:00 +0530

    Removing unused methods from Hangout model and converting class methods to scopes

--- app/models/hangout.rb
+++ app/models/hangout.rb
@@ -5,36 +5,20 @@ class Hangout < ActiveRecord::Base
 
   serialize :participants
 
+  scope :started, -> { where.not(hangout_url: nil) }
+  scope :live, -> { where('updated_at > ?', 5.minutes.ago) }
+  scope :pp_hangouts, -> { where(category: 'PairProgramming') }
+
   def started?
-    hangout_url.present?
+    hangout_url?
   end
 
   def live?
-    hangout_url.present? && self.updated_at > 5.minutes.ago
-  end
-
-  def hookup?
-    category == "PairProgramming"
-  end
-
-  def expired?
-    started? && !live?
+    started? && updated_at > 5.minutes.ago
   end
 
   def self.active_hangouts
     select(&:live?)
-  end
-
-  def self.live
-    where('hangouts.updated_at > :five_minutes_ago', five_minutes_ago: 5.minutes.ago.to_datetime)
-  end
-
-  def self.started
-    where.not(hangout_url: nil)
-  end
-
-  def self.pp_hangouts
-    where(category: "PairProgramming")
   end
 
   def start_datetime
