e6a5cf5ec70a2bf823139413f51a11e545c4225b
Sampriti Panda <sampdingo@hotmail.com>
2014-08-14 19:35:09 +0530

    Refactored Document Spec to RSpec 3 syntax

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,68 +1,35 @@
 require 'spec_helper'
 
-describe Document do
-  before do
-    @project = Project.create!(valid_attributes_for(:project))
-    @document = @project.documents.create!(valid_attributes_for(:document))
+describe Document, :type => :model do
+  subject { FactoryGirl.build_stubbed(:document) }
+
+  it { is_expected.to be_versioned }
+
+  it 'is valid with proper attributes' do
+    expect(FactoryGirl.build(:document)).to be_valid
   end
 
-  it { should be_versioned }
-
-  context 'return false on invalid inputs' do
-    it 'blank Title' do
-      @document.title = ''
-      expect(@document.save).to be_false
-    end
-
-    it 'blank project' do
-      @document.project_id = nil
-      expect(@document.save).to be_false
-    end
+  it 'is invalid without title' do
+    expect(FactoryGirl.build(:document, title: '')).to_not be_valid
   end
 
-  # TODO Bryan: this scenario cannot be implemented with the current gem
-  #it 'should allow friendly IDs to be shared between projects' do
-  #  project = Project.create! valid_attributes_for(:project)
-  #  doc = project.documents.create! title: @document.title
-  #  expect(doc.friendly_id).to eq @document.friendly_id
-  #end
-
-  it 'should NOT allow friendly IDs to be shared within a project' do
-    doc = Document.create(title: @document.title, project_id: @document.project_id)
-    expect(doc.friendly_id).to_not eq @document.friendly_id
-  end
-
-  context 'return true on correct inputs' do
-    it 'belongs to project' do
-      expect(@document.project.nil?).to be_false
-    end
+  it 'is invalid without project' do
+    expect(FactoryGirl.build(:document, project: nil)).to_not be_valid
   end
 
   describe '#url_for_me' do
     it 'returns correct url for show action' do
-      @document.url_for_me('show').should eq "/projects/#{@project.slug}/documents/#{@document.slug}"
+      expect(subject.url_for_me('show')).to eq "/projects/#{subject.project.slug}/documents/#{subject.slug}"
     end
 
     it 'returns correct url for other actions' do
-      @document.url_for_me('new').should eq "/projects/#{@project.slug}/documents/#{@document.slug}/new"
+      expect(subject.url_for_me('new')).to eq "/projects/#{subject.project.slug}/documents/#{subject.slug}/new"
     end
   end
 
   describe '#slug_candidates' do
     it 'returns correct slug candidates' do
-      @document.slug_candidates.should eq [ :title, [:title, :project_title] ]
+      expect(subject.slug_candidates).to eq [ :title, [:title, :project_title] ]
     end
   end
-
-  describe '#project_title' do
-    it 'returns nil if project does not exists' do
-      @document.stub(:project_id)
-      @document.project_title.should be_nil
-    end
-
-    it 'returns project titles if project xists' do
-      @document.project_title.should eq @project.title
-    end
-  end
-
 end
