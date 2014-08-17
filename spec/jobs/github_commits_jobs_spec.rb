0afa34895bfc78cd9cfa4d70549e22d9a76471d4
Sampriti Panda <sampdingo@hotmail.com>
2014-08-17 19:34:16 +0530

    Tests for the GitHub commit count job

--- spec/jobs/github_commits_jobs_spec.rb
+++ spec/jobs/github_commits_jobs_spec.rb
@@ -1,15 +1,35 @@
 require 'spec_helper'
 
 describe GithubCommitsJob do
-  describe '.job' do
+  vcr_index = {cassette_name: 'github_commit_count/websiteone_stats'}
+
+  describe '.job', vcr: vcr_index do
     before do
-      FactoryGirl.create_list(:project, 9, github_url: nil)
-      FactoryGirl.create_list(:project, 3, github_url: 'http://github.com/project')
+      @project = FactoryGirl.create(:project, github_url: 'http://github.com/AgileVentures/WebsiteOne')
+      @project_without_url = FactoryGirl.create(:project)
+      @users = [
+        FactoryGirl.create(:user, github_profile_url: 'https://github.com/yggie'),
+        FactoryGirl.create(:user, github_profile_url: 'https://github.com/tochman'),
+        FactoryGirl.create(:user, github_profile_url: nil)
+      ]
+      GithubCommitsJob.run
     end
 
-    it 'updates commit counts for projects that have a github_url' do
-      expect(CommitCount).to receive(:update_commit_counts_for).with(an_instance_of(Project)).exactly(3).times
-      GithubCommitsJob.run
+    it 'stores commit counts only for projects that have a github_url' do
+      expect(@project.commit_counts.count).to be > 1
+      expect(@project_without_url.commit_counts.count).to eq 0
+    end
+
+    it 'stores commit counts only for users that have a github_profile_url' do
+      commit_count_users = @project.commit_counts.map(&:user)
+      expect(commit_count_users).to_not include(@users[2])
+      expect(commit_count_users).to include(@users[0])
+      expect(commit_count_users).to include(@users[1])
+    end
+
+    it 'stores correct commit counts by user and project' do
+      expect(CommitCount.find_by!(project: @project, user: @users[0]).commit_count).to eq 388
+      expect(CommitCount.find_by!(project: @project, user: @users[1]).commit_count).to eq 316
     end
   end
 end
