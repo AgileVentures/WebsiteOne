50c833057e1abf63992b667a546bae525ed372cc
Sampriti Panda <sampdingo@hotmail.com>
2014-08-17 20:15:19 +0530

    Switch to Octokit.rb

--- app/jobs/github_commits_job.rb
+++ app/jobs/github_commits_job.rb
@@ -10,14 +10,10 @@ module GithubCommitsJob
   private
 
   def update_commit_counts_for(project)
-    contributors_json = fetch_contributor_data(project)
-    contributors_json.map do |contributor|
+    contributors = Octokit.contributor_stats(project.github_repo)
+    contributors.map do |contributor|
       user = User.find_by_github_username(contributor[:author][:login])
       project.commit_counts.create(user: user, commit_count: contributor[:total]) if user
     end
-  end
-
-  def fetch_contributor_data(project)
-    JSON.parse(open("https://api.github.com/repos/#{project.github_repo}/stats/contributors").read, symbolize_names: true)
   end
 end
