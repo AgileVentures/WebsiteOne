5a9b1d2e8c2dafa56329d6942d4b58003ec0934b
Sampriti Panda <sampdingo@hotmail.com>
2014-08-17 17:57:53 +0530

    Moving logic for the GithubCommit fetching from the model to the job

--- app/jobs/github_commits_job.rb
+++ app/jobs/github_commits_job.rb
@@ -1,7 +1,23 @@
-class GithubCommitsJob
-  def self.run
+module GithubCommitsJob
+  extend self
+
+  def run
     Project.with_github_url.each do |project|
-      CommitCount.update_commit_counts_for(project)
+      update_commit_counts_for(project)
     end
+  end
+
+  private
+
+  def update_commit_counts_for(project)
+    contributors_json = fetch_contributor_data(project)
+    contributors_json.map do |contributor|
+      user = User.find_by_github_username(contributor[:author][:login])
+      project.commit_counts.create(user: user, commit_count: contributor[:total]) if user
+    end
+  end
+
+  def fetch_contributor_data(project)
+    JSON.parse(open("https://api.github.com/repos/#{project.github_repo}/stats/contributors").read, symbolize_names: true)
   end
 end
--- app/models/commit_count.rb
+++ app/models/commit_count.rb
@@ -5,18 +5,5 @@ class CommitCount < ActiveRecord::Base
   belongs_to :project
 
   validates :user, :project, :commit_count, presence: true
-
-  def self.update_commit_counts_for(project)
-    contributors_json = fetch_contributor_data(project)
-    contributors_json.map do |contributor|
-      user = User.find_by_github_username(contributor[:author][:login])
-      project.commit_counts.create(user: user, commit_count: contributor[:total]) if user
-    end
-  end
-
-  private
-
-  def self.fetch_contributor_data(project)
-    JSON.parse(open("https://api.github.com/repos/#{project.github_repo}/stats/contributors").read, symbolize_names: true)
-  end
 end
+
