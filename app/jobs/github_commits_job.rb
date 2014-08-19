fa8e12d85a9ef1b8e3f270e4e5f2cf262b1468c8
Sampriti Panda <sampdingo@hotmail.com>
2014-08-19 22:20:33 +0530

    Implemented Contributions on User Profile Page

--- app/jobs/github_commits_job.rb
+++ app/jobs/github_commits_job.rb
@@ -12,8 +12,8 @@ module GithubCommitsJob
   def update_commit_counts_for(project)
     contributors = Octokit.contributor_stats(project.github_repo)
     contributors.map do |contributor|
-      user = User.find_by_github_username(contributor[:author][:login])
-      project.commit_counts.create(user: user, commit_count: contributor[:total]) if user
+      user = User.find_by_github_username(contributor.author.login)
+      project.commit_counts.create(user: user, commit_count: contributor.total) if user
     end
   end
 end
