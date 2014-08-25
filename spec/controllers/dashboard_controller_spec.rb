637ee9eba91a7b88eb485e9aa4ce19efbf558d51
Yaro Apletov <yaro@mail.ru>
2014-08-25 15:51:22 +0100

    added StatiscsConcern and spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,29 +2,27 @@ require 'spec_helper'
 
 describe DashboardController, type: :controller do
 
-   describe "GET index" do
+  describe "GET index" do
 
-     it "renders the correct template" do
-       get :index
-       expect(response).to render_template 'dashboard/index'
-     end
+    it "renders the correct template" do
+      get :index
+      expect(response).to render_template 'dashboard/index'
+    end
 
-     it "assigns stats variable" do
-        allow(controller).to receive(:get_stats).and_return('test')
-        get :index
-        expect(assigns(:stats)).to eq 'test'
-     end
+    it "assigns stats variable" do
+      allow(controller).to receive(:get_stats).and_return('test')
+      get :index
+      expect(assigns(:stats)).to eq 'test'
+    end
+  end
+end
 
-     describe "gets stats for" do
-     end
-   end
-
-   # describe "computes interesting statistics" do
-   #
+# describe "computes interesting statistics" do
+  #
    #   it "counts articles" do
    #     allow(Article).to receive(:count).and_return(10)
    #     expect(controller.get_article_count[:articles][:count]).to eq 10 
-   #   end
+  #   end
    #
    #   it "counts projects" do
    #     allow(Project).to receive(:count).and_return(10)
@@ -43,4 +41,3 @@ describe DashboardController, type: :controller do
    #   end
    # end
 
-end 
