aa5ebda5f8a78b6ade220d871d49a5de78271b56
Yaro Apletov <yaro@mail.ru>
2014-08-25 15:51:22 +0100

    added StatiscsConcern and spec

--- spec/controllers/concerns/statistics_spec.rb
+++ spec/controllers/concerns/statistics_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
-require 'statistics'
 
-describe "Statistics", type: :controller do
-  it "gets stats for" do
-    expect(Statistics::get_stats_for(:articles)).to eq({})
+class FakeController < ActionController::Base
+  include Statistics
+end
+
+describe 'StatisticsConcern' do
+  let(:fake_controller) { FakeController.new }
+
+  describe FakeController, type: :controller do
+    it "gets stats for articles" do
+      FactoryGirl.create_list(:article, 5)
+      expect(fake_controller.get_stats_for(:articles)).to eq({ count: 5})
+    end
+    
+    it "gets stats for projects" do
+      FactoryGirl.create_list(:project, 5, status: 'active')
+      FactoryGirl.create_list(:project, 3, status: 'disactivated')
+      expect(fake_controller.get_stats_for(:projects)).to eq({ count: 5})
+    end
+
+    it "gets stats for members" do
+      FactoryGirl.create_list(:user, 5)
+      expect(fake_controller.get_stats_for(:members)).to eq({ count: 5})
+    end
   end
 end
