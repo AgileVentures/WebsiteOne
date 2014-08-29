b506b9bff6ecf1e18a07d155e48cfb96aa69fe61
Yaro Apletov <yaro@mail.ru>
2014-08-29 12:11:44 +0100

    falling back to js

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,31 @@
+this.EventsUtils = function() {
+  this.ajaxRequest = function() {
+    var _this = this;
+    if (window.location.href === _this.href) {
+      return $.get(_this.href, _this.updateHangoutsData);
+    } else {
+      clearInterval(_this.intervalId);
+    }
+  };
+
+  this.updateHangoutsData = function(data) {
+    var _this = this;
+    if (data.match('hangouts-details-well')) {
+      clearInterval(_this.intervalId);
+
+      $('#hg-management').html(data);
+      $('.readme-link').popover({ trigger: 'focus' });
+      WebsiteOne.renderHangoutButton();
+    }
+  };
+
+  this.init = function() {
+    var _this = this;
+    _this.href = window.location.href;
+    _this.intervalId = setInterval(_this.ajaxRequest, 10000);
+
+    $('.readme-link').popover({ trigger: 'focus' });
+  };
+};
+
+(new this.EventsUtils).init();
