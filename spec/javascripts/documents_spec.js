3717f095b95be79344c5ac9c3ea22d6d03adbd55
Christopher Milne <christopher.milne@gmail.com>
2014-09-01 17:23:45 -0400

    jasmine test now working. jQuery animations turned off

--- spec/javascripts/documents_spec.js
+++ spec/javascripts/documents_spec.js
@@ -4,19 +4,19 @@ describe('Documents', function(){
     appendSetFixtures('<a id="revisions-anchor" href="#">Revisions</a><div id="revisions" style="display:none"><br/>This is <b>first</b> revision</div>');
     reloadScript('documents.js');
     WebsiteOne.Documents.init();
+    jQuery.fx.off = true;
   });
 
   it('prevents refreshing the page after link gets clicked', function() {
     var submitSpy = jasmine.createSpy('submitSpy')
     $(document).on('submit', submitSpy);
 
-    $('#revisions-anchor').trigger('cklick');
+    $('#revisions-anchor').trigger('click');
 
     expect(submitSpy).not.toHaveBeenCalled();
   });
 
   it('shows revisions div after Revisions link is clicked', function(){
-    //expect($('#revisions')).toHaveAttr('style', 'display:none');
     $('#revisions-anchor').trigger('click');
     expect($('#revisions')).toBeVisible();
   });
@@ -25,5 +25,6 @@ describe('Documents', function(){
     $('#revisions-anchor').trigger('click');
     $('#revisions-anchor').trigger('click');
     expect($('#revisions')).toBeHidden();
+    jQuery.fx.off = false
   });
 });
