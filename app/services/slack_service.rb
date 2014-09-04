23c5cfa428820bedfb51d34765a144d10da109cc
Sampriti Panda <sampdingo@hotmail.com>
2014-09-05 01:18:22 +0530

    Added Support for Host in SlackService

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -8,7 +8,16 @@ module SlackService
     Net::HTTP.post_form uri, {
       title: hangout.title,
       link: hangout.hangout_url,
-      type: hangout.category
+      type: hangout.category,
+      host_name: hangout.user.display_name,
+      host_avatar: avatar_url(hangout.user)
     }
+  end
+
+  private
+
+  def avatar_url(user)
+    hash = Digest::MD5::hexdigest(user.email.strip.downcase)
+    "https://www.gravatar.com/avatar/#{hash}"
   end
 end
