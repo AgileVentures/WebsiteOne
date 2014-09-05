222b904772a6c43f644f00512227ef35e6cb3684
Sampriti Panda <sampdingo@hotmail.com>
2014-09-05 16:16:35 +0530

    Added spec for Slack Service

--- spec/services/slack_service_spec.rb
+++ spec/services/slack_service_spec.rb
@@ -6,12 +6,14 @@ describe SlackService do
    it 'sends a post request to the agile-bot with the proper data' do
      Features.slack_notifications.enabled = true
      stub_request(:post, 'https://agile-bot.herokuapp.com/hubot/hangouts-notify')
-     hangout = Hangout.new(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url")
+     user = User.new email: 'random@random.com'
+     gravatar = CGI.escape 'https://www.gravatar.com/avatar/47548e7f026bc689ba743b2af2d391ee?d=retro'
+     hangout = Hangout.new(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user)
 
      subject.post_hangout_notification(hangout)
 
      assert_requested(:post, 'https://agile-bot.herokuapp.com/hubot/hangouts-notify', times: 1) do |req|
-       expect(req.body).to eq 'title=MockEvent&link=mock_url&type=PairProgramming'
+       expect(req.body).to eq "title=MockEvent&link=mock_url&type=PairProgramming&host_name=random&host_avatar=#{gravatar}"
      end
    end
 end
