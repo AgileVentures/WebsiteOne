a1e5569e5a1465a9bb7311d4ff0095612829a009
Christopher Milne <christopher.milne@gmail.com>
2014-09-20 09:05:31 -0400

    readability improvements to mercury authentication

--- lib/mercury/authentication.rb
+++ lib/mercury/authentication.rb
@@ -2,28 +2,27 @@ module Mercury
   module Authentication
 
     def can_edit?
-      if user_signed_in? && valid_mercury_path? 
-        true
-      else
-        flash[:alert] = 'You do not have the right privileges to complete action.' #TODO: this is not the right error message
+      unless user_signed_in? && valid_mercury_path? 
+        flash[:alert] = 'You do not have the right privileges to complete action.'
         false
+      else
+        true
       end
     end
    
     def valid_mercury_path?
-      return true if is_project_edit_path? || is_in_path? || static_page_exists? 
+      return true if is_project_edit_path? || is_documents_edit_path? || is_static_pages_edit_path? 
     end
  
     def is_project_edit_path?
-      /editor\/projects\/.+/i =~ request.env['PATH_INFO']
+      /editor(\/|\/\/)projects\/[^\/]+/i =~ request.env['PATH_INFO']
     end
 
-    def is_in_path?
-      # TODO: what is this ???
-      /editor(\/|\/\/)projects\/[^\/]+\/documents\/[^\/]+/i.match(request.env['PATH_INFO'])
+    def is_documents_edit_path?
+      /editor(\/|\/\/)projects\/[^\/]+\/documents\/[^\/]+/i =~ request.env['PATH_INFO']
     end
 
-    def static_page_exists?
+    def is_static_pages_edit_path? 
       StaticPage.friendly.exists?((params[:requested_uri] || request.env['PATH_INFO']).split("/").reject { |i| ["mercury_saved", "mercury_update"].include? i }.last)
     end
   end
