d6a80e3f313105c32f7d7e25751ba9b443ceec60
Sampriti Panda <sampdingo@hotmail.com>
2014-10-05 10:14:17 -0400

    Added loop in github commits job to make sure that the latest contributors data is computed and fetched properly

--- app/jobs/github_commits_job.rb
+++ app/jobs/github_commits_job.rb
@@ -10,10 +10,18 @@ module GithubCommitsJob
   private
 
   def update_commit_counts_for(project)
-    contributors = Octokit.contributor_stats(project.github_repo)
+    contributors = get_contributor_stats(project.github_repo)
     contributors.map do |contributor|
       user = User.find_by_github_username(contributor.author.login)
       CommitCount.find_or_initialize_by(user: user, project: project).update(commit_count: contributor.total) if user
+    end
+  end
+
+  def get_contributor_stats(repo)
+    loop do
+      contributors = Octokit.contributor_stats(repo)
+      return contributors unless contributors.nil?
+      sleep 3
     end
   end
 end
