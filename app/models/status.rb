1a52cc29f50feb8e97cee5c58c5cabaa29803219
Thomas Ochman <thomas@nocebo.se>
2014-10-06 23:25:31 +0200

    code changes after code review PP-session with Bryan

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,10 @@ class Status < ActiveRecord::Base
   belongs_to :user
 
   validates :status, :user_id, presence: true
+
+  OPTIONS = ['Ready to pair',
+             'Doing code review',
+             'Plz, do not disturb',
+             'Waiting for next scrum...',
+             'Sleeping at my keyboard']
 end
