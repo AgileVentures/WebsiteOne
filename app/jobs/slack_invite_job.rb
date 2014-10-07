bfd1668fbe6d04b84e50845657b80630306de357
Sampriti Panda <sampdingo@hotmail.com>
2014-10-07 19:55:14 +0530

    Return proper response from invite job

--- app/jobs/slack_invite_job.rb
+++ app/jobs/slack_invite_job.rb
@@ -3,10 +3,11 @@ class SlackInviteJob
 
   def perform(email)
     uri = URI.parse 'https://agileventures.slack.com/api/users.admin.invite'
-    Net::HTTP.post_form(uri, {
+    response = Net::HTTP.post_form(uri, {
       email: email,
       channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
       token: Slack::AUTH_TOKEN
     })
+    JSON.parse response.body
   end
 end
