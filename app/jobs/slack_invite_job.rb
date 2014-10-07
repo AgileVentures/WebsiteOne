8bd9b45018fb9efb17aa868da8dfd3d2ffdb4fce
Sampriti Panda <sampdingo@hotmail.com>
2014-10-07 19:53:36 +0530

    Switched from Mechanize to Net::HTTP

--- app/jobs/slack_invite_job.rb
+++ app/jobs/slack_invite_job.rb
@@ -2,8 +2,8 @@ class SlackInviteJob
   include SuckerPunch::Job
 
   def perform(email)
-    mechanize = Mechanize.new
-    mechanize.post('https://agileventures.slack.com/api/users.admin.invite', {
+    uri = URI.parse 'https://agileventures.slack.com/api/users.admin.invite'
+    Net::HTTP.post_form(uri, {
       email: email,
       channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
       token: Slack::AUTH_TOKEN
