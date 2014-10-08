f6b73428b3fd59a789097fdde364601b347fef45
Rene Paulokat <rene@so36.net>
2014-10-08 11:53:01 +0200

    fix - thx @tochman && @sampritipanda

--- db/migrate/20141002084933_create_newsletters.rb
+++ db/migrate/20141002084933_create_newsletters.rb
@@ -4,8 +4,8 @@ class CreateNewsletters < ActiveRecord::Migration
       t.string :title, null: false
       t.string :subject, null: false
       t.text :body, null: false
-      t.boolean :do_send, :boolean, default: false
-      t.boolean :was_sent, :boolean, default: false
+      t.boolean :do_send, default: false
+      t.boolean :was_sent, default: false
       t.integer :last_user_id, default: 0
       t.datetime :sent_at
 
