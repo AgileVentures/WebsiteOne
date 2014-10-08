486cde3e10cc988261f36609b776f9e8a8782143
Rene Paulokat <rene@so36.net>
2014-10-08 14:43:48 +0200

    spec sending expected emails per chunk - @yaggi

--- spec/jobs/send_newsletter_spec.rb
+++ spec/jobs/send_newsletter_spec.rb
@@ -22,7 +22,7 @@ describe SendNewsletter do
     it 'persists last processed user_id' do
       SendNewsletter.run
       @newsletter.reload
-      @newsletter.last_user_id.should > 0
+      expect(@newsletter.last_user_id).to be > 0
     end
 
     it 'was_sent is false until all recipients processed' do
@@ -35,6 +35,29 @@ describe SendNewsletter do
       2.times { SendNewsletter.run }
       @newsletter.reload
       expect(@newsletter.was_sent).to be_truthy
+    end
+
+    it 'sends newsletter to adequate users per run' do
+      # empty mail-queue
+      ActionMailer::Base.deliveries = []
+
+      first_chunk = User.mail_receiver.order('id ASC').limit(Newsletter::CHUNK_SIZE)
+      second_chunk = User.mail_receiver.order('id ASC').where('id > ?', first_chunk.last.id)
+      # first run
+      SendNewsletter.run
+      @newsletter.reload
+
+      expect(@newsletter.last_user_id).to eq(first_chunk.last.id)
+      expect(ActionMailer::Base.deliveries.map(&:to).flatten).to eq(first_chunk.map(&:email))
+
+      # empty mail-queue
+      ActionMailer::Base.deliveries = []
+      #second run
+      SendNewsletter.run
+      @newsletter.reload
+
+      expect(@newsletter.last_user_id).to eq(second_chunk.last.id)
+      expect(ActionMailer::Base.deliveries.map(&:to).flatten).to eq(second_chunk.map(&:email))
     end
   end
 
