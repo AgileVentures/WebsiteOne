d13f1537a95ed485b41b326048e36d8778295a4c
Norbert Szivos <sysqa@yahoo.com>
2014-10-18 12:30:19 +0200

    Change the Bullet config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ WebsiteOne::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
