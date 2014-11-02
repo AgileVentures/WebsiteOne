4f0db677e255f96c2d9b7dafdb794b31105d0be7
Norbert Szivos <sysqa@yahoo.com>
2014-11-02 16:38:16 +0100

    Add email verification to the hire_me_contact_form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   before_filter :get_user, only: [:show, :add_status]
   before_filter :authenticate_user!, only: [:add_status]
 
+  EMAIL_REGEXP = /^(("[^\f\n\r\t\v\b]+[\s\w\(\),:;<>\[\]@\\!\#$%&'"*+\/=?^`{|}~-]+")|([\w!\#$%&'*+\/=?^`{|}~-]+(?:\.[\w!\#$%&'*+\/=?^`{|}~-]+)*))@((((\w+\-+)|(\w+\.))*\w{1,}\.[a-zA-Z]{2,6})|([a-zA-Z]{2,6}))$/
+
   def index
     @users = User.search(params)
   end
@@ -23,6 +25,8 @@ class UsersController < ApplicationController
       redirect_to :back, alert: 'Please fill in Name, Email and Message field'
     elsif !User.find(message_params['recipient_id']).display_hire_me
       redirect_to :back, alert: 'User has disabled hire me button'
+    elsif !EMAIL_REGEXP.match(message_params['email'])
+      redirect_to :back, alert: 'Please give a valid email address'
     elsif !message_params['fellforit'].blank?
       redirect_to :root, notice: 'Form not submitted. Are you human?'
     elsif Mailer.hire_me_form(User.find(message_params['recipient_id']), message_params).deliver
