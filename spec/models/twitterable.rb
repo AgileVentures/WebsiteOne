78654417735980452f1bb8a7fe1421b2ded58fd7
Christopher Milne <christopher.milne@gmail.com>
2014-11-23 12:21:17 -0500

    refactor twitterable concern, better separation of concerns, better test coverage

--- spec/models/twitterable.rb
+++ spec/models/twitterable.rb
@@ -2,39 +2,44 @@ require 'spec_helper'
 require 'twitterable'
 
 describe 'TwitterConcern' do
-    before :all do
-      # FIXME make better use of the settings
-      Features.twitter.hangout_notifications.enabled = true
-
-      # TODO most Twitterable methods are now class methods. Does it still make sense to test the 'mixin' functionality?
-      class FakeModel
-        include ActiveModel::Model
-        include Twitterable
-      end
-      @fake_model = FakeModel.new
-    end
-
-    it "should create a twitter client object" do
-      expect(Twitterable.can_tweet?).to be_truthy
-    end
-
-    it "consumer key should be defined" do
-        twitter_client = Twitterable.twitter_client
-        expect(twitter_client.consumer_key).to_not be_nil
-    end
-
-    it 'sends a post request to the twitter api' do
+    before :each do
+      Settings.features.twitter.notifications.enabled = true
       stub_request(:post, /api\.twitter\.com/).to_return(:status => 200, :body => '{ "id": 243145735212777472, "text": "hello world" }')
-
-      response = Twitterable.tweet('hello world')
-
-      assert_requested(:post, /api\.twitter\.com/) do |req|
-        expect(response).to eq true
-      end
     end
 
-    after :all do
-      Object.send(:remove_const, :FakeModel)
-      Features.twitter.hangout_notifications.enabled = false
+    context "with tweet notification setting" do
+      describe "enabled" do
+        it "allows the tweet" do
+          expect(Twitterable.tweet('this should tweet')).to be_truthy
+        end
+
+        it "creates a valid twitter client object" do
+          expect(Twitterable.twitter_client.kind_of?(Twitter::REST::Client)).to eq true
+        end
+
+        it "consumer key should be defined" do
+          twitter_client = Twitterable.twitter_client
+
+          expect(twitter_client.consumer_key).to_not be_nil
+          expect(twitter_client.consumer_secret).to_not be_nil
+          expect(twitter_client.access_token).to_not be_nil
+          expect(twitter_client.access_token_secret).to_not be_nil
+        end
+
+        it 'sends a post request to the twitter api' do
+          response = Twitterable.tweet('hello world')
+
+          assert_requested(:post, /api\.twitter\.com/) do |req|
+            expect(response).to eq true
+          end
+        end
+      end
+
+      describe "disabled" do
+        it "does not allow the tweet" do
+          Settings.features.twitter.notifications.enabled = false
+          expect(Twitterable.tweet('this should not tweet')).to eq false
+        end
+      end
     end
 end
