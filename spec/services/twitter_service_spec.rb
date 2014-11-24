0df8b2dd5f7736bd138db53e0f80c4e9db10b6c7
Christopher Milne <christopher.milne@gmail.com>
2014-11-24 13:02:30 -0500

    removed nesting for some tests

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -13,33 +13,33 @@ describe 'TwitterService' do
           expect(TwitterService.tweet('this should tweet')).to be_truthy
         end
 
-        it "creates a valid twitter client object" do
-          expect(TwitterService.twitter_client.kind_of?(Twitter::REST::Client)).to eq true
-        end
-
-        it "consumer key should be defined" do
-          twitter_client = TwitterService.twitter_client
-
-          expect(twitter_client.consumer_key).to_not be_nil
-          expect(twitter_client.consumer_secret).to_not be_nil
-          expect(twitter_client.access_token).to_not be_nil
-          expect(twitter_client.access_token_secret).to_not be_nil
-        end
-
-        it 'sends a post request to the twitter api' do
-          response = TwitterService.tweet('hello world')
-
-          assert_requested(:post, /api\.twitter\.com/) do |req|
-            expect(response).to eq true
-          end
-        end
       end
-
       describe "disabled" do
         it "does not allow the tweet" do
           Settings.features.twitter.notifications.enabled = false
           expect(TwitterService.tweet('this should not tweet')).to eq false
         end
+      end
+    end
+
+    it "creates a valid twitter client object" do
+      expect(TwitterService.twitter_client.kind_of?(Twitter::REST::Client)).to eq true
+    end
+
+    it "configures the client object" do
+      twitter_client = TwitterService.twitter_client
+
+      expect(twitter_client.consumer_key).to_not be_nil
+      expect(twitter_client.consumer_secret).to_not be_nil
+      expect(twitter_client.access_token).to_not be_nil
+      expect(twitter_client.access_token_secret).to_not be_nil
+    end
+
+    it 'sends a post request to the twitter api' do
+      response = TwitterService.tweet('hello world')
+
+      assert_requested(:post, /api\.twitter\.com/) do |req|
+        expect(response).to eq true
       end
     end
 end
