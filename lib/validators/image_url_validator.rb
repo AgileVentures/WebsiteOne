75ce93e82a3e1ff557aa5417ea3f1af338571cea
Christopher Milne <christopher.milne@gmail.com>
2014-12-05 10:31:12 -0500

    image_url_validator: basic whitelist validation working

--- lib/validators/image_url_validator.rb
+++ lib/validators/image_url_validator.rb
@@ -3,12 +3,14 @@ class ImageUrlValidator < ActiveModel::Validator
   IMAGE_HOST_WHITELIST = %w(bluemelon.com deviantart.com facebook.com flickr.com freeimagehosting.net imagebam.com imageshack.com imagevenue.com imgur.com instagram postimage.org photobucket.com picasa.com shutterfly.com slickpic.com snapfish.com smugmug.com tinypic.com twitpic.com webshots.com weheartit.com zenfolio.com)
 
   def validate(record)
-    is_whitelisted?(record.image_url) if record.image_url.present?
+    unless is_whitelisted?(record.image_url)
+      record.errors[:image_url] = 'Invalid image url. Image provider not found in provider whitelist.'
+    end
   end
 
   private
 
   def is_whitelisted?(url)
-    IMAGE_HOST_WHITELIST.include? url
+    IMAGE_HOST_WHITELIST.any? {|whitelist_item| url.match /#{Regexp.escape(whitelist_item)}/ }
   end
 end
