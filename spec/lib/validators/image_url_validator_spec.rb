8dc7fc1df901b7c54ffe8ce65d20d3742610b845
Christopher Milne <christopher.milne@gmail.com>
2014-12-05 09:23:22 -0500

    image_url_validator: begin TDD with one passing example

---
+++ spec/lib/validators/image_url_validator_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+
+describe ImageUrlValidator do
+  let(:dummy_class) do
+    Class.new do
+      include ActiveModel::Validations
+      attr_accessor :image_url
+      validates_with ImageUrlValidator
+    end
+  end
+
+  subject { dummy_class.new }
+
+  it 'should be valid for a valid image url' do
+    subject.image_url = 'https://www.facebook.com/filename.jpg'
+    subject.valid?
+    expect(subject.errors.full_messages).to eq([])
+  end
+
+  ['http://github.com/AgileVentures/WebsiteOne','<>hi'].each do |invalid_url|
+    xit "#{invalid_url.inspect} is an invalid url" do
+      subject.image_url = invalid_url
+      subject.valid?
+      expect(subject.errors).to have_key(:image_url)
+    end
+  end
+end
