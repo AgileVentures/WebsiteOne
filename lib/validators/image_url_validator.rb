3fedd66bfcd6c2c60d0f1fe602541179f6fd6f3f
Christopher Milne <christopher.milne@gmail.com>
2014-12-04 23:43:36 -0500

    image url validator (not working)

--- lib/validators/image_url_validator.rb
+++ lib/validators/image_url_validator.rb
@@ -1,26 +1,14 @@
 class ImageUrlValidator < ActiveModel::Validator
+
+  IMAGE_HOST_WHITELIST = %w(bluemelon.com deviantart.com facebook.com flickr.com freeimagehosting.net imagebam.com imageshack.com imagevenue.com imgur.com instagram postimage.org photobucket.com picasa.com shutterfly.com slickpic.com snapfish.com smugmug.com tinypic.com twitpic.com webshots.com weheartit.com zenfolio.com)
+
   def validate(record)
-    validate_image_url(record) if record.image_url.present?
+    is_whitelisted?(record.image_url) if record.image_url.present?
   end
 
   private
 
-  def validate_image_url(record)
-    #image_url = record.image_url
-    #validates_format_of :image, :with => %r{\.(png|jpg|jpeg)$}i, :message => "whatever"
-
-    match = image_url.match(/^(?:https|http|)[:\/]*www\.#{}\/s\/projects\/(\d+)$/i)
-    # if match.present?
-    #   pv_id = match.captures[0]
-    # elsif url =~ /^\d+$/
-    #   pv_id = url
-    # end
-
-    # if pv_id.present?
-    #   # tidy up URL
-    #   record.pivotaltracker_url = "https://www.pivotaltracker.com/s/projects/#{pv_id}"
-    # else
-    #   record.errors[:pivotaltracker_url] << 'Invalid Pivotal Tracker URL'
-    # end
+  def is_whitelisted?(url)
+    IMAGE_HOST_WHITELIST.include? {|image_host| url.match(/#{image_host}/i) }
   end
 end
