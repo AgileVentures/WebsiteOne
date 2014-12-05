645591789e167c91c77e82e52fadc83451377dc8
Amy Lightholder <sidhene@gmail.com>
2014-12-04 17:54:11 -0800

    started developing a validator

---
+++ lib/validators/image_url_validator.rb
@@ -0,0 +1,26 @@
+class ImageUrlValidator < ActiveModel::Validator
+  def validate(record)
+    validate_image_url(record) if record.image_url.present?
+  end
+
+  private
+
+  def validate_pivotal_tracker_url(record)
+    image_url = record.image_url
+    validates_format_of :image, :with => %r{\.(png|jpg|jpeg)$}i, :message => "whatever"
+
+    match = url.match(/^(?:https|http|)[:\/]*www\.pivotaltracker\.com\/s\/projects\/(\d+)$/i)
+    if match.present?
+      pv_id = match.captures[0]
+    elsif url =~ /^\d+$/
+      pv_id = url
+    end
+
+    if pv_id.present?
+      # tidy up URL
+      record.pivotaltracker_url = "https://www.pivotaltracker.com/s/projects/#{pv_id}"
+    else
+      record.errors[:pivotaltracker_url] << 'Invalid Pivotal Tracker URL'
+    end
+  end
+end
