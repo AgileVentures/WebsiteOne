87e1d975413c75c2be3c564a85ef8428ca80db6e
Ryan Smith <rsmith23@binghamton.edu>
2014-12-11 13:10:26 -0400

    Started rewriting layout_helper_spec

--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -1,35 +1,40 @@
 require 'spec_helper'
 
-describe LayoutHelper do
+describe LayoutHelper, :type => :helper do
+
 	describe '#show_layout_flash?' do
-		it 'should return true if layout_flash is nil' do
-			@layout_flash = nil
-			expect(show_layout_flash?).to be_truthy
-		end
-		it 'should return layout_flash if layout_flash not nil' do
-			@layout_flash = "layout flash"
-			expect(show_layout_flash?).to eq(@layout_flash)
-		end
+    subject { show_layout_flash? }
+
+    context 'without a layout flash' do
+		  before { @layout_flash = nil }
+      it { is_expected.to be_truthy }
+    end
+    
+    context 'with a layout flash' do
+      before { @layout_flash = 'layout flash' }
+      it { is_expected.to be_truthy }
+    end
 	end
+
+# Not complete, need to rethink this whole portion
 	describe '#flash_messages' do
-		before do
-			flash[:notice] = 'notice flash'
-			flash[:error] = 'error flash'
-			@opts = {
-			:layout_flash => 'flash'
-				}
-		end
-		it 'should set @layout_flash from passed hash' do
-			flash_messages(@opts)
-			expect(@layout_flash).to eq(@opts[:layout_flash])
-		end
-		it 'should return div with flash' do
-			output = flash_messages(@opts)
-			flash.each do |name, msg| 
-				output.should have_css "div#flash_#{name}"
-				output.should have_text "#{msg}"
-			end
-			output.should have_css("div", :count => flash.to_hash().length)
-		end
+
+    context 'without :layout_flash set' do
+      let(:opts){ Hash.new }
+      before { flash_messages(opts) }
+
+      it 'sets @layout_flash to true' do
+        expect(@layout_flash).to be_truthy
+      end
+    end
+
+    context 'with :layout_flash set' do
+      let(:opts){ {:layout_flash => false} }
+      before { flash_messages(opts) }
+
+      it 'sets @layout_flash opts(:layout_flash)' do
+        expect(@layout_flash).to eq(opts[:layout_flash])
+      end
+    end
 	end
 end
