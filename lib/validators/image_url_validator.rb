13ab3a92bb7ebea098654e6cda18020d63bc199e
Amy Lightholder <sidhene@gmail.com>
2014-12-16 10:12:11 -0800

    made changes from pull request comments

--- lib/validators/image_url_validator.rb
+++ lib/validators/image_url_validator.rb
@@ -1,5 +1,5 @@
 class ImageUrlValidator < ActiveModel::Validator
-  #include HelperMethods
+
 
   IMAGE_HOST_WHITELIST = %w(bluemelon.com deviantart.com facebook.com flickr.com freeimagehosting.net imagebam.com imageshack.com imagevenue.com imgur.com instagram postimage.org photobucket.com picasa.com shutterfly.com slickpic.com snapfish.com smugmug.com tinypic.com twitpic.com webshots.com weheartit.com zenfolio.com)
 
