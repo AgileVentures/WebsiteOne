23714e0e0e6f07cc2514e1e0a9f129b0a1501170
rolatil1 <rolatil1@binghamton.edu>
2014-12-17 18:40:43 -0400

    added EuroAsiaScrum scrum title

--- app/controllers/scrums_controller.rb
+++ app/controllers/scrums_controller.rb
@@ -1,7 +1,7 @@
 class ScrumsController < ApplicationController
   def index
     client = YouTubeIt::Client.new(:dev_key => Youtube::YOUTUBE_KEY)
-    query = client.videos_by(:query => "AtlanticScrum|AmericasScrum|EuroScrum Pair Hookup", 
+    query = client.videos_by(:query => "AtlanticScrum|AmericasScrum|EuroScrum|EuroAsiaScrum Pair Hookup", 
                              :order_by => :published,
                              :max_results => 20)
     @scrums = query.videos.map { |video| YoutubeHelper.video_data(video) }
