07856dbf6f313a1f6f4887f674510bc7460fc12c
Ryan Smith <rsmith23@binghamton.edu>
2014-12-17 15:38:04 -0400

    Updated laout_helper_spec

--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -6,17 +6,16 @@ describe LayoutHelper, :type => :helper do
     subject { show_layout_flash? }
 
     context 'without a layout flash' do
-		  before { @layout_flash = nil }
+      before { @layout_flash = nil }
       it { is_expected.to be_truthy }
     end
-    
+
     context 'with a layout flash' do
       before { @layout_flash = 'layout flash' }
       it { is_expected.to be_truthy }
     end
   end
 
-# Not complete, need to rethink this whole portion
   describe '#flash_messages' do
 
     context 'without :layout_flash set' do
@@ -34,6 +33,19 @@ describe LayoutHelper, :type => :helper do
 
       it 'sets @layout_flash opts(:layout_flash)' do
         expect(@layout_flash).to eq(opts[:layout_flash])
+      end
+    end
+
+    context 'with or without :layout_flash set' do
+      before { flash[:notice] = 'notice_flash' }
+      let(:opts){ {:layout_flash => false} }
+      subject(:output) { flash_messages(opts) }
+
+      it 'will have flash message' do
+        expect(output).to contain(flash[:notice])
+      end
+      it 'will have div with id for each flash' do
+        expect(output).to have_selector("div#flash_notice")
       end
     end
   end
