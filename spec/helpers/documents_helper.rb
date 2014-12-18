af15e881d3f15d8f5eb70b39591b2de51d4e3368
ohashmi1 <ohashmi1@binghamton.edu>
2014-12-18 18:11:11 -0500

    Update documents_helper.rb

--- spec/helpers/documents_helper.rb
+++ spec/helpers/documents_helper.rb
@@ -6,12 +6,7 @@ require 'spec_helper'
   it '#shows metadata which includes data and the owner of document' do
      @document = mock_model(Document, user: "John", created_at: (Date.new(2015,1,1)))
      expect(document).to match / "Created #{time_ago_in_words(@document.created_at)} ago by #{@document.user.display_name}/
-
-  end
-end
-
- describe DocumentsHelper do
-
+     end
   it '#shows the date of a document' do
      @document = mock_model(Document, user: "John", created_at: (Date.new(2015,1,1)))
      expect(document).to match / "Created #{time_ago_in_words(@document.created_at)} ago/
