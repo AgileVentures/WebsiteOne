70bd35c5461ce7408d59646b778a1bd230fe10c1
Christopher Milne <christopher.milne@gmail.com>
2014-12-26 16:50:29 -0500

    event_instance feature: fix failing cucumber spec

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -9,12 +9,16 @@ Given(/^following events exist:$/) do |table|
 end
 
 Given(/^following events exist with active hangouts:$/) do |table|
+  project = Project.all.first
   table.hashes.each do |hash|
     event = Event.create!(hash)
     event.event_instances.create(hangout_url: 'x@x.com',
                           updated_at: 1.minute.ago,
                           category: event.category,
-                          title: event.name)
+                          title: event.name,
+                          project_id: project.id
+                                )
+
   end
 end
 
