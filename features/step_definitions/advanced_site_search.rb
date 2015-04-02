adfeaac565147cd1a0a87e9d2e04258d2d2c9283
Michelle Sausa <mnikki137@gmail.com>
2015-04-02 15:26:49 -0600

    Added scenario for site search.

---
+++ features/step_definitions/advanced_site_search.rb
@@ -0,0 +1,23 @@
+Then(/^I should see a see a Search Toggle button$/) do
+  expect(page).to have_selector("a#google_search")
+end
+
+Then(/^when I click the button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a search form should appear$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^when I search for 'WebsiteOne'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I click 'Submit'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the search results overlay$/) do
+  pending # express the regexp above with the code you wish you had
+end
