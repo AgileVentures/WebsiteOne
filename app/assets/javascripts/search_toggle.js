b9e3b150f7260b11364b251411736082e8b69a81
Raoul DIFFOUO <diraulo@gmail.com>
2015-04-06 22:40:51 +0200

    minor fix on search behavior
    Prevent search results overlay from closing when clicking
    on a pagination link

--- app/assets/javascripts/search_toggle.js
+++ app/assets/javascripts/search_toggle.js
@@ -1,14 +1,16 @@
-$(document).on('ready page:load', function() {
-    $('#google-search-dropdown').on('show.bs.dropdown', function() {
-        $('#google_search i').removeClass('fa-search').addClass('fa-times');
-        $('#nav').css('z-index', 'inherit')
-    });
-    $('#google-search-dropdown').on('shown.bs.dropdown', function() {
-        $("#gsc-i-id1").focus();
-    });
-    $('#google-search-dropdown').on('hidden.bs.dropdown', function() {
-        $('#google_search i').removeClass('fa-times').addClass('fa-search');
-        $('#nav').css('z-index', 0)
+$(document).on('ready page:load', function(e) {
+    $("#google_search").click(function() {
+        // Replace search icon with close
+        if($('#google_search_wrapper').css('display') == "none") {
+            $('#google_search i').removeClass('fa-search').addClass('fa-times');
+            $('#nav').css('z-index', 1000);
+        } else {
+            $('#google_search i').removeClass('fa-times').addClass('fa-search');
+        }
+
+        $('#google_search_wrapper').fadeToggle(200, function() {
+            $("#gsc-i-id1").focus();
+        });
     });
     return false;
 });
