d7e413d4e975e414565cddf81950b13f50d048f8
Raoul DIFFOUO <diraulo@gmail.com>
2015-04-10 01:25:11 +0200

    revisit advanced search feature tests
    Because of the timeout issue with other tests, we had to prevent the google search
    script from loading when in test env. We then can't test an actual search.
    We'll assume google won't fail us ;)

--- features/step_definitions/advanced_site_search.rb
+++ features/step_definitions/advanced_site_search.rb
@@ -1,21 +1,11 @@
+Then(/^I should( not)? see the search form$/) do |negative|
+  if negative
+    expect(page).to have_css '#google_search_wrapper', visible: false
+  else
+    expect(page).to have_css '#google_search_wrapper', visible: true
+  end
+end
+
 Then(/^I click the Search Toggle button$/) do
-  # expect(page).to have_selector("a#google_search")
-  page.find(:css, 'a#google_search', visible: true).click()
-end
-
-Then(/^a search form should appear$/) do
-  expect(page).to have_css '#google_search_wrapper', visible: true
-end
-
-Then(/^when I search for 'WebsiteOne'$/) do
-  # page.find('gsc-i-id1').set('WebsiteOne')
-  page.fill_in 'gsc-i-id1', :with => 'WebsiteOne'
-end
-
-Then(/^I click 'Submit'$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should see the search results overlay$/) do
-  pending # express the regexp above with the code you wish you had
+  page.find(:css, 'a#google_search', visible: true).click
 end
