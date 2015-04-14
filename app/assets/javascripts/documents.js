f472193d9cf9f1e808855831cd04c3366e2368ce
Raoul DIFFOUO <diraulo@gmail.com>
2015-04-14 17:45:08 +0200

    Fix revisions toggle link on about page
    There was a duplicate script block at the bottom of the static_pages view. Thus causing a
    conflict between the global defined script performing exacting the same action.

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,16 +1,8 @@
-WebsiteOne.define('Documents', function(){
-  function init(){
-    //alert('this shit is on');
-    $('#revisions-anchor').on('click', function(e){
-      e.preventDefault();
-      //$('#revisions').css('display', 'block');
-      $('#revisions').slideToggle('slow');
-      $("#arrow").toggleClass("fa-arrow-up").toggleClass("fa-arrow-down");
-      //alert('ooh, it\'s ooon!');
-    });
-  }
-
-  return {
-    init: init
-  }
+$(document).ready(function() {
+  $('#revisions-anchor').on('click', function(e){
+    e.preventDefault();
+    $('#revisions').slideToggle('slow');
+    $("#arrow").toggleClass("fa-arrow-up").toggleClass("fa-arrow-down");
+  });
+  return false;
 });
