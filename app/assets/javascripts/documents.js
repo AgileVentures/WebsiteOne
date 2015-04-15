48c46357e9098633c41f9baa4ddda49454c6774b
Raoul DIFFOUO <diraulo@gmail.com>
2015-04-15 13:34:01 +0200

    wrap documents actions in custom JS module define

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
-  $('#revisions-anchor').on('click', function(e){
-    e.preventDefault();
-    $('#revisions').slideToggle('slow');
-    $("#arrow").toggleClass("fa-arrow-up").toggleClass("fa-arrow-down");
-  });
-  return false;
+WebsiteOne.define('Documents', function(){
+  function init(){
+    $('#revisions-anchor').on('click', function(e){
+      e.preventDefault();
+      $('#revisions').slideToggle('slow');
+      $("#arrow").toggleClass("fa-arrow-up").toggleClass("fa-arrow-down");
+    });
+  }
+
+  return {
+    init: init
+  };
 });
