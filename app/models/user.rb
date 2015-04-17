d5a6de0e8acd81262f1499a845af406d2beef85f
Alejandro Babio <alejandro.e.babio@hotmail.com>
2015-04-17 19:03:10 -0300

    Refactor on User#incomplete? using blank? instead of nil? || empty?

--- app/models/user.rb
+++ app/models/user.rb
@@ -127,8 +127,7 @@ class User < ActiveRecord::Base
   end
 
   def incomplete?
-    bio.nil? || bio.empty? || skills.empty? || first_name.nil? ||
-      first_name.empty? || last_name.nil? || last_name.empty?
+    bio.blank? || skills.blank? || first_name.blank? || last_name.blank?
   end
 
   private
