c9d26915d95e49609107d2d5198db4661b60f2a9
tochman <thomas@nocebo.se>
2015-06-17 10:27:19 +0200

    fix for right YT id

--- app/helpers/scrums_helper.rb
+++ app/helpers/scrums_helper.rb
@@ -2,7 +2,7 @@ module ScrumsHelper
   def scrum_link(video)
     link_to(
       raw('<i class="fa fa-bg fa-play"></i>') + video[:title],
-      video[:hangout_url], {
+      video[:yt_video_id], {
         id: video[:id],
         class: 'scrum_yt_link',
         data: { content: video[:title], toggle: 'modal', target: '#scrumVideo' }
