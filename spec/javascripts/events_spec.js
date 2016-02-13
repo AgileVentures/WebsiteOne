27e22b84f0a19e2b81d63cf26055933142ae2c53
tochman <thomas@nocebo.se>
2016-02-13 11:05:28 +0100

    added js function to toggle menu item text and visibility, WIP jasmine tests

--- spec/javascripts/events_spec.js
+++ spec/javascripts/events_spec.js
@@ -1,7 +1,7 @@
-  describe('RefreshEventHangout', function() {
-    beforeEach(function() {
-      this.app = new window.EventsUtils();
-      setFixtures(sandbox({ id: 'hg-management' }));
+describe('RefreshEventHangout', function () {
+    beforeEach(function () {
+        this.app = new window.EventsUtils();
+        setFixtures(sandbox({id: 'hg-management'}));
     });
 
     /**it('calls refresh every 10 sec', function() {
@@ -11,19 +11,37 @@
       expect(window.setInterval).toHaveBeenCalledWith(this.app.ajaxRequest, 10000);
     });*/
 
-    it('replaces hg-management section', function() {
-      spyOn(WebsiteOne, 'renderHangoutButton');
-      spyOn(window, 'clearInterval');
-      this.app.updateHangoutsData('hangouts-details-well');
-      expect(window.clearInterval).toHaveBeenCalledWith(this.app.intervalId);
-      expect($('#hg-management').text()).toEqual('hangouts-details-well');
-      expect(WebsiteOne.renderHangoutButton).toHaveBeenCalled();
+    it('replaces hg-management section', function () {
+        spyOn(WebsiteOne, 'renderHangoutButton');
+        spyOn(window, 'clearInterval');
+        this.app.updateHangoutsData('hangouts-details-well');
+        expect(window.clearInterval).toHaveBeenCalledWith(this.app.intervalId);
+        expect($('#hg-management').text()).toEqual('hangouts-details-well');
+        expect(WebsiteOne.renderHangoutButton).toHaveBeenCalled();
     });
 
-    it('clears refresh repeat if href changes', function() {
-      spyOn(window, 'clearInterval');
-      this.app.href = 'new_href';
-      this.app.ajaxRequest();
-      expect(window.clearInterval).toHaveBeenCalledWith(this.app.intervalId);
+    it('clears refresh repeat if href changes', function () {
+        spyOn(window, 'clearInterval');
+        this.app.href = 'new_href';
+        this.app.ajaxRequest();
+        expect(window.clearInterval).toHaveBeenCalledWith(this.app.intervalId);
     });
-  });
+
+    //We are not loading the right fixture so these two test will not pass. Blocked by not knowing this files structure
+    xit('toggles Edit HOA URL menu item test', function () {
+        var element = $("li[role='edit_hoa_link']");
+        element.trigger('click');
+        var link = element.children("a");
+        expect(link).toHaveText('Hide edit controls');
+    });
+
+    xit('toggles Edit HOA URL menu item test', function () {
+        $("#edit-link-form").collapse('show');
+        $('#hoa_link_cancel').trigger('click');
+        var link = $("li[role='edit_hoa_link']").children("a");
+        expect(link).toHaveText('Edit hangout link');
+        expect($("edit-link-form")).toHaveClass('in');
+    });
+
+
+});
