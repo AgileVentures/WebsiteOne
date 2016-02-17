10aa69afdff7bfbb0bf85445a100c53e4ff56289
tochman <thomas@nocebo.se>
2016-02-17 21:57:25 +0100

    refactored events helper methods for time range and added helper specs

--- spec/helpers/event_helper_spec.rb
+++ spec/helpers/event_helper_spec.rb
@@ -14,7 +14,7 @@ describe EventHelper do
   end
 
   it 'extract event ocurrence from hash' do
-    ocurrence = Time.utc(2014,"mar",9,23,0,0)
+    ocurrence = Time.utc(2014, "mar", 9, 23, 0, 0)
     time = IceCube::Occurrence.new(ocurrence)
     event = mock_model(Event, name: 'DailyScrum', category: 'Scrum')
     nested_value = {:event => event, :time => time}
@@ -23,8 +23,64 @@ describe EventHelper do
   end
 
   it 'should wrap the current event time with a time tag when calling format_local_time' do
-    datetime = Time.utc(2014,"mar",9,23,0,0)
+    datetime = Time.utc(2014, "mar", 9, 23, 0, 0)
     result = helper.format_local_time(datetime)
     expect(result).to match /<time data-format="%l:%M %p \(%Z\)" data-local="time" datetime="2014-03-09T23:00:00Z">11:00 PM \(UTC\)<\/time>/
+  end
+
+  describe 'start prefix' do
+
+    it 'returns \'Started at\' for past time' do
+      time = Time.now - 4.minutes
+      expect(helper.start_prefix(time)).to eql 'Started at '
+    end
+
+    it 'returns \'Start at\' for future time' do
+      time = Time.now + 4.minutes
+      expect(helper.start_prefix(time)).to eql 'Starts at '
+    end
+
+  end
+
+  describe 'end prefix' do
+
+    it 'returns \'Ended at\' for past time' do
+      time = Time.now - 4.minutes
+      expect(helper.end_prefix(time)).to eql 'Ended at '
+    end
+
+    it 'returns \'Ends at\' for future time' do
+      time = Time.now + 4.minutes
+      expect(helper.end_prefix(time)).to eql 'Ends at '
+    end
+
+  end
+
+  describe 'time ranges' do
+    let(:current_time) { Time.utc(2016, 2, 9, 19, 0, 0) }
+    let(:event) { double(:event, start_time: current_time, instance_end_time: current_time + 15.minutes) }
+
+    before { Delorean.time_travel_to(current_time - 15.minutes) }
+
+    after { Delorean.back_to_the_present }
+
+    it 'returns a UTC time range' do
+      expected_result = 'Starts at 19:00  -  Ends at 19:15 (UTC)'
+      actual_result = helper.show_time_range(event)
+      expect(expected_result).to eql actual_result
+    end
+
+    it 'returns a local time range' do
+      expected_result = \
+      "Starts at <time data-format=\"%H:%M\" data-local=\"time\""\
+      " datetime=\"2016-02-09T19:00:00Z\">19:00</time> - Ends at "\
+      "<time data-format=\"%H:%M\" data-local=\"time\" "\
+      "datetime=\"2016-02-09T19:15:00Z\">19:15</time> "\
+      "<time data-format=\"(%Z)\" data-local=\"time\" "\
+      "datetime=\"2016-02-09T19:00:00Z\">(UTC)</time>"
+
+      actual_result = helper.show_local_time_range(event)
+      expect(expected_result).to eql actual_result
+    end
   end
 end
