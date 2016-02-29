585ec57034afe7eab0c43a2857f79d57859acd74
Tony Butler <tony@Tonys-MacBook-Pro.local>
2016-02-29 08:00:21 +0000

    Remove constant already defined warnings

--- spec/jobs/send_newsletter_spec.rb
+++ spec/jobs/send_newsletter_spec.rb
@@ -4,14 +4,14 @@ describe SendNewsletter do
 
   describe 'processes' do
     before :each do
-      Newsletter.const_set('CHUNK_SIZE', 5 ) # by default at 180 - but too much overhead in specs
+      Newsletter.redefine_without_warning('CHUNK_SIZE', 5 ) # by default at 180 - but too much overhead in specs
       # make sure it takes 2 runs - a.k.a more user than CHUNK_SIZE
       FactoryGirl.create_list(:user, Newsletter::CHUNK_SIZE + Newsletter::CHUNK_SIZE/2)
       @newsletter = FactoryGirl.create(:newsletter, do_send: true)
     end
 
     after :all do
-      Newsletter.const_set('CHUNK_SIZE', 180 ) # reset to default
+      Newsletter.redefine_without_warning('CHUNK_SIZE', 180 ) # reset to default
     end
 
     it 'specified chunk-size' do
