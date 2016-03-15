6d441ddb4c3df36d513b2baa31c241cd2334e77e
Sam Joseph <tansaku@gmail.com>
2016-03-15 13:09:58 +0000

    makes minor name refactoring and adds jasmine test stub

---
+++ spec/javascripts/event_datepicker_spec.js
@@ -0,0 +1,11 @@
+describe('eventDatePicker', function(){
+    beforeEach(function(){
+        // concerned that fixture is large and involved and creating too complex a seam
+        setFixtures('');
+    });
+
+    xit('hides event repeat options by default').pend('awaiting refactor of related form for use in fixture');
+    xit('shows event repeat options when event is set to repeat').pend('awaiting refactor of related form for use in fixture');
+    xit('shows event repeat optional ending when event is set to end').pend('awaiting refactor of related form for use in fixture');
+    xit('hides event repeat optional ending when event is set to never end').pend('awaiting refactor of related form for use in fixture');
+});
