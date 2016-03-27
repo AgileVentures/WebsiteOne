c9ef59161e10398aa48a72168caf61a30ba0b09d
Raoul DIFFOUO <diraulo@gmail.com>
2016-03-28 01:07:56 +0200

    fix failing scrums features

--- features/step_definitions/scrums_steps.rb
+++ features/step_definitions/scrums_steps.rb
@@ -2,7 +2,7 @@ Then(/^I should see 20 scrums in descending order by published date:$/) do
   dates = page.text.scan(/\d{4}-\d{2}-\d{2}/)
   clocks = page.all(:css, '.fa-clock-o')
   expect(clocks.count).to eq(20)
-  expect(dates.sort { |x,y| y <=> x }).to eq(dates)
+  expect(dates.sort { |x, y| y <=> x }).to eq(dates)
 end
 
 Given(/^I play a video$/) do
@@ -11,33 +11,27 @@ Given(/^I play a video$/) do
 end
 
 Then(/^I should see a modal window with the (first|second) scrum$/) do |ord|
-  ord_hash ={"first" => 0, "second" => 1}
-  expect(page.find("#scrumVideo")[:style]).to include("display: block;")
-  title = page.body.gsub(/\n/,'').scan(/<\/i>\s*(.*?)\s*<\/a>/)[ord_hash[ord]]
-  expect(page).to have_selector("#playerTitle", text: title[1])
+  ord_hash = { 'first' => 0, 'second' => 1 }
+  expect(page.find('#scrumVideo')[:style]).to include('display: block;')
+  title = page.body.delete("\n").scan(%r{<\/i>\s*(.*?)\s*<\/a>})[ord_hash[ord]]
+  expect(page).to have_selector('#playerTitle', text: title[1])
 end
 
 Then(/^I should not see a modal window$/) do
-  expect(page.evaluate_script("$('.modal').css('display')")).to eq "none"
+  expect(page.evaluate_script("$('.modal').css('display')")).to eq 'none'
 end
 
-When(/^I click the first scrum in the timeline$/) do
-  # binding.pry
-  title = page.body.gsub(/\n/,'').scan(/<\/i>\s*(.*?)\s*<\/a>/)[0]
-  vid = page.body.gsub(/\n/,'').scan(/<a class=\"scrum_yt_link.*?id=\"(.*?)"/).flatten
-  page.find(:xpath, "//a[@id=\"#{vid[0]}\"]").click
-end
-
-When(/^I click the second scrum in the timeline$/) do
-  vid = page.body.gsub(/\n/,'').scan(/<a class=\"scrum_yt_link.*?id=\"(.*?)"/).flatten
-  page.find(:xpath, "//a[@id=\"#{vid[1]}\"]").trigger('click')
-  title = page.body.gsub(/\n/,'').scan(/<\/i>\s*(.*?)\s*<\/a>/)[1]
+When(/^I click the (first|second) scrum in the timeline$/) do |order|
+  order_hash = { 'first' => 0, 'second' => 1 }
+  vid = page.body.delete("\n")
+            .scan(/<a id=\"(\d*?)\" class=\"scrum_yt_link"/).flatten
+  page.find(:xpath, "//a[@id=\"#{vid[order_hash[order]]}\"]").click
 end
 
 When(/^I close the modal$/) do
-  page.find(:css,'.close').click
+  page.find(:css, '.close').click
 end
 
 Then(/^I should see a modal$/) do
-  expect(page.find("#myModal")[:style]).to eq("display: block; ")
+  expect(page.find('#myModal')[:style]).to eq('display: block; ')
 end
