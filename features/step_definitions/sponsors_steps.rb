260842610abdd52f982219487bcc518298939730
Sam Joseph <tansaku@gmail.com>
2016-04-07 15:24:21 +0100

    removes all cukes to start re-adding them one by one

--- features/step_definitions/sponsors_steps.rb
+++
@@ -1,4 +0,0 @@
-Then(/^I should see sponsor banner for "(.*?)"$/) do |supporter_name|
-  expect(page).to have_selector('div#sponsorsBar')
-  expect(page).to have_css("img[alt*='#{supporter_name}']")
-end
