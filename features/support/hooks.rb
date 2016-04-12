78da13a6eddac78cd034f159a0c2ffe8075864f5
Sam Joseph <tansaku@gmail.com>
2016-04-12 16:53:54 +0100

    adds back in event countdown feature

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,3 +6,18 @@ Before('@smartphone') { page.driver.resize(640, 640) }
 
 After('@desktop', '@tablet', '@smartphone') { page.driver.resize(1600, 1200) }
 
+Before('@time-travel') do
+  @default_tz = ENV['TZ']
+  ENV['TZ'] = 'UTC'
+  Delorean.time_travel_to(Time.parse('2014/02/01 09:15:00 UTC'))
+end
+
+Before('@time-travel-step') do
+  @default_tz = ENV['TZ']
+  ENV['TZ'] = 'UTC'
+end
+
+After('@time-travel , @time-travel-step') do
+  Delorean.back_to_the_present
+  ENV['TZ'] = @default_tz
+end
