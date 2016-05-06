22e117bb0483539419fff74bb59f1c699fa6811b
Raoul DIFFOUO <diraulo@gmail.com>
2016-05-06 21:10:54 +0200

    1026 update airbrake config (#1027)
    * Update airbrake configuration file
    * suppress assets pre-compile deprecation warning
    * airbrake should only be enabled for production
    * clear out some comments

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,19 @@
 if Rails.env.production?
-  Airbrake.configure do |config|
-    config.api_key = ENV.fetch('AIRBRAKE_API_KEY')
+  Airbrake.configure do |c|
+    c.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    c.project_key = ENV['AIRBRAKE_API_KEY']
+
+    c.root_directory = Rails.root
+    c.logger = Rails.logger
+
+    c.environment = Rails.env
+    c.ignore_environments = %w(test)
+
+    c.blacklist_keys = [/password/i]
   end
 end
+
+# If Airbrake doesn't send any expected exceptions, we suggest to uncomment the
+# line below. It might simplify debugging of background Airbrake workers, which
+# can silently die.
+# Thread.abort_on_exception = ['test', 'development'].include?(Rails.env)
