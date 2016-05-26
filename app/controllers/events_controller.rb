afca734bbe1bc5c8ab3900a72b5882522dff3d2d
Sam Joseph <tansaku@gmail.com>
2016-05-26 17:26:22 +0100

    refactors further to reduce method length

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -63,14 +63,18 @@ class EventsController < ApplicationController
 
   def transform_params
     event_params = params.require(:event).permit!
+    create_start_date_time(event_params)
+    event_params[:repeat_ends] = (event_params['repeat_ends_string'] == 'on')
+    event_params[:repeat_ends_on]= params[:repeat_ends_on].present? ? "#{params[:repeat_ends_on]} UTC" : ""
+    event_params
+  end
+
+  def create_start_date_time(event_params)
     if (params['start_date'].present? && params['start_time'].present?)
       Time.zone = params["start_time_tz"]["time_zone"]
       event_params[:start_datetime] = Time.zone.parse(params["start_date"]+" " + params["start_time"]).utc
       Time.zone = "UTC"
     end
-    event_params[:repeat_ends] = (event_params['repeat_ends_string'] == 'on')
-    event_params[:repeat_ends_on]= params[:repeat_ends_on].present? ? "#{params[:repeat_ends_on]} UTC" : ""
-    event_params
   end
 
   def specified_project
