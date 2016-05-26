b5bf0ef745b3bd1f7fd4cd8c5b96f4707e2314e7
Sam Joseph <tansaku@gmail.com>
2016-05-26 16:52:10 +0100

    adjust timezone script to select based on daylight savings

--- app/assets/javascripts/timezone_select.js
+++ app/assets/javascripts/timezone_select.js
@@ -1,20 +1,10 @@
 jQuery.fn.selectTimeZone = function() {
 
     var $el = $(this[0]); // our element
-    // var offsetFromGMT = String(- new Date('1/1/2009').getTimezoneOffset() / 60); // using 1/1/2009 so we know DST isn't tripping us up
 
-    year = new Date().getFullYear();
-    var jan = new Date(year, 0, 1);
-    var jul = new Date(year, 6, 1);
-    var offsetFromGMT = String(-(Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset())/60));
+    var date = new Date();
+    var offsetFromGMT = /\+\d\d/.exec(date)[0];
 
-
-    if (offsetFromGMT[0] != '-') {
-        offsetFromGMT = '+' + offsetFromGMT; // if it's not negative, prepend a +
-    }
-    if (offsetFromGMT.length < 3) {
-        offsetFromGMT = offsetFromGMT.substr(0, 1) + '0' + offsetFromGMT.substr(1); // add a leading zero if we need it
-    }
     offsetFromGMT = '\\' + offsetFromGMT;
 
     var regEx = new RegExp(offsetFromGMT); // create a RegExp object with our pattern
