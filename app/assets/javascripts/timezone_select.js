0b8624290f359a797eac4d2c44bb0131e22a6066
mtc2013 <mtc2013opensource@gmail.com>
2016-05-27 11:29:38 -0400

    Spikey changing of date and time to local time automatically

--- app/assets/javascripts/timezone_select.js
+++ app/assets/javascripts/timezone_select.js
@@ -1,4 +1,25 @@
-jQuery.fn.selectTimeZone = function() {
+normalize_date_time = function(date, time) {
+    date_matches = date.match(/(\d\d\d\d)-(\d\d)-(\d\d)/);
+    var year = date_matches[1];
+    var day = date_matches[2];
+    var month = date_matches[3];
+    time_matches = time.match(/(\d\d):(\d\d) (PM|AM)/);
+    var minutes = time_matches[2];
+    var hour = time_matches[1];
+    if (time_matches[3] == "PM") {
+        hour = parseInt(hour) + 12 + "";
+    }
+    normalized_date = new Date()
+    normalized_date.setUTCFullYear(year);
+    normalized_date.setUTCDate(day);
+    normalized_date.setUTCMonth(month);
+    normalized_date.setUTCHours(hour);
+    normalized_date.setUTCMinutes(minutes);
+    normalized_date.setUTCSeconds(0);
+    return normalized_date;
+}
+
+jQuery.fn.selectTimeZone = function(normalizer) {
 
     var $el = $(this[0]); // our element
 
@@ -13,8 +34,31 @@ jQuery.fn.selectTimeZone = function() {
             return false; // stop the loop—we're all done here
         }
     });
+    normalized_date_time = normalizer($('#start_date').val(), $('#start_time').val());
+
+    local_day = normalized_date.getDate();
+    local_day = (local_day < 10)? "0"+local_day:""+local_day;
+    local_month = normalized_date.getMonth();
+    local_month = (local_month<10)?"0"+local_month:""+local_month;
+    local_date = normalized_date_time.getFullYear()+ "-"+local_day+"-"+local_month;
+
+    local_hours = normalized_date_time.getHours() % 12;
+    local_hours = (local_hours < 10) ? ("0"+local_hours):""+local_hours;
+
+    local_pm = " AM";
+    if(normalized_date_time.getHours()>11){
+        local_pm = " PM";
+    }
+
+    local_minutes = normalized_date_time.getMinutes();
+    local_minutes = (local_minutes<10)?("0"+local_minutes):""+local_minutes;
+
+    local_time = local_hours+":"+local_minutes+local_pm;
+
+    $('#start_date').val(local_date);
+    $('#start_time').val(local_time);
 }
 
 $(document).on('ready page:load', function () {
-    $('#start_time_tz').selectTimeZone();
+    $('#start_time_tz').selectTimeZone(normalize_date_time);
 });
