98374d73af8bd897684d4168e7f16df8b2cdbcf9
Sam Joseph <tansaku@gmail.com>
2016-06-06 15:06:51 +0100

    adjusts event controller to take account of next event date when working out timezone during event save

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,10 +69,16 @@ class EventsController < ApplicationController
     event_params
   end
 
+  # next_date and start_date are in the same dst or non-dst
+  # next_date in dst and start_date in non-dst ==> get an hour back
+  # next_date in non-dst and start_date in dst ==> ???
+
   def create_start_date_time(event_params)
     return unless date_and_time_present?
     tz = TZInfo::Timezone.get(params['start_time_tz'])
-    event_params[:start_datetime] = tz.local_to_utc(DateTime.parse(params['start_date']+ ' ' + params['start_time']))
+    next_date_time = DateTime.parse(params['next_date'] + ' ' + params['start_time'])
+    next_date_offset = tz.period_for_utc(next_date_time).offset
+    event_params[:start_datetime] = next_date_offset.to_utc(DateTime.parse(params['start_date']+ ' ' + params['start_time']))
   end
 
   def date_and_time_present?
