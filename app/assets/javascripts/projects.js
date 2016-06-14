0a8a3edf7b0b82cb7e6713aa91369fe63ee68896
SuperManEver <nikita.luparev@gmail.com>
2016-06-15 01:42:00 +0500

    slightly rearranged js code for projects

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,21 +1,22 @@
 WebsiteOne.define('Projects', function() {
+  var setHash = function(newHash) {
+    window.location.hash = newHash;
+  };
+
+  var init = function() {
+    var hash = window.location.hash;
+
+    if (hash) { 
+      $('a[data-hash="'+hash.replace('#','')+'"]').tab('show');
+    } 
+
+    $('.nav-tabs a').click(function (e) {
+      setHash($(this).data('hash'));
+      $(this).tab('show');
+    });
+  };
+
   return {
-    init: function() {
-
-      var hash = window.location.hash;
-
-      if (hash) { 
-        $('a[data-hash="'+hash.replace('#','')+'"]').tab('show');
-      } 
-
-      $('.nav-tabs a').click(function (e) {
-        setHash($(this).data('hash'));
-        $(this).tab('show');
-      });
-
-      var setHash = function(newHash) {
-        window.location.hash = newHash;
-      };
-    }
-  }
+    init: init
+  };
 });
