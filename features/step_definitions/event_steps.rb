bccea5bb227370d9f24bf18a2b14c6cd27dd3278
mtc2013 <mtc2013opensource@gmail.com>
2016-06-27 11:34:30 -0400

    Failing test

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -292,7 +292,8 @@ Given(/^the event "([^"]*)"$/) do |name|
 end
 
 Then(/^they should see the icon of the creator of the event$/) do
-  expect(page).to have_xpath("//img[contains(@src, 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?s=80&d=retro')]")
+  byebug
+  expect(page).to have_xpath("//a[@href='#{user_path(@event.creator)}']/img[contains(@src, 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?s=80&d=retro')]")
 end
 
 Then(/^they should see a link to the creator of the event$/) do
