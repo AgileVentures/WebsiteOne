7475e46033b47d57e15cba7ab54478adbd7a4f52
Sam Joseph <tansaku@gmail.com>
2016-07-12 15:29:37 +0100

    sort by karma and add icon (#1212)
    * sort by karma and add icon
    
    * beefs up karma to include starting hangouts that at least one other person joined and github commits and authentication hookups

--- app/services/karma_calculator.rb
+++ app/services/karma_calculator.rb
@@ -10,13 +10,14 @@ class KarmaCalculator
     user.karma_points = 0
     return if user.created_at.blank? || user_age_in_months < 1
 
-    user.karma_points = membership_length + profile_completeness + activity
+    user.karma_points = membership_length + profile_completeness + activity + number_pairing_sessions + number_github_contributions
+    # better to have time in pairing sessions, code contributed (related to quality), issues, ...
   end
 
   private
 
   def membership_length # 6
-    1 * [ user_age_in_months.to_i, 6 ].min
+    1 * [user_age_in_months.to_i, 6].min
   end
 
   def profile_completeness # 10
@@ -27,11 +28,20 @@ class KarmaCalculator
     awarded += 2 if user.bio.present?
     awarded += 1 if user.first_name.present?
     awarded += 1 if user.last_name.present?
+    awarded += user.authentications.count * 100
     return awarded
   end
 
+  def number_github_contributions
+    user.commit_counts.inject(0){|memo,c| memo += c.commit_count}
+  end
+
+  def number_pairing_sessions
+    user.event_instances.select { |h| h.participants != nil && h.participants.count > 1 }.count
+  end
+
   def activity # 6
-    2*[ [ (user.sign_in_count - 2), 0 ].max, 3 ].min
+    2*[[(user.sign_in_count - 2), 0].max, 3].min
   end
 
   def user_age_in_months
