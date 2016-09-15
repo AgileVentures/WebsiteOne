da592c7d938a3b5299f2ee016f5620c6572c0d94
Sam Joseph <tansaku@gmail.com>
2016-09-15 16:27:57 +0100

    refactors a little

--- app/jobs/slack_invite_job.rb
+++ app/jobs/slack_invite_job.rb
@@ -4,17 +4,17 @@ class SlackInviteJob
   def perform(email)
     uri = URI.parse 'https://agileventures.slack.com/api/users.admin.invite'
     response = Net::HTTP.post_form(uri, {
-      email: email,
-      channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
-      token: Slack::AUTH_TOKEN
+        email: email,
+        channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
+        token: Slack::AUTH_TOKEN
     })
     error = nil
     json_response = JSON.parse response.body
-    rescue => e
-      error = e
-    ensure
-      unless json_response.present? and json_response['ok']
-        AdminMailer.failed_to_invite_user_to_slack(email,error)
-      end
+  rescue => e
+    error = e
+  ensure
+    unless json_response.present? and json_response['ok']
+      AdminMailer.failed_to_invite_user_to_slack(email, error)
+    end
   end
 end
--- spec/jobs/slack_invite_job_spec.rb
+++ spec/jobs/slack_invite_job_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe SlackInviteJob do
   subject { SlackInviteJob.new }
+  let(:email) { 'random@random.com' }
+  before { stub_const('Slack::AUTH_TOKEN', 'test') }
 
   it 'sends a post request to invite the user to slack and returns the proper response' do
     stub_request(:post, 'https://agileventures.slack.com/api/users.admin.invite').
         to_return(body: '{"ok": true}')
-    email = 'random@random.com'
-    stub_const('Slack::AUTH_TOKEN', 'test')
+
 
     response = subject.perform(email)
-    
+
     expect(response['ok']).to be_truthy
     assert_requested(:post, 'https://agileventures.slack.com/api/users.admin.invite', times: 1) do |req|
       expect(req.body).to eq "email=#{CGI.escape email}&channels=#{CGI.escape 'C02G8J689,C0285CSUH,C02AA0ARR'}&token=test"
@@ -18,27 +19,21 @@ describe SlackInviteJob do
   end
 
   it 'when attempt errors we should notify admin' do
-    email = 'random@random.com'
     error = StandardError.new 'boom!'
     expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email, error)
 
     stub_request(:post, 'https://agileventures.slack.com/api/users.admin.invite').
         and_raise(error)
-    stub_const('Slack::AUTH_TOKEN', 'test')
 
     response = subject.perform(email)
-
   end
 
   it 'when slack response not ok should notify admin' do
-    email = 'random@random.com'
-    expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email,nil)
+    expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email, nil)
 
     stub_request(:post, 'https://agileventures.slack.com/api/users.admin.invite').
         to_return(body: '{"ok": false}')
-    stub_const('Slack::AUTH_TOKEN', 'test')
 
     response = subject.perform(email)
-
   end
 end
