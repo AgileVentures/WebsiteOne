c33bfcfb2e8dca68097bc34847e5cf17fdc82fe9
Sam Joseph <tansaku@gmail.com>
2016-09-15 16:45:56 +0100

    spikes an admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,11 @@
 class AdminMailer < ApplicationMailer
+  default from: "bot@agileventures.org"
+
+  def failed_to_invite_user_to_slack(email, error)
+    @error = error
+    @email = email
+    mail(subject: "Slack invite failed for user with email '#{email}'.",
+         to: "support@agileventures.org")
+  end
+
 end
