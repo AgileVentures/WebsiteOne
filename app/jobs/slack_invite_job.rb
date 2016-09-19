94fdf4ee4f5ae8befd82572870b6569629e24e96
Sam Joseph <tansaku@gmail.com>
2016-09-19 15:16:20 +0100

    adds test to check admin mail is delivered

--- app/jobs/slack_invite_job.rb
+++ app/jobs/slack_invite_job.rb
@@ -1,8 +1,10 @@
 class SlackInviteJob
   include SuckerPunch::Job
 
+  SLACK_INVITE_URL = 'https://agileventures.slack.com/api/users.admin.invite'
+
   def perform(email)
-    uri = URI.parse 'https://agileventures.slack.com/api/users.admin.invite'
+    uri = URI.parse SLACK_INVITE_URL
     response = Net::HTTP.post_form(uri, {
         email: email,
         channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
@@ -14,7 +16,7 @@ class SlackInviteJob
     error = e
   ensure
     unless json_response.present? and json_response['ok']
-      AdminMailer.failed_to_invite_user_to_slack(email, error)
+      AdminMailer.failed_to_invite_user_to_slack(email, error).deliver_later
     end
   end
 end
--- spec/jobs/slack_invite_job_spec.rb
+++ spec/jobs/slack_invite_job_spec.rb
@@ -1,39 +1,60 @@
 require 'spec_helper'
 
+
+
 describe SlackInviteJob do
   subject { SlackInviteJob.new }
   let(:email) { 'random@random.com' }
   before { stub_const('Slack::AUTH_TOKEN', 'test') }
 
   it 'sends a post request to invite the user to slack and returns the proper response' do
-    stub_request(:post, 'https://agileventures.slack.com/api/users.admin.invite').
+    stub_request(:post, SlackInviteJob::SLACK_INVITE_URL).
         to_return(body: '{"ok": true}')
 
 
     response = subject.perform(email)
 
     expect(response['ok']).to be_truthy
-    assert_requested(:post, 'https://agileventures.slack.com/api/users.admin.invite', times: 1) do |req|
+    assert_requested(:post, SlackInviteJob::SLACK_INVITE_URL, times: 1) do |req|
       expect(req.body).to eq "email=#{CGI.escape email}&channels=#{CGI.escape 'C02G8J689,C0285CSUH,C02AA0ARR'}&token=test"
     end
   end
 
-  it 'when attempt errors we should notify admin' do
-    error = StandardError.new 'boom!'
-    expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email, error)
+  context 'when invite attempt fails notify admin' do
+    let(:error) { StandardError.new 'boom!' }
 
-    stub_request(:post, 'https://agileventures.slack.com/api/users.admin.invite').
-        and_raise(error)
+    before do
+      stub_request(:post, SlackInviteJob::SLACK_INVITE_URL).and_raise(error)
+    end
 
-    response = subject.perform(email)
+    it 'sends the email later' do
+      expect(AdminMailer).to receive_message_chain(:failed_to_invite_user_to_slack, :deliver_later)
+      subject.perform(email)
+    end
+
+    it 'includes information about user and error' do
+      allow(AdminMailer).to receive_message_chain(:failed_to_invite_user_to_slack, :deliver_later)
+      expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email, error)
+      subject.perform(email)
+    end
   end
 
-  it 'when slack response not ok should notify admin' do
-    expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email, nil)
 
-    stub_request(:post, 'https://agileventures.slack.com/api/users.admin.invite').
-        to_return(body: '{"ok": false}')
+  context 'when slack response not ok should notify admin' do
+    before do
+      stub_request(:post, SlackInviteJob::SLACK_INVITE_URL)
+          .to_return(body: '{"ok": false}')
+    end
 
-    response = subject.perform(email)
+    it 'sends the email later' do
+      expect(AdminMailer).to receive_message_chain(:failed_to_invite_user_to_slack, :deliver_later)
+      subject.perform(email)
+    end
+
+    it 'includes information about user and error' do
+      allow(AdminMailer).to receive_message_chain(:failed_to_invite_user_to_slack, :deliver_later)
+      expect(AdminMailer).to receive(:failed_to_invite_user_to_slack).with(email, nil)
+      subject.perform(email)
+    end
   end
 end
