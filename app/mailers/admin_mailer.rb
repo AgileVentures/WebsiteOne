dc4ad14665862da5b5bd2233d8162e2a5f6275e6
Sam Joseph <tansaku@gmail.com>
2016-09-19 16:03:06 +0100

    unit test for admin mailer and more robust display

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,10 +3,18 @@ class AdminMailer < ApplicationMailer
 
   def failed_to_invite_user_to_slack(email, error, slack_error_message)
     @error = error
+    @backtrace_output = backtrace_output
     @email = email
     @slack_error_message = slack_error_message
     mail(subject: "Slack invite failed for user with email '#{email}'.",
          to: "support@agileventures.org")
+  end
+
+  private
+
+  def backtrace_output
+    backtrace = @error.backtrace if @error
+    backtrace ? backtrace.join("\n\t") : ''
   end
 
 end
--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-RSpec.describe AdminMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe AdminMailer, type: :mailer do
+  context 'slack invite errors' do
+    subject(:mailer) { AdminMailer.failed_to_invite_user_to_slack('tansaku@gmail.com', StandardError.new('Boom!'), nil) }
+    it 'sends a mail' do
+      expect { subject.deliver_now }.to change { ActionMailer::Base.deliveries.length }.by(1)
+    end
+  end
 end
+
