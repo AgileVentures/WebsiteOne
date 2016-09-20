52cc4e31904300fbf36711e34a4a68e6c82e5755
Sam Joseph <tansaku@gmail.com>
2016-09-20 19:14:51 +0100

    Unvendoring the JavaScript (#1278)
    * First pass at installing moment dependencies
    from npm and copying files to vendor directory
    as postinstall hook
    Update gitignore for now
    
    * Move bootstrap-datepicker into npm
    
    * Add bootstrap-timepicker from npm
    
    * Use corejs-typeahead from npm
    
    * Move nprogress(bar) into npm
    
    * updates docs to mention npm
    
    * adds nodejs buildpack
    
    * adds npm install to ubuntu and OSX install notes

---
+++ scripts/copy_javascript_dependencies.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+var copyfiles = require('copyfiles');
+
+copyfiles(['node_modules/nprogress/nprogress.js','node_modules/corejs-typeahead/dist/typeahead.jquery.js',
+    'node_modules/bootstrap-timepicker/js/bootstrap-timepicker.min.js', 'node_modules/bootstrap-datepicker/js/bootstrap-datepicker.js','node_modules/moment/min/moment.min.js',
+    'node_modules/moment-timezone/builds/moment-timezone-with-data-2010-2020.js', 'vendor/assets/javascripts'], true,
+    function(err,files){});
