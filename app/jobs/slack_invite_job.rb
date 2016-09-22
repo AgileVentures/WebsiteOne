1367f77fc9d8d3308e1d81c60383842ec7239170
Sam Joseph <tansaku@gmail.com>
2016-09-22 09:03:50 +0100

    send alerts when slack invites fail (#1274)
    * adds failing tests for sending admin messages during slack invite fail
    
    * Spec out and implement passing error and user email to adminmailer
    
    * refactors a little
    
    * spikes an admin mailer
    
    * defend against nil error
    
    * switches spec to spec (not rails) helper
    
    * adds test to check admin mail is delivered
    
    * Expand error information from slack invite job
    
    * unit test for admin mailer and more robust display
    
    * ensures error message correctly displays
    
    * ensures error message in emails with tests

--- app/jobs/slack_invite_job.rb
+++ app/jobs/slack_invite_job.rb
@@ -1,13 +1,24 @@
 class SlackInviteJob
   include SuckerPunch::Job
 
+  SLACK_INVITE_URL = 'https://agileventures.slack.com/api/users.admin.invite'
+
   def perform(email)
-    uri = URI.parse 'https://agileventures.slack.com/api/users.admin.invite'
+    uri = URI.parse SLACK_INVITE_URL
     response = Net::HTTP.post_form(uri, {
-      email: email,
-      channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
-      token: Slack::AUTH_TOKEN
+        email: email,
+        channels: 'C02G8J689,C0285CSUH,C02AA0ARR',
+        token: Slack::AUTH_TOKEN
     })
-    JSON.parse response.body
+    error = nil
+    json_response = JSON.parse response.body
+    slack_error_message = json_response['error']
+    json_response
+  rescue => e
+    error = e
+  ensure
+    unless json_response.present? and json_response['ok']
+      AdminMailer.failed_to_invite_user_to_slack(email, error, slack_error_message).deliver_later
+    end
   end
 end
