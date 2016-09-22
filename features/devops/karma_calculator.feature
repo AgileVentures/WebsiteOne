ca78d33e28cb9922b7221e27f374863fd088047f
mtc2013 <mtc2013opensource@gmail.com>
2016-09-22 13:38:46 -0400

    Fix rake task cuke steps

--- features/devops/karma_calculator.feature
+++ features/devops/karma_calculator.feature
@@ -1,4 +1,4 @@
-@vcr
+@vcr @rake
 Feature: Update karma points of all users
   As a website admin, i should be able to update
   total karma points of my members
@@ -7,5 +7,5 @@ Feature: Update karma points of all users
     Given a gplus user who has attended one event
 
   Scenario: Update github commit count of all projects with valid github_url
-    When I run rake task "karma_calculator"
+    When I run the rake task for calculating karma points
     Then the gplus user should be credit as having attended one event
--- features/step_definitions/devops_steps.rb
+++ features/step_definitions/devops_steps.rb
@@ -1,10 +1,21 @@
 When(/^I run rake task "([^"]*)"$/) do |name|
-  # Write code here that turns the phrase above into concrete actions
-   require "rake"
-   @rake = Rake::Application.new
-   Rake.application = @rake
-   Rake::Task.define_task(:environment)
-   Rake.application.rake_require "tasks/scheduler"
-   @rake[name].invoke
+  # # Write code here that turns the phrase above into concrete actions
+  #  require "rake"
+  #  @rake ||= Rake::Application.new
+  #  Rake.application = @rake
+  #  Rake::Task.define_task(:environment)
+  #  Rake.application.rake_require "tasks/scheduler"
+  #  @rake[name].execute
+  $rake[name].execute
 end
+
+When(/^I run the rake task for calculating karma points$/) do
+   # require "rake"
+   # @rake ||= Rake::Application.new
+   # Rake.application = @rake
+   # Rake::Task.define_task(:environment)
+   # Rake.application.rake_require "tasks/scheduler"
+   $rake["karma_calculator"].execute
+end
+
 
