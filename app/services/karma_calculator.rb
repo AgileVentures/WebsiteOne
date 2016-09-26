b11696e6d99f46acc5ad18ed5ce54b6e4b6993aa
mtc2013 <mtc2013opensource@gmail.com>
2016-09-26 05:34:32 -0400

    1290 karma credit for participating in hangouts (#1293)
    * Cuke passing for karma for credit for participating in hangout
    
    * Add rake task to perform karma calculations
    
    * Fix rake task cuke steps
    
    * halfway through
    
    * a bit further
    
    * rspecs working
    
    * fixes for cukes
    
    * refactors to address code climate issue of long method complexity
    
    * more work for codeclimate
    
    * cleans up devops steps

--- app/services/karma_calculator.rb
+++ app/services/karma_calculator.rb
@@ -10,7 +10,7 @@ class KarmaCalculator
     user.karma_points = 0
     return if user.created_at.blank?
 
-    user.karma_points = membership_length + profile_completeness + activity + number_hangouts_started_with_more_than_one_participant + number_github_contributions
+    user.karma_points = membership_length + profile_completeness + activity + number_hangouts_started_with_more_than_one_participant + number_github_contributions + hangouts_attended_with_more_than_one_participant
     # better to have time in pairing sessions, code contributed (related to quality), issues, ...
   end
 
@@ -32,6 +32,30 @@ class KarmaCalculator
 
   def number_hangouts_started_with_more_than_one_participant
     user.number_hangouts_started_with_more_than_one_participant
+  end
+
+  def hangouts_attended_with_more_than_one_participant
+    id = gplus_id
+    return 0 unless id
+    hangouts_attended = calculate_hangouts_attended_with_more_than_one_participant(id)
+    user.hangouts_attended_with_more_than_one_participant = hangouts_attended
+  end
+
+  def gplus_id
+    gplus_auth = user.authentications.select { |a| a.provider == 'gplus' }
+    gplus_auth.try(:first).try(:uid)
+  end
+
+  def calculate_hangouts_attended_with_more_than_one_participant(id)
+    EventInstance.all.select do |i|
+      more_than_one?(i) && i.participants.values.any? do |p|
+        p['person']['id'] == id
+      end
+    end.count
+  end
+
+  def more_than_one?(event_instance)
+    !event_instance.participants.nil? && event_instance.participants.values.count > 1
   end
 
   def activity # 6
---
+++ features/step_definitions/karma_calculator_steps.rb
@@ -0,0 +1,9 @@
+
+Given(/^a gplus user who has attended one event$/) do
+  hangout = FactoryGirl.create(:event_instance, created: '1979-10-14 11:15 UTC')
+  @gplus_user = FactoryGirl.create(:user, gplus: hangout.participants.first.last['person']['id'])
+end
+
+Then(/^the gplus user should be credit as having attended one event$/) do
+ expect(@gplus_user.reload.hangouts_attended_with_more_than_one_participant).to eq 1
+end
