a60305132f99eb40a8388a578a77d5fcbe484a6c
Sam Joseph <tansaku@gmail.com>
2016-09-28 15:40:36 +0100

    clean up for code climate

--- app/services/karma_calculator.rb
+++ app/services/karma_calculator.rb
@@ -10,11 +10,15 @@ class KarmaCalculator
     user.karma.total = 0
     return if user.created_at.blank?
 
-    user.karma.total = membership_length + profile_completeness + activity + number_hangouts_started_with_more_than_one_participant + number_github_contributions + hangouts_attended_with_more_than_one_participant
+    user.karma.total = sum_elements
     # better to have time in pairing sessions, code contributed (related to quality), issues, ...
   end
 
   private
+
+  def sum_elements
+    membership_length + profile_completeness + activity + number_hangouts_started_with_more_than_one_participant + number_github_contributions + hangouts_attended_with_more_than_one_participant
+  end
 
   def membership_length # 6
     user.membership_length
