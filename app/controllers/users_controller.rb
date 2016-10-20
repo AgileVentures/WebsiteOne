3aa388825779e7cb2680a218c60d8a0fffd148da
Marouen Bousnina <marouen.it.technology@gmail.com>
2016-10-20 15:38:23 +0200

    1245 link karma number to breakdown of karma on user profile page (#1328)
    * link karma number to breakdown of karma on user profile page (#1245)
    
    Addresses: https://github.com/AgileVentures/WebsiteOne/issues/1245
    
    * Add hover effect to user profile info
    
    * refactor UsersController#set_activity_tab
    
    * remove helpers and controllers tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,7 @@ class UsersController < ApplicationController
     if should_display_user?(@user)
       @event_instances = EventInstance.where(user_id: @user.id)
                              .order(created_at: :desc).limit(5)
+      set_activity_tab(params[:tab])
     else
       raise ActiveRecord::RecordNotFound.new('User has not exposed his profile publicly')
     end
@@ -106,6 +107,15 @@ class UsersController < ApplicationController
       else
         redirect_to :back, alert: "Can't determine your location!"
       end
+    end
+  end
+
+  def set_activity_tab(param)
+    return unless param.present?
+    @param_tab = param
+    unless UserPresenter.new(@user).contributed?
+      @param_tab = nil
+      flash.now[:notice] = 'User does not have activity log'
     end
   end
 end
