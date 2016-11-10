46966cf161cbde3b43fd18f57340db6f4cf49d51
Sam Joseph <tansaku@gmail.com>
2016-11-10 15:17:01 +0000

    avoids manually started hangouts causing following event to go live

--- app/helpers/event_instances_helper.rb
+++ app/helpers/event_instances_helper.rb
@@ -1,5 +1,6 @@
+require 'securerandom'
 module EventInstancesHelper
   def generate_event_instance_id(user, project_id = nil)
-    "#{user.id}#{project_id || '00'}"
+    "#{user.id}#{project_id || '00'}-#{SecureRandom.uuid}"
   end
 end
