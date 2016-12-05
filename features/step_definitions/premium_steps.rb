a2e137eb5dac065ff6886d48b38d3be1e10af796
Sam Joseph <tansaku@gmail.com>
2016-12-05 15:21:12 +0000

    remove as yet unused steps

--- features/step_definitions/premium_steps.rb
+++
@@ -1,7 +0,0 @@
-Then(/^my member page should show premium details$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I should be redirected to Paypal's payment screens$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
