e72d24d55867bab7374cbbd0ca2427704b2104e9
Sam Joseph <tansaku@gmail.com>
2016-12-21 19:06:34 +0000

    removes legacy stripe_customer field from user and adds test for migrating to new plans for subscription setup

--- features/step_definitions/devops_steps.rb
+++ features/step_definitions/devops_steps.rb
@@ -14,3 +14,31 @@ Then(/^"([^"]*)" shoud have "([^"]*)" in their subscription$/) do |email, stripe
   user = User.find_by_email(email)
   expect(user.stripe_customer_id).to eq stripe_id
 end
+
+Given(/^the following payment sources exist$/) do |table|
+  table.hashes.each do |hash|
+    user = User.find_by(first_name: hash.delete('user'))
+    subscription = Subscription.find_by(user_id: user.id)
+    hash[:subscription_id] = subscription
+    PaymentSource::PaymentSource.create!(hash)
+  end
+end
+
+Given(/^the following subscriptions exist$/) do |table|
+  table.hashes.each do |hash|
+    user = User.find_by(first_name: hash.delete('user'))
+    hash[:user_id] = user.id
+    hash[:started_at] = Time.now
+    Subscription.create!(hash)
+  end
+end
+
+When(/^I run the rake task for migrating plans$/) do
+  $rake['db:migrate_plans'].execute
+end
+
+Then(/^"([^"]*)" should have a "([^"]*)" subscription plan$/) do |email, plan|
+  user = User.find_by_email(email)
+  expect(user.subscription.plan).to eq Plan.find_by(name: plan)
+end
+
