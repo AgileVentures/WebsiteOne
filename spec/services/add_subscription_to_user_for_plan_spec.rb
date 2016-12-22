524001ec1b9d01fd4408d2cdae9e3174f2c0f0c1
Sam Joseph <tansaku@gmail.com>
2016-12-22 16:06:57 +0000

    refactor as suggested by code climate and adapt paypal form depending on presence of free trial

--- spec/services/add_subscription_to_user_for_plan_spec.rb
+++ spec/services/add_subscription_to_user_for_plan_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe AddSubscriptionToUserForPlan do
 
-  subject(:add_subscription_to_user_for_plan) { described_class.with(user, time, third_party_id, plan, payment_source_klass, subscription_klass) }
+  subject(:add_subscription_to_user_for_plan) { described_class.with(user, time, plan, payment_source, subscription_klass) }
 
   let(:subscription_klass) { class_double(Subscription) }
-  let(:payment_source_klass) { class_double(PaymentSource::Stripe) }
 
   let(:plan) { instance_double(Plan, name: 'Premium') }
   let(:third_party_id) { instance_double('ThirdPartyID') }
@@ -15,20 +14,13 @@ describe AddSubscriptionToUserForPlan do
   let(:payment_source) { instance_double(PaymentSource::Stripe) }
 
   before do
-    allow(payment_source_klass).to receive(:new)
     allow(subscription_klass).to receive(:new)
     allow(user).to receive(:subscription=)
     allow(user).to receive(:save)
     allow(user).to receive_message_chain(:title_list, :<<)
   end
 
-  it 'creates a payment source' do
-    expect(payment_source_klass).to receive(:new).with({identifier: third_party_id})
-    add_subscription_to_user_for_plan
-  end
-
   it 'creates a subscription of the appropriate type' do
-    allow(payment_source_klass).to receive(:new).and_return(payment_source)
     expect(subscription_klass).to receive(:new).with(started_at: time, plan: plan, payment_source: payment_source)
     add_subscription_to_user_for_plan
   end
