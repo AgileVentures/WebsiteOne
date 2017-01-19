1ef64c067c72ed9129b63876f1f7fbfbcb63c74a
Matt Lindsey <matthew.r.lindsey@gmail.com>
2017-01-19 13:01:56 -0500

    Fix routing to disallow get on subscriptions/upgrade (#1502)

---
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe SubscriptionsController, :type => :controller do
+
+  describe "Attempt to get individual subscription/upgrade" do
+    it "raises URL Generation error" do
+        expect { get "/subscriptions/upgrade" }.to raise_error(ActionController::UrlGenerationError)
+    end
+  end
+
+end
