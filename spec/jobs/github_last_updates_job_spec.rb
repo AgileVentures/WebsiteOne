ee28555d9db42207a1327f638dc0e7d84dfd1ac1
mtc2013 <mtc2013opensource@gmail.com>
2017-03-09 13:06:14 -0500

    Fix most recent commit update job for projects with hyphens in repo name (#1585)
    * Fix most recent commit update job for projects with hyphens in repo name
    
    * Update cache

---
+++ spec/jobs/github_last_updates_job_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe GithubLastUpdatesJob do
+  describe '#run' do
+    context 'shf-project with hyphen', vcr: true do
+      let!(:project){FactoryGirl.create(:project, github_url: 'https://github.com/AgileVentures/shf-project')}
+      it 'has correct last commit date after job run' do
+        GithubLastUpdatesJob.run
+        expect(project.reload.last_github_update).not_to be_nil
+      end
+    end
+  end
+end
