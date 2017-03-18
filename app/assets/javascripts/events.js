95831cbaee23c24837efdec0fdd31b968a5ff27f
Amber <amber@amberwilkie.com>
2017-03-18 20:45:18 +0100

    Nest google cal link under calendar icon

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -12,11 +12,21 @@ var events = {
             var href = clicked_row.find('.event-title a')[0].href;
             browserAdapter.jumpTo(href);
         });
+    },
+    hideCalendarLinks: function () {
+        $('#calendar_links').hide();
+    },
+    addToCalendar: function () {
+        $('#calendar_link').on('click', function () {
+            $('#calendar_links').show();
+        })
     }
-}
+};
 
 $(document).ready(function () {
     events.makeRowBodyClickable();
+    events.hideCalendarLinks();
+    events.addToCalendar();
     editEventForm.handleUserTimeZone();
     showEvent.showUserTimeZone();
 });
