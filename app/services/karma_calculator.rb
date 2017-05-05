b3ada6dda5c056ff660a9ed832643dfac5e50186
Matt Lindsey <matthew.r.lindsey@gmail.com>
2017-05-05 07:23:43 -0400

    Fix KarmaCaclulator

--- app/services/karma_calculator.rb
+++ app/services/karma_calculator.rb
@@ -13,8 +13,8 @@ class KarmaCalculator
       user.karma = Karma.find_or_create_by(user_id: user.id, total: 0)
     end
 
-    user.karma.total = sum_elements
     return if user.created_at.blank?
+    user.karma.total = sum_elements
     # better to have time in pairing sessions, code contributed (related to quality), issues, ...
   end
 
