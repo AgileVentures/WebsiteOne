8b3ba49f494c8bf58635e30c0da02a78d4b9133f
lokeshh <lokeshhsharma@gmail.com>
2017-07-05 10:18:31 +0000

    modify step to specify hangout link set to different URL

--- features/step_definitions/hangout_steps.rb
+++ features/step_definitions/hangout_steps.rb
@@ -115,6 +115,16 @@ And(/^I manually set a hangout link for event "([^"]*)"$/) do |name|
   visit event_path(event)
 end
 
+And(/^I manually set a hangout link for event "([^"]*)" to a different URL$/) do |name|
+  @hangout_url = 'https://hangouts.google.com/hangouts/_/ytl/25xi7ef6erhbxeff5q5ymzgdfme=?hl=en_US&authuser=0'
+  event = Event.find_by_name(name)
+  visit event_path(event)
+  page.execute_script(  %q{$('li[role="edit_hoa_link"] > a').trigger('click')}  )
+  fill_in 'hangout_url', :with => @hangout_url
+  page.find(:css, %q{input[id="hoa_link_save"]}).trigger('click')
+  visit event_path(event)
+end
+
 Then(/^"([^"]*)" shows live for that hangout link for the event duration$/) do |event_name|
   event = Event.find_by_name(event_name)
   visit event_path(event)
@@ -130,7 +140,7 @@ Then(/^"([^"]*)" shows live for that hangout link for the event duration$/) do |
   expect(page).not_to have_link('Join now')
 end
 
-Given(/^"([^"]*)" doesn't shows live for that hangout link at the moment$/) do |event_name|
+Given(/^"([^"]*)" doesn't go live$/) do |event_name|
   event = Event.find_by_name(event_name)
   visit event_path(event)
   expect(page).not_to have_link('Join now')
