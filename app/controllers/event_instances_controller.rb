eafc87379735ce90b01d2be4a4539c27beb98e9e
lokeshh <lokeshhsharma@gmail.com>
2017-07-20 09:12:12 +0000

    separate out slack notify for hangout and yt

--- app/controllers/event_instances_controller.rb
+++ app/controllers/event_instances_controller.rb
@@ -8,10 +8,11 @@ class EventInstancesController < ApplicationController
     event_instance_params = check_and_transform_params(event_instance)
     hangout_url_changed = event_instance.hangout_url != event_instance_params[:hangout_url]
     yt_video_id_changed = event_instance.yt_video_id != event_instance_params[:yt_video_id]
-    slack_notify = params[:notify] == 'true'
+    slack_notify_hangout = params[:notify_hangout] == 'true'
+    slack_notify_yt = params[:notify_yt] == 'true'
     if event_instance.try!(:update, event_instance_params)
-      SlackService.post_hangout_notification(event_instance) if (slack_notify && event_instance.hangout_url?) || (event_instance.started? && hangout_url_changed)
-      SlackService.post_yt_link(event_instance) if (slack_notify && event_instance.yt_video_id?) || yt_video_id_changed
+      SlackService.post_hangout_notification(event_instance) if (slack_notify_hangout && event_instance.hangout_url?) || (event_instance.started? && hangout_url_changed)
+      SlackService.post_yt_link(event_instance) if (slack_notify_yt && event_instance.yt_video_id?) || yt_video_id_changed
 
       TwitterService.tweet_hangout_notification(event_instance) if event_instance.started? && hangout_url_changed
       TwitterService.tweet_yt_link(event_instance)
