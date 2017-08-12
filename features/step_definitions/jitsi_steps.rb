0b133dcc8004a30c7553715e71ffbbd1c11989d5
Lokesh Sharma <lokeshhsharma@gmail.com>
2017-08-12 13:57:38 +0530

    test for start jitsi button on event and project page (#1765)
    * test for start jitsi on event page
    
    * fix last test
    
    * add test for project page
    
    * add unit tests

---
+++ features/step_definitions/jitsi_steps.rb
@@ -0,0 +1,5 @@
+Then(/^I should see a start jitsi meet button with link "([^"]*)"$/) do |url|
+  expect(page).to have_css('#start-jitsi')
+  url = page.find(:css, '#start-jitsi').find(:xpath,"..")[:href]
+  expect(url).to eq url
+end
