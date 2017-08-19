f78441f6a152ba555bc43a1e48d99b1c358969f3
Sam Joseph <tansaku@gmail.com>
2017-08-19 20:14:59 +0900

    experiment with only working out schedule for upcoming events (#1778)
    * experiment with only working out schedule for upcoming events
    
    * update to make specs pass since they use NULLs in the repeat_ends column ...
    
    * adjust method name to avoid confusion

--- app/models/event.rb
+++ app/models/event.rb
@@ -29,12 +29,16 @@ class Event < ActiveRecord::Base
     @repeat_ends_string = repeat_ends ? "on" : "never"
   end
 
-  def self.base_events(project)
-    project.nil? ? Event.all : Event.where(project_id: project)
+  def self.base_future_events(project)
+    project.nil? ? Event.future_events : Event.future_events.where(project_id: project)
+  end
+
+  def self.future_events
+    Event.where('repeat_ends = false OR repeat_ends IS NULL OR repeat_ends_on > ?', Time.now)
   end
 
   def self.upcoming_events(project=nil)
-    events = Event.base_events(project).inject([]) do |memo, event|
+    events = Event.base_future_events(project).inject([]) do |memo, event|
       memo << event.next_occurrences
     end.flatten.sort_by { |e| e[:time] }
     Event.remove_past_events(events)
