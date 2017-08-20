fe778953d618c86cd97da3addcd71651da7f8329
Sam Joseph <tansaku@gmail.com>
2017-08-20 13:42:21 +0900

    reduce initial number of hangouts displayed on page

--- app/controllers/event_instances_controller.rb
+++ app/controllers/event_instances_controller.rb
@@ -26,7 +26,7 @@ class EventInstancesController < ApplicationController
   def index
     relation = (params[:live] == 'true') ? EventInstance.live : EventInstance.latest
     relation = relation.includes(:project, :event, :user)
-    @event_instances = relation.paginate(:page => params[:page])
+    @event_instances = relation.paginate(:page => params[:page], per_page: 5)
   end
 
   private
