86382649c2c3d65490e4f6b3866d10ffb58e7777
Matt Lindsey <matthew.r.lindsey@gmail.com>
2017-08-21 02:20:56 -0400

    fix StaticPages init javascript function

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,14 +1,15 @@
 //= require medium-editor
 
-window._init_editor = window._init_editor||[];
-!function(){
-    var editor = new MediumEditor('.editable', {
-        placeholder: {
-            /* This example includes the default options for placeholder,
-                if nothing is passed this is what it used */
-            text: 'Type your text',
-            hideOnClick: true
+WebsiteOne.define('StaticPages', function() {
+    return {
+        init: function() {
+            var editor = new MediumEditor('.editable', {
+                placeholder: {
+                    text: 'Type your text',
+                    hideOnClick: true
+                }
+            })
         }
-    })
-}();
+    }
+});
 
