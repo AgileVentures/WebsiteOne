9fcb31edd327646d59eb5f7f38df43163cab1466
alexander_kh <khlipun@gmail.com>
2017-09-02 10:31:27 +0000

    Remove unused helper modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 require 'custom_errors.rb'
 
 class ApplicationController < ActionController::Base
-  include YoutubeHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
