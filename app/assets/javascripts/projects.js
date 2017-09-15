4c96f3f9a0c76ec22b67c4d1065f5b5b000a3819
Sam Joseph <tansaku@gmail.com>
2017-09-15 12:50:30 +0100

    spiking up some interface stuff

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -16,6 +16,22 @@ WebsiteOne.define('Projects', function() {
       var setHash = function(newHash) {
         window.location.hash = newHash;
       };
+    },
+    setupAddRepoButton: function () {
+      $('#add_repo_field_button').click(function(event){
+        event.preventDefault()
+
+        var new_html = '<div class="form-group"> \
+            <label class="control-label" for="project_github_url">GitHub link</label>\
+            <input class="form-control" \
+                   placeholder="https://github.com/projectname" name="project[github_url]" id="project_github_url">\
+              </div>'
+        $(new_html).insertAfter('#project_github_url')
+        return false
+      })
     }
   }
 });
+
+
+$(document).on('ready page:load', WebsiteOne.Projects.setupAddRepoButton)
