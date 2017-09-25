ea82bc12c55328203e333cd6399a5932a6ac7ec2
Sam Joseph <tansaku@gmail.com>
2017-09-25 17:16:59 +0100

    experimenting with code that could replace agile bot

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -5,15 +5,56 @@ module SlackService
     return unless Features.slack.notifications.enabled
     return if hangout.hangout_url.blank?
 
-    uri = URI.parse "#{Slack::BOT_URL}/hubot/hangouts-notify"
-    Net::HTTP.post_form uri, {
-      title: hangout.title,
-      link: hangout.hangout_url,
-      type: hangout.category,
-      host_name: hangout.user.display_name,
-      host_avatar: hangout.user.gravatar_url,
-      project: hangout.project.try(:slug)
-    }
+    client = Slack::Web::Client.new
+    client.chat_postMessage(channel: '', text: '', username: hangout.user.display_name, icon_url: hangout.user.gravatar_url)
+
+    room = find_project_for_hangout(hangout.project.try(:slug))
+
+    if hangout.category == "Scrum"
+      send_slack_message client, CHANNELS.general, "@here #{req.body.title}: #{req.body.link}", hangout.user
+      send_slack_message client, CHANNELS.standup_notifications, "@channel #{req.body.title}: #{req.body.link}", hangout.user
+
+    elsif hangout.category == "PairProgramming"
+
+      if room == CHANNELS.cs169
+        console.log('sending PP event to gitter: ' + room)
+        send_gitter_message_avoid_repeats room, "[#{req.body.title} with #{user.name}](#{req.body.link}) is starting NOW!"
+      else
+        send_slack_message client, CHANNELS.general, "#{req.body.title}: #{req.body.link}", hangout.user
+
+        send_slack_message client, CHANNELS.pairing_notifications, "@channel #{req.body.title}: #{req.body.link}", hangout.user
+        console.log('sending PP event to slack: ' + room)
+      end
+
+      # send all types of events to associated project "room" if there is one
+      if room
+        send_slack_message client, room, "@here #{req.body.title}: #{req.body.link}", hangout.user
+
+      end
+    end
+    # uri = URI.parse "#{Slack::BOT_URL}/hubot/hangouts-notify"
+    # Net::HTTP.post_form uri, {
+    #   title: hangout.title,
+    #   link: hangout.hangout_url,
+    #   type: hangout.category,
+    #   host_name: hangout.user.display_name,
+    #   host_avatar: hangout.user.gravatar_url,
+    #   project: hangout.project.try(:slug)
+    # }
+
+  end
+
+  def send_slack_message(client, channel, text, user)
+    client.chat_postMessage(channel: channel, text: text, username: user.display_name, icon_url: user.gravatar_url)
+
+  end
+
+  def send_gitter_message_avoid_repeats(room, text)
+
+  end
+
+  def find_project_for_hangout(project_slug)
+
   end
 
   def post_yt_link(hangout)
@@ -22,12 +63,12 @@ module SlackService
 
     uri = URI.parse "#{Slack::BOT_URL}/hubot/hangouts-video-notify"
     Net::HTTP.post_form uri, {
-      title: hangout.title,
-      video: "https://youtu.be/#{hangout.yt_video_id}",
-      type: hangout.category,
-      host_name: hangout.user.display_name,
-      host_avatar: hangout.user.gravatar_url,
-      project: hangout.project.try(:slug)
+        title: hangout.title,
+        video: "https://youtu.be/#{hangout.yt_video_id}",
+        type: hangout.category,
+        host_name: hangout.user.display_name,
+        host_avatar: hangout.user.gravatar_url,
+        project: hangout.project.try(:slug)
     }
   end
 end
--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,4 +1,8 @@
-module Slack
-  BOT_URL = Settings.agile_bot_url
-  AUTH_TOKEN = ENV['SLACK_AUTH_TOKEN']
+# module Slack
+#   BOT_URL = Settings.agile_bot_url
+#   AUTH_TOKEN = ENV['SLACK_AUTH_TOKEN']
+# end
+
+Slack.configure do |config|
+  config.token = ENV['PRODUCTION_SLACK_AUTH_TOKEN']
 end
