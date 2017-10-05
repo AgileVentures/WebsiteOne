ee622993a25e0cf4695f6140e9e7c1354595e52a
Sam Joseph <tansaku@gmail.com>
2017-10-05 10:12:48 +0100

    more unit tests that hopefully give us full coverage here

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -4,30 +4,33 @@ require 'gitter'
 module SlackService
   extend self
 
-  def post_hangout_notification(hangout, client = Slack::Web::Client.new(logger: Rails.logger))
+  def post_hangout_notification(hangout,
+                                slack_client = Slack::Web::Client.new(logger: Rails.logger),
+                                gitter_client = Gitter::Client.new(ENV['GITTER_API_TOKEN']))
+
     return unless Features.slack.notifications.enabled
     return if hangout.hangout_url.blank?
 
     channel = channel_for_project(hangout.project)
 
     if hangout.category == "Scrum"
-      send_slack_message client, CHANNELS[:general], "@here #{hangout.title}: #{hangout.hangout_url}", hangout.user
-      send_slack_message client, CHANNELS[:standup_notifications], "@channel #{hangout.title}: #{hangout.hangout_url}", hangout.user
+      send_slack_message slack_client, CHANNELS[:general], "@here #{hangout.title}: #{hangout.hangout_url}", hangout.user
+      send_slack_message slack_client, CHANNELS[:standup_notifications], "@channel #{hangout.title}: #{hangout.hangout_url}", hangout.user
 
     elsif hangout.category == "PairProgramming"
 
       if channel == CHANNELS[:cs169]
         # puts("sending PP event to gitter: #{channel}")
-        send_gitter_message_avoid_repeats "[#{hangout.title} with #{hangout.user.display_name}](#{hangout.hangout_url}) is starting NOW!"
+        send_gitter_message_avoid_repeats gitter_client, "[#{hangout.title} with #{hangout.user.display_name}](#{hangout.hangout_url}) is starting NOW!"
       else
-        send_slack_message client, CHANNELS[:general], "#{hangout.title}: #{hangout.hangout_url}", hangout.user
-        send_slack_message client, CHANNELS[:pairing_notifications], "@channel #{hangout.title}: #{hangout.hangout_url}", hangout.user
+        send_slack_message slack_client, CHANNELS[:general], "@here #{hangout.title}: #{hangout.hangout_url}", hangout.user
         # puts("sending PP event to slack: #{channel}")
       end
+      send_slack_message slack_client, CHANNELS[:pairing_notifications], "@channel #{hangout.title}: #{hangout.hangout_url}", hangout.user
     end
     # send all types of events to associated project "channel" if there is one
     if channel
-      send_slack_message client, channel, "@here #{hangout.title}: #{hangout.hangout_url}", hangout.user
+      send_slack_message slack_client, channel, "@here #{hangout.title}: #{hangout.hangout_url}", hangout.user
     end
   end
 
@@ -35,13 +38,11 @@ module SlackService
     client.chat_postMessage(channel: channel, text: text, username: user.display_name, icon_url: user.gravatar_url, link_names: 1)
   end
 
-  def send_gitter_message_avoid_repeats(text)
-    client = Gitter::Client.new(ENV['GITTER_API_TOKEN'])
-
-    messages = client.messages(GITTER_ROOMS['saasbook/MOOC'], limit: 50)
+  def send_gitter_message_avoid_repeats(gitter_client, text)
+    messages = gitter_client.messages(GITTER_ROOMS[:'saasbook/MOOC'], limit: 50)
     return if messages.include? text
 
-    client.send_message(text, GITTER_ROOMS['saasbook/MOOC'])
+    gitter_client.send_message(text, GITTER_ROOMS[:'saasbook/MOOC'])
   end
 
   def channel_for_project(project)
@@ -69,61 +70,58 @@ module SlackService
 
   end
 
+  if ENV['LIVE_ENV'] == 'production'
+    CHANNELS = {
+        "asyncvoter": "C2HGJF54G",
+        "autograder": "C0UFNHRAB",
+        "betasaasers": "C02AHEA5P",
+        "binghamton-university-bike-share": "C033Z02P9",
+        "codealia": "C0297TUQC",
+        "communityportal": "C02HVF1TP",
+        "cs169": "C02A6835V",
+        "dda-pallet": "C2QM5N48P",
+        "educhat": "C02AD0LG0",
+        "esaas-mooc": "C02A6835V",
+        "eventmanager": "C39J4DTP0",
+        "metplus": "C09LSBWER",
+        "localsupport": "C0KK907B5",
+        "osra-support-system": "C02AAM8SY",
+        "github-api-gem": "C02QZ46S9",
+        "oodls": "C03GBBASJ",
+        "projectscope": "C1NJX7KM1",
+        "redeemify": "C1FQZHJJX",
+        "refugee_tech": "C0GUTH7RS",
+        "secondappinion": "C03D6RUR7",
+        "shf-project": "C2SBUUNRY",
+        "snow-angels": "C03D6RUR7",
+        "takemeaway": "C04B0TN0S",
+        "teamaidz": "C03DA8NH0",
+        "visualizer": "C3NE9JQJX",
+        "websiteone": "C029E8G80",
+        "websitetwo": "C0ASA1X98",
+        "wiki-ed-dashboard": "C36MNPWTD",
+        "general": "C0285CSUF",
+        "pairing_notifications": "C02BNVCM1",
+        "standup_notifications": "C02B4QH1C"
+    }
+    GITTER_ROOMS = {
+        "saasbook/MOOC": "544100afdb8155e6700cc5e4",
+        "saasbook/AV102": "55e42db80fc9f982beaf2725",
+        "AgileVentures/agile-bot": "56b8bdffe610378809c070cc"
+    }
+  else
+    CHANNELS =    {
+        "cs169": "C29J4CYA2",
+        "websiteone": "C29J4QQ9W",
+        "general": "C0TLAE1MH",
+        "pairing_notifications": "C29J3DPGW",
+        "standup_notifications": "C29JE6HGR",
+    }
 
-  # @TODO move these all into settings - need to swtich based on instance,
-  # e.g. staging.agileventures.org, www.agileventures.org ...
-
-  CHANNELS =    {
-      "cs169": "C29J4CYA2",
-      "websiteone": "C29J4QQ9W",
-      "general": "C0TLAE1MH",
-      "pairing_notifications": "C29J3DPGW",
-      "standup_notifications": "C29JE6HGR",
-  }
-
-  GITTER_ROOMS = {
-      "saasbook/MOOC": "56b8bdffe610378809c070cc",
-      "saasbook/AV102": "56b8bdffe610378809c070cc",
-      "AgileVentures/agile-bot": "56b8bdffe610378809c070cc"
-  }
-
-  #
-  # CHANNELS = {
-  #     "asyncvoter": "C2HGJF54G",
-  #     "autograder": "C0UFNHRAB",
-  #     "betasaasers": "C02AHEA5P",
-  #     "binghamton-university-bike-share": "C033Z02P9",
-  #     "codealia": "C0297TUQC",
-  #     "communityportal": "C02HVF1TP",
-  #     "cs169": "C02A6835V",
-  #     "dda-pallet": "C2QM5N48P",
-  #     "educhat": "C02AD0LG0",
-  #     "esaas-mooc": "C02A6835V",
-  #     "eventmanager": "C39J4DTP0",
-  #     "metplus": "C09LSBWER",
-  #     "localsupport": "C0KK907B5",
-  #     "osra-support-system": "C02AAM8SY",
-  #     "github-api-gem": "C02QZ46S9",
-  #     "oodls": "C03GBBASJ",
-  #     "projectscope": "C1NJX7KM1",
-  #     "redeemify": "C1FQZHJJX",
-  #     "refugee_tech": "C0GUTH7RS",
-  #     "secondappinion": "C03D6RUR7",
-  #     "shf-project": "C2SBUUNRY",
-  #     "snow-angels": "C03D6RUR7",
-  #     "takemeaway": "C04B0TN0S",
-  #     "teamaidz": "C03DA8NH0",
-  #     "visualizer": "C3NE9JQJX",
-  #     "websiteone": "C029E8G80",
-  #     "websitetwo": "C0ASA1X98",
-  #     "wiki-ed-dashboard": "C36MNPWTD",
-  #     "general": "C0285CSUF",
-  #     "pairing_notifications": "C02BNVCM1",
-  #     "standup_notifications": "C02B4QH1C"
-  # }
-  # GITTER_ROOMS = {
-  #     "saasbook/MOOC": "544100afdb8155e6700cc5e4",
-  #     "saasbook/AV102": "55e42db80fc9f982beaf2725",
-  #     "AgileVentures/agile-bot": "56b8bdffe610378809c070cc"
-  # }
+    GITTER_ROOMS = {
+        "saasbook/MOOC": "56b8bdffe610378809c070cc",
+        "saasbook/AV102": "56b8bdffe610378809c070cc",
+        "AgileVentures/agile-bot": "56b8bdffe610378809c070cc"
+    }
+  end
 end
--- spec/services/slack_service_spec.rb
+++ spec/services/slack_service_spec.rb
@@ -8,102 +8,191 @@ describe SlackService do
   before { Features.slack.notifications.enabled = true }
 
   describe '.post_hangout_notification' do
-    let(:hangout) { EventInstance.create(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user) }
-    let(:client) { double(:slack_client) }
+    let(:slack_client) { double(:slack_client) }
+    let(:gitter_client) { double(:gitter_client) }
 
     # @TODO Improve the message format to hide the URL details ... and welcome newcomers ...
-    # @TODO we should pass in CHANNELS and GITTER ROOMS work out a way to handle that based on deployed instance, e.g staging, production etc.
 
-    let(:general_channel_post_args) do
+    let(:default_post_args) do
       {
-          channel: 'C0TLAE1MH',
-          text: 'MockEvent: mock_url',
           username: user.display_name,
           icon_url: user.gravatar_url,
           link_names: 1
       }
     end
-    let(:project_channel_post_args) do
+    let(:general_channel_post_args) do
+      {
+          channel: 'C0TLAE1MH',
+          text: '@here MockEvent: mock_url',
+      }.merge!(default_post_args)
+    end
+    let(:websiteone_project_channel_post_args) do
       {
           channel: 'C29J4QQ9W',
           text: '@here MockEvent: mock_url',
-          username: user.display_name,
-          icon_url: user.gravatar_url,
-          link_names: 1
-      }
+      }.merge!(default_post_args)
+    end
+    let(:cs169_project_channel_post_args) do
+      {
+          channel: 'C29J4CYA2',
+          text: '@here MockEvent: mock_url',
+      }.merge!(default_post_args)
     end
     let(:pairing_notifications_channel_post_args) do
       {
           channel: 'C29J3DPGW',
           text: '@channel MockEvent: mock_url',
+      }.merge!(default_post_args)
+    end
+    let(:standup_notifications_channel_post_args) do
+      {
+          channel: 'C29JE6HGR',
+          text: '@channel MockEvent: mock_url',
+      }.merge!(default_post_args)
+    end
+
+    context('PairProgramming') do
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user) }
+
+      it 'sends the correct slack message to the correct channels' do
+        hangout.project = Project.create(title: 'websiteone', description: 'hmm', status: 'active')
+
+        expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(websiteone_project_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(pairing_notifications_channel_post_args)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+      it 'does not fail when event has no associated project' do
+
+        expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(pairing_notifications_channel_post_args)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+      it 'should ping gitter and slack (but not general) when the project is cs169' do
+        hangout.project = Project.create(title: 'cs169', description: 'hmm', status: 'active')
+
+        expect(gitter_client).to receive(:messages).with('56b8bdffe610378809c070cc', limit: 50).and_return([])
+        expect(gitter_client).to receive(:send_message).with('[MockEvent with random](mock_url) is starting NOW!', "56b8bdffe610378809c070cc")
+        expect(slack_client).to receive(:chat_postMessage).with(cs169_project_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(pairing_notifications_channel_post_args)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+      it 'does not post notification if hangout url is blank' do
+        hangout.hangout_url = "     "
+
+        expect(slack_client).not_to receive(:chat_postMessage)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+    end
+
+    context('Scrums') do
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user) }
+
+      it 'sends the correct slack message to the correct channels' do
+        hangout.project = Project.create(title: 'websiteone', description: 'hmm', status: 'active')
+
+        expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(websiteone_project_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(standup_notifications_channel_post_args)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+      it 'does not fail when event has no associated project' do
+
+        expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(standup_notifications_channel_post_args)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+      it 'should ping slack when the project is cs169' do
+        hangout.project = Project.create(title: 'cs169', description: 'hmm', status: 'active')
+
+        expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(cs169_project_channel_post_args)
+        expect(slack_client).to receive(:chat_postMessage).with(standup_notifications_channel_post_args)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+      it 'does not post notification if hangout url is blank for pairing' do
+        hangout.hangout_url = "     "
+
+        expect(slack_client).not_to receive(:chat_postMessage)
+
+        slack_service.post_hangout_notification(hangout, slack_client, gitter_client)
+      end
+
+    end
+  end
+
+  describe '.post_yt_link' do
+    let(:client) { spy(:slack_client) }
+    let(:expected_post_args) do
+      {
+          text: 'Video/Livestream for MockEvent: https://youtu.be/mock_url',
           username: user.display_name,
           icon_url: user.gravatar_url,
           link_names: 1
       }
     end
+    context('PairProgramming') do
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user) }
 
-    it 'sends the correct slack message to the correct channels for pairing' do
-      hangout.project = Project.create(title: 'websiteone', description: 'hmm', status: 'active')
+      it 'sends the correct slack message to the correct channels' do
+        hangout.yt_video_id = 'mock_url'
+        hangout.project = Project.create(title: 'WebSiteOne', description: 'hmmm', status: 'active')
 
-      expect(client).to receive(:chat_postMessage).with(general_channel_post_args)
-      expect(client).to receive(:chat_postMessage).with(project_channel_post_args)
-      expect(client).to receive(:chat_postMessage).with(pairing_notifications_channel_post_args)
+        slack_service.post_yt_link(hangout, client)
 
-      slack_service.post_hangout_notification(hangout, client)
+        expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
+        expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C29J4QQ9W'))
+      end
+
+      it 'does not ping slack when the project is cs169' do
+        hangout.yt_video_id = 'mock_url'
+        hangout.project = Project.create(title: 'cs169', description: 'hmmm', status: 'active')
+
+        slack_service.post_yt_link(hangout, client)
+
+        expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
+        expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C29J4QQ9W'))
+      end
+
+      it 'does not post youtube video link if yt_video_id is blank' do
+        hangout.yt_video_id = '    '
+        slack_service.post_yt_link(hangout, client)
+
+        expect(client).not_to have_received(:chat_postMessage)
+      end
     end
 
-    it 'does not fail when event has no associated project for pairing' do
+    context('Scrums') do
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: 'Scrum', hangout_url: 'mock_url', user: user) }
 
-      expect(client).to receive(:chat_postMessage).with(general_channel_post_args)
-      expect(client).to receive(:chat_postMessage).with(pairing_notifications_channel_post_args)
+      it 'sends the correct slack message to the correct channels' do
+        hangout.yt_video_id = 'mock_url'
+        hangout.project = Project.create(slug: 'websiteone', title: 'WebSiteOne', description: 'hmmm', status: 'active')
 
-      slack_service.post_hangout_notification(hangout, client)
-    end
+        slack_service.post_yt_link(hangout, client)
 
-    xit 'should ping gitter when the project is cs169'
-    xit 'sends the correct slack message to the correct channels for scrums'
-    xit 'does not fail when event has no associated project for scrums'
-    xit 'does not post notification if hangout url is blank for scrums'
+        expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
+      end
 
-    it 'does not post notification if hangout url is blank for pairing' do
-      hangout.hangout_url = "     "
+      it 'does not post youtube video link if yt_video_id is blank' do
+        hangout.yt_video_id = '    '
+        slack_service.post_yt_link(hangout, client)
 
-      expect(client).not_to receive(:chat_postMessage)
-
-      slack_service.post_hangout_notification(hangout)
-    end
-  end
-
-  describe '.post_yt_link' do
-    let(:hangout) { EventInstance.create(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user) }
-    let(:client) { spy(:slack_client) }
-    let(:expected_post_args) do
-      {
-        text: 'Video/Livestream for MockEvent: https://youtu.be/mock_url',
-        username: user.display_name,
-        icon_url: user.gravatar_url,
-        link_names: 1
-      }
-    end
-
-    it 'sends the correct slack message to the correct channels for pairing' do
-      hangout.yt_video_id = 'mock_url'
-      hangout.project = Project.create(slug: 'websiteone', title: 'WebSiteOne', description: 'hmmm', status: 'active')
-
-      slack_service.post_yt_link(hangout, client)
-
-      expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
-      expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C29J4QQ9W'))
-    end
-
-    xit 'sends the correct slack message to the correct channels for scrums'
-    xit 'does not post youtube video link if yt_video_id is blank for scrums'
-
-    it 'does not post youtube video link if yt_video_id is blank for pairing' do
-      hangout.yt_video_id = '    '
-      slack_service.post_yt_link(hangout, client)
-
-      expect(client).not_to have_received(:chat_postMessage)
+        expect(client).not_to have_received(:chat_postMessage)
+      end
     end
   end
 end
