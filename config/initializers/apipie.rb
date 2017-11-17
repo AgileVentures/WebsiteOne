49019e12aae269a854f6fe88a99f66a4620c6c36
Marian Mosley <mmosley@ieee.org>
2017-11-17 12:42:17 -0500

    Add route and controller for subscriptions api endpoint (#1959)
    * Add route ans controller for subscriptions api endpoint
    
    * spiking some authentication
    
    * Try some headers
    
    * latest version - test still not working ...
    
    * Make authentication spec pass
    
    * Add spec to check that request fails without creds; change options arg to block to satisfy codeclimate
    
    * Add started_on, ended_on, plan_name, and payment_source fields and accompanying specs
    
    * add some documentation

---
+++ config/initializers/apipie.rb
@@ -0,0 +1,10 @@
+Apipie.configure do |config|
+  config.translate = false
+  config.default_locale = nil
+  config.app_name = "WebsiteOne"
+  config.api_base_url = "/api"
+  config.doc_base_url = "/apipie"
+  # where is your API defined?
+  # config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.api_controllers_matcher = Rails.root.join('app', 'controllers', 'api', '**', '*.rb')
+end
