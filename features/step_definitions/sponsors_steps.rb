07b530a3ca45fd30fca61a034579f13050fb073a
marianmosley <marian.mosley@gmail.com>
2017-11-18 16:36:40 -0500

    Add sponsor id to subscription

--- features/step_definitions/sponsors_steps.rb
+++ features/step_definitions/sponsors_steps.rb
@@ -2,3 +2,9 @@ Then(/^I should see sponsor banner for "(.*?)"$/) do |supporter_name|
   expect(page).to have_selector('div#sponsorsBar')
   expect(page).to have_css("img[alt*='#{supporter_name}']")
 end
+
+Then(/^I should be (.*)'s sponsor$/) do |name|
+  user = User.find_by_first_name name
+  subscription = Subscription.find_by(user: user)
+  expect(subscription.sponsor).to eq(@current_user)
+end
