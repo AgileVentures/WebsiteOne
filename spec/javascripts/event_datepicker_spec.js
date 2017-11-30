5f6427216e9793d2f7774dc6648673bc97dcaef6
Sam Joseph <tansaku@gmail.com>
2017-11-30 11:02:22 +0000

    clear up all pending items from unit tests, fixes #1997

--- spec/javascripts/event_datepicker_spec.js
+++ spec/javascripts/event_datepicker_spec.js
@@ -1,11 +1,45 @@
 describe('eventDatePicker', function(){
-    beforeEach(function(){
-        // concerned that fixture is large and involved and creating too complex a seam
-        setFixtures('');
-    });
 
-    xit('hides event repeat options by default').pend('awaiting refactor of related form for use in fixture');
-    xit('shows event repeat options when event is set to repeat').pend('awaiting refactor of related form for use in fixture');
-    xit('shows event repeat optional ending when event is set to end').pend('awaiting refactor of related form for use in fixture');
-    xit('hides event repeat optional ending when event is set to never end').pend('awaiting refactor of related form for use in fixture');
+  beforeEach(function () {
+    // Michael is concerned that fixture is large and involved and creating too complex a seam
+    loadFixtures('event_datepicker.html');
+    eventDatepicker.init();
+  });
+
+  var set_event_repeats_to_weekly = function(){
+    $('#event_repeats').val('weekly')
+    $('#event_repeats').trigger('change')
+  }
+
+  var set_event_repeat_ends_to_never = function(){
+    $('#event_repeat_ends_string').val('never')
+    $('#event_repeat_ends_string').trigger('change')
+  }
+
+  it('hides event repeat options by default', function () {
+    expect($('#repeats_options')).toBeHidden();
+    expect($('#repeats_weekly_options')).toBeHidden();
+    expect($('.event_option')).toBeHidden();
+  });
+
+  it('shows event repeat options when event is set to repeat', function () {
+    set_event_repeats_to_weekly();
+    expect($('#repeats_options')).not.toBeHidden();
+    expect($('#repeats_weekly_options')).not.toBeHidden();
+    expect($('.event_option')).not.toBeHidden();
+  });
+
+  it('shows event repeat optional ending when event is set to end', function () {
+    set_event_repeats_to_weekly();
+    expect($('#repeat_ends_on_label')).not.toBeHidden();
+    expect($('#repeat_ends_on')).not.toBeHidden();
+  });
+
+  it('hides event repeat optional ending when event is set to never end', function () {
+    set_event_repeats_to_weekly();
+    set_event_repeat_ends_to_never();
+    expect($('#repeat_ends_on_label')).toBeHidden();
+    expect($('#repeat_ends_on')).toBeHidden();
+  });
+
 });
