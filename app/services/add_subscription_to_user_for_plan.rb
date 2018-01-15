d283d828fb4f4c08a82cc7f9990ca25a478cb37e
Sam Joseph <tansaku@gmail.com>
2018-01-15 12:38:54 +0000

    ensure previous subscriptions are ended when new ones start, and that premium mob stripe upgrade creates new subscription rather than overwriting previous one

--- app/services/add_subscription_to_user_for_plan.rb
+++ app/services/add_subscription_to_user_for_plan.rb
@@ -1,10 +1,17 @@
 class AddSubscriptionToUserForPlan
 
   def self.with(user, sponsor, time, plan, payment_source, subscription_klass = Subscription)
-    # TODO must ensure all previous subscriptions are ended
+    ensure_all_previous_subscriptions_are_ended(user, time)
     user.subscriptions << subscription_klass.new(started_at: time, sponsor: sponsor, plan: plan, payment_source: payment_source)
     user.title_list << plan.name
     user.save!
+  end
+
+  private
+
+  def self.ensure_all_previous_subscriptions_are_ended(user, time)
+    unended_subs = user.subscriptions.select { |s| s.ended_at.nil? }
+    unended_subs.each { |s| s.ended_at = time }
   end
 
 end
--- spec/services/add_subscription_to_user_for_plan_spec.rb
+++ spec/services/add_subscription_to_user_for_plan_spec.rb
@@ -12,12 +12,14 @@ describe AddSubscriptionToUserForPlan do
   let(:user) { instance_double(User) }
   let(:sponsor) { instance_double(User) }
   let(:subscription) { instance_double(Subscription) }
+  let(:existing_subscription1) { instance_double(Subscription) }
+  let(:existing_subscription2) { instance_double(Subscription) }
   let(:payment_source) { instance_double(PaymentSource::Stripe) }
 
   before do
     allow(subscription_klass).to receive(:new)
     allow(user).to receive_message_chain(:subscriptions, :<<)
-    allow(user).to receive(:save)
+    allow(user).to receive(:save!)
     allow(user).to receive_message_chain(:title_list, :<<)
   end
 
@@ -38,7 +40,15 @@ describe AddSubscriptionToUserForPlan do
   end
 
   it 'saves the user' do
-    expect(user).to receive(:save)
+    expect(user).to receive(:save!)
+    add_subscription_to_user_for_plan
+  end
+
+  it 'ensures all other existing subscriptions are ended' do
+    allow(existing_subscription1).to receive_message_chain(:ended_at, :nil?).and_return(true)
+    allow(existing_subscription2).to receive_message_chain(:ended_at, :nil?).and_return(false)
+    allow(user).to receive(:subscriptions).and_return [existing_subscription1, existing_subscription2]
+    expect(existing_subscription1).to receive(:ended_at=).with(time)
     add_subscription_to_user_for_plan
   end
 end
