eb896b3089719c5a483de33a694cfd776114021e
Nicolas Sebastian Vidal <nicolas.s.vidal@gmail.com>
2018-01-22 13:10:52 +0100

    1865 - Upgrade pivotal tracker API (#2114)
    * Upgrade 'pivotal-tracker-api' gem.
    
    * No need to mock the stories alone since they come when we request the current iteration.
    
    * No need to mock the labels response.
    
    * Updated mock for current iteration.
    
    * Added mock for project response.
    
    * Spec on project controller was updated since the API has change the way it works.
    
    * Steps wer updated since we only need to mock the project and current iteration reponse.
    
    * The 'pivotal-tracker-api' gem has implemented new methods and functionality to get the stories and the controller was updated accordingly.

--- features/step_definitions/pivotal_steps.rb
+++ features/step_definitions/pivotal_steps.rb
@@ -1,18 +1,16 @@
 Given(/^The project has some stories on Pivotal Tracker$/) do
-  @labels ||= File.read('spec/fixtures/project_pt_labels_response.json')
-  @current ||= File.read('spec/fixtures/project_pt_current_response.json')
-  @stories ||= File.read('spec/fixtures/project_pt_stories_response.json')
+  @current ||= File.read('spec/fixtures/pivotal_tracker_project_current_iteration.json')
+  @project ||= File.read('spec/fixtures/pivotal_tracker_project_response.json')
   stub_request(:get, /www\.pivotaltracker\..+\d+[^\/]*$/).
-    to_return(status: 200, body: @labels, headers: {})
+    to_return(status: 200, body: @project, headers: {})
   stub_request(:get, /www\.pivotaltracker\..+\d+\/iterations[^\/]*$/).
     to_return(:status => 200, :body => @current, :headers => {})
-  stub_request(:get, /www\.pivotaltracker\..+\d+\/stories[^\/]*$/).
-    to_return(:status => 200, body: @stories, :headers =>  {})
 end
 
 Given(/The project has no stories on Pivotal Tracker/) do
-  dummy = Object.new
-  dummy.stub(stories: nil)
-  PivotalService.stub(one_project: nil)
-  PivotalService.stub(iterations: dummy)
+  project = Object.new
+  iteration = Object.new
+  iteration.stub(stories: nil)
+  project.stub(current_iteration: iteration)
+  allow(PivotalAPI::Project).to receive(:retrieve).and_return(project)
 end
