1fa402d2dafedb136b4c9888179ab45adcdddb20
Nick Schimek <cfme00@gmail.com>
2018-04-09 09:15:49 -0700

    Decrease running time of karma_calculator rake task (#2279)
    * Add field to users to hold event participation counts
    
    The google plus hangouts no longer caputures event participation
    information and the algorithm used to calculate this data takes to long
    to run. Since the data is static we do not need to recalculate the data
    and instead can store it in the User object.
    
    * Add error handling to rake task
    
    Error handling added to rake task and use the find_each method to
    process data in batches instead of `all.each`.
    
    Rails docs recommends find_each over each when processing large amounts
    of data.
    http://www.rubydoc.info/docs/rails/3.2.8/ActiveRecord%2FBatches:find_each
    
    * Create rake task to update event participation field
    
    Added the csv file that contains the participation counts from google
    hangouts. Added rake tasks to parse the file and update the user.
    
    Created the rake task to run the csv file
    
    * Remove event participation feature
    
    This feature spec is no longer needed since google API hangouts does not
    provide participant information anymore.
    
    * Update spec to account for event participation
    
    New users have zero event participation and the google hangout api no
    longer captures participant information. Users will always have zero
    unless they attended events when participant data was allowed to be
    captured.
    
    * Update feature spec to reflect current data
    
    Remove participation field as it can no longer be captured.

--- app/services/karma_calculator.rb
+++ app/services/karma_calculator.rb
@@ -1,10 +1,9 @@
 class KarmaCalculator
 
-  attr_reader :user, :events
+  attr_reader :user
 
-  def initialize(user, events)
+  def initialize(user)
     @user = user
-    @events = events
   end
 
   def perform
@@ -22,7 +21,8 @@ class KarmaCalculator
   private
 
   def sum_elements
-    membership_length + profile_completeness + activity + number_hangouts_started_with_more_than_one_participant + number_github_contributions + hangouts_attended_with_more_than_one_participant
+    membership_length + profile_completeness + activity + number_hangouts_started_with_more_than_one_participant +
+    number_github_contributions + event_participation
   end
 
   def membership_length # 6
@@ -43,27 +43,11 @@ class KarmaCalculator
     user.number_hangouts_started_with_more_than_one_participant
   end
 
-  def hangouts_attended_with_more_than_one_participant
-    id = gplus_id
-    return 0 unless id
-    hangouts_attended = calculate_hangouts_attended_with_more_than_one_participant(id)
-    user.hangouts_attended_with_more_than_one_participant = hangouts_attended
-  end
-
-  def gplus_id
-    gplus_auth = user.authentications.select { |a| a.provider == 'gplus' }
-    gplus_auth.try(:first).try(:uid)
-  end
-
-  def calculate_hangouts_attended_with_more_than_one_participant(id)
-    events.select do |i|
-      participant = i.participants.to_unsafe_h.values
-      participant.count > 1 && participant.any? { |p| p['person']['id'] == id }
-    end.count
-  end
-
   def activity # 6
     user.activity
   end
 
+  def event_participation
+    user.event_participation_count
+  end
 end
