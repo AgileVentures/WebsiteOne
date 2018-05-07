aa8019351619c84b635a0d1148b13f52e00903c6
Nick Schimek <cfme00@gmail.com>
2018-05-06 22:03:41 -0700

    Add column modifier_id to events
    Generated migration to add a new column to the events table in order to
    persist the user_id when updating/modifying events.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406015134) do
+ActiveRecord::Schema.define(version: 20180507045056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20180406015134) do
     t.integer "project_id"
     t.integer "creator_id"
     t.string "for"
+    t.integer "modifier_id"
     t.index ["slug"], name: "index_events_on_slug", unique: true
     t.index ["start_datetime"], name: "index_events_on_start_datetime"
   end
