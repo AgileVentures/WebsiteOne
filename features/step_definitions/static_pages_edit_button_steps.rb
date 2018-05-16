479d7bad5c44c04388ff59ebbaa9df1c26ac513b
SDLondon <sjdlondon@gmail.com>
2018-05-16 10:23:12 +0100

    WIP can't seem to find Edit Page link

---
+++ features/step_definitions/static_pages_edit_button_steps.rb
@@ -0,0 +1,26 @@
+def github_edit_url
+	"https://github.com/AgileVentures/AgileVentures/edit/master/#{static_page.tr(' ','_').upcase}.md"
+end
+
+def static_page
+	'Sponsors'
+end
+
+When(/^I click the 'Sponsors' page$/) do
+  click_on('Becoming a sponsor')
+end
+
+Then(/^I should be on 'Sponsors' page$/) do
+  expect(page.title).to have_content('Sponsors')
+end
+
+Then(/^I should see an 'Edit Page' button$/) do
+  expect(page).to have_link 'Edit Page'
+end
+
+Then(/^'Edit Page' should link to github edit page$/) do
+  expect(find_link('Edit Page')[:target]).to eq(github_edit_url)
+end
+
+
+
