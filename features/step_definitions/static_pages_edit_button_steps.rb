ae0dbf70af64faa4b2fc1adb3e369835afa9767f
Sam Joseph <tansaku@gmail.com>
2018-05-16 11:33:13 +0100

    Update static_pages_edit_button_steps.rb

--- features/step_definitions/static_pages_edit_button_steps.rb
+++ features/step_definitions/static_pages_edit_button_steps.rb
@@ -1,9 +1,9 @@
 def github_edit_url
-	"https://github.com/AgileVentures/AgileVentures/edit/master/#{static_page.tr(' ','_').upcase}.md"
+  "https://github.com/AgileVentures/AgileVentures/edit/master/#{static_page.tr(' ','_').upcase}.md"
 end
 
 def static_page
-	'Sponsors'
+  'Sponsors'
 end
 
 When(/^I click the 'Sponsors' page$/) do
@@ -21,6 +21,3 @@ end
 Then(/^'Edit Page' should link to github edit page$/) do
   expect(find_link('Edit Page')[:target]).to eq(github_edit_url)
 end
-
-
-
