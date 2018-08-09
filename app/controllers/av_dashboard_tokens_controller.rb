a25810dc4d594db4d60cc65911bf3e1e8a65f1be
mtc2013 <mtc2013opensource@gmail.com>
2018-08-09 15:07:57 -0400

    Add features for authorized/nonauthorized users and app code

--- app/controllers/av_dashboard_tokens_controller.rb
+++ app/controllers/av_dashboard_tokens_controller.rb
@@ -5,8 +5,9 @@ class AvDashboardTokensController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    authorization_status = current_user.can_see_dashboard ? "true" : "false"
     expiration_timestamp = (DateTime.now + 1.day).strftime("%Q")
-    payload = { authorized: 'true', exp: expiration_timestamp}
+    payload = { authorized: authorization_status, exp: expiration_timestamp}
     @token = JWT.encode payload, Settings.av_dashboard_token_secret, 'HS256'
   end
 end
--- features/step_definitions/av_dashboard_token_steps.rb
+++ features/step_definitions/av_dashboard_token_steps.rb
@@ -8,3 +8,14 @@ Then(/^I should have a valid authorized AVDashboard token on the rendered page$/
   expect(DateTime.strptime(decoded_token.first['exp'], "%Q")).to be >= DateTime.now
   expect(DateTime.strptime(decoded_token.first['exp'], "%Q")).to be <= (DateTime.now + 1.day)
 end
+
+Then(/^I should have a valid unauthorized AVDashboard token on the rendered page$/) do
+  expect(page.body).to have_css("form#av_dashboard")
+  encoded_token = page.find(:xpath, '//form[@id="av_dashboard"]/input[1]', visible: false).value
+  require 'jwt'
+  decoded_token = JWT.decode encoded_token, Settings.av_dashboard_token_secret, 'HS256'
+  expect(decoded_token).to include(hash_including("authorized" => "false"))
+  expect(decoded_token).to include(hash_including("exp"))
+  expect(DateTime.strptime(decoded_token.first['exp'], "%Q")).to be >= DateTime.now
+  expect(DateTime.strptime(decoded_token.first['exp'], "%Q")).to be <= (DateTime.now + 1.day)
+end
