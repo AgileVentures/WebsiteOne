fb953aeec4c4cf449926a2b36aa5f97d87228713
mtc2013 <mtc2013opensource@gmail.com>
2018-08-12 20:54:50 -0400

    2465 jwt token (#2523)
    jwt token

---
+++ app/controllers/av_dashboard_tokens_controller.rb
@@ -0,0 +1,13 @@
+class AvDashboardTokensController < ApplicationController
+  require 'jwt'
+  require 'date'
+
+  before_action :authenticate_user!
+
+  def create
+    authorization_status = current_user.can_see_dashboard ? "true" : "false"
+    expiration_timestamp = (DateTime.now + 1.day).strftime("%Q")
+    payload = { authorized: authorization_status, exp: expiration_timestamp}
+    @token = JWT.encode payload, Settings.av_dashboard_token_secret, 'HS256'
+  end
+end
