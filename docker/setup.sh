8e6921135e68a765e41929764645dcf9644bb97a
mtc2013 <mtc2013opensource@gmail.com>
2018-08-13 21:13:56 -0400

    Better dev part two (#2540)
    * Get rid of the two configs for docker and local; just one
    
    * Make changes to allow seamless editing of source in docker
    
    * Change to use volumes in web app
    
    * Adjust docker files
    
    * Add changes to docker for windows
    
    * Fixing it so that the javascript doesn't leak out onto host; also moving everything inside compose

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,7 +2,8 @@
 
 docker-compose down --rmi all --volumes --remove-orphans
 docker-compose build --force-rm --no-cache
-docker-compose run --rm web rake db:create
-docker-compose run --rm web rake db:migrate RAILS_ENV=development
-docker-compose run --rm web rake db:seed
-docker-compose run --rm web rake db:test:prepare
+DB_HOST=db docker-compose run --rm web rake db:drop
+DB_HOST=db docker-compose run --rm web rake db:create
+DB_HOST=db docker-compose run --rm web rake db:migrate RAILS_ENV=development
+DB_HOST=db docker-compose run --rm web rake db:seed
+DB_HOST=db docker-compose run --rm web rake db:test:prepare
