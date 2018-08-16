073e2c923c32c164f5932fed7e1d82d68bdda4af
Vikas Verma <vikasvr23@gmail.com>
2018-08-16 06:55:33 +0530

    [WIP] add other karma breakdown elements to karma table (#2547)
    add other karma breakdown elements to karma table

---
+++ db/migrate/20180810180605_add_karma_breakdown_elements_to_karma_table.rb
@@ -0,0 +1,9 @@
+class AddKarmaBreakdownElementsToKarmaTable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :karmas, :membership_length, :integer, default: 0
+    add_column :karmas, :profile_completeness, :integer, default: 0
+    add_column :karmas, :number_github_contributions, :integer, default: 0
+    add_column :karmas, :activity, :integer, default: 0
+    add_column :karmas, :event_participation, :integer, default: 0
+  end
+end
