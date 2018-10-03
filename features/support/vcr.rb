831135e48d05108cca829fd3050126e661f32951
mattwr18 <mattwr18@gmail.com>
2018-10-03 12:19:49 -0300

    Automate vcr renewal (#2643)
    * Automate vcr renewal and renew cassettes
    
    * Update tests
    
    * Remove automatic vcr renewal

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -5,10 +5,10 @@ VCR.configure do |c|
   c.cassette_library_dir = 'features/support/fixtures/cassettes'
   c.ignore_localhost = true
   c.default_cassette_options = {
-      :match_requests_on => [
-          :method,
-          VCR.request_matchers.uri_without_param(:imp, :prev_imp, :distinct_id)
-      ]
+    :match_requests_on => [
+        :method,
+        VCR.request_matchers.uri_without_param(:imp, :prev_imp, :distinct_id)
+    ]
   }
   c.filter_sensitive_data('<SLACK_AUTH_TOKEN>') { ENV['SLACK_AUTH_TOKEN'] }
   c.filter_sensitive_data('<GITTER_API_TOKEN>') { ENV['GITTER_API_TOKEN'] }
