5fba4782053d49d52198b80d7335b15b8cf908e7
SDLondon <sjdlondon@gmail.com>
2018-10-15 21:15:51 +0100

    add inspectlet code

---
+++ app/assets/javascripts/inspectlet.js
@@ -0,0 +1,14 @@
+// snippet for inspectlet session recording software
+// https://www.inspectlet.com
+// inspectlet login: wsoinspectlet@gmail.com, password: websiteone
+// gmail account set up as admin: wsoinspectlet@gmail.com, Pass: websiteone
+
+
+(function() {
+	window.__insp = window.__insp || [];
+	__insp.push(['wid', 1674848404]);
+	var ldinsp = function(){
+		if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=1674848404&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+	setTimeout(ldinsp, 0);
+})();
+
