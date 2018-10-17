5f5c3e195f894d87a73b08308cdcdf08eebbf2ae
mattwr18 <mattwr18@gmail.com>
2018-10-17 14:18:42 -0300

    premium mob events should not ping public channels (#2710)
    * fixes #2677
    
    * Edit private event hangout url pings slack appropriately
    
    * sorts the unit tests on slack services
    
    * Unit tests, implement post premium mob notification
    
    * Acceptance, unit tests for editing private event youtube links
    
    * test that we do not post hangout updates during youtube updates and fix that
    
    * Update acceptance tests

--- features/events/private_events.feature
+++ features/events/private_events.feature
@@ -34,6 +34,25 @@ Feature: Private Events
             | Associate    | should not |
             | Free         | should not |
 
+    Scenario: Edit hangout url for private event pings only appropriate private channels
+      Given I have logged in
+      And the date is "2014/02/03 9:00:00 UTC"
+      And that we're spying on the SlackService private and public channels
+      And the Slack notifications are enabled
+      When I manually set a hangout link for event "Mob"
+      Then the Hangout URL is posted only in appropriate private channels in Slack
+
+    Scenario: Edit youtube url for private event pings only appropriate private channels
+      Given I have logged in
+      And the date is "2014/02/03 9:00:00 UTC"
+      And that we're spying on the SlackService private and public channels
+      And the Slack notifications are enabled
+      When I manually set youtube link with youtube id "12341234111" for event "Mob"
+      Then the Youtube URL is posted in select private channels in Slack
+
+# ideally what we'd love the Premium/Associate/Free members to see is a note that
+# event is live and a link to upgrade if they'd like to join
+# and perhaps the option to view the video as part of a free trial?
 
 # ideally what we'd love the Premium/Associate/Free members to see is a note that
 # event is live and a link to upgrade if they'd like to join
--- spec/services/slack_service_spec.rb
+++ spec/services/slack_service_spec.rb
@@ -59,18 +59,32 @@ describe SlackService do
           text: channel_message,
       }.merge!(default_post_args)
     end
+    let(:post_premium_mob_notification_post_args) do 
+      {
+        channel: 'C29J4QQ9M',
+        text: here_message,
+      }.merge!(default_post_args)
+    end
+    let(:post_premium_mob_trialists_notification_post_args) do 
+      {
+        channel: 'C29J4QQ9F',
+        text: here_message,
+      }.merge!(default_post_args)
+    end
 
     let(:cs169_project) { mock_model Project, slug: 'cs169' }
     let(:websiteone_project) { mock_model Project, slug: 'websiteone' }
     let(:noslack_project) { mock_model Project, slug: 'noslack' }
-
+    
     context('PairProgramming') do
 
-      let(:cs169_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: cs169_project }
-      let(:missing_url_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "  ", user: user }
-      let(:websiteone_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: websiteone_project }
-      let(:no_project_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: nil }
-      let(:project_with_no_slack_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: noslack_project }
+      let(:cs169_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: cs169_project, for: '' }
+      let(:missing_url_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "  ", user: user, for: '' }
+      let(:websiteone_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: websiteone_project, for: '' }
+      let(:no_project_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: nil, for: '' }
+      let(:project_with_no_slack_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: noslack_project, for: '' }
+
+     #  if hangout.event.for == 'Premium Mob Members'
 
       it 'sends the correct slack message to the correct channels when associated with a project' do
         expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
@@ -110,7 +124,7 @@ describe SlackService do
       end
 
       let(:multiple_channel_project) { mock_model Project, slug: 'multiple-channels' }
-      let(:multiple_channel_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: multiple_channel_project }
+      let(:multiple_channel_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: multiple_channel_project, for: '' }
 
       it 'can post to multiple channels' do
         expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
@@ -123,10 +137,10 @@ describe SlackService do
     end
 
     context('Scrums') do
-      let(:cs169_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user, project: cs169_project }
-      let(:missing_url_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "  ", user: user }
-      let(:websiteone_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user, project: websiteone_project }
-      let(:no_project_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user, project: nil }
+      let(:cs169_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user, project: cs169_project, for: ''  }
+      let(:missing_url_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "  ", user: user, for: ''  }
+      let(:websiteone_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user, project: websiteone_project, for: ''  }
+      let(:no_project_hangout) { mock_model EventInstance, title: 'MockEvent', category: "Scrum", hangout_url: "mock_url", user: user, project: nil, for: ''  }
 
       it 'sends the correct slack message to the correct channels' do
         expect(slack_client).to receive(:chat_postMessage).with(general_channel_post_args)
@@ -157,69 +171,141 @@ describe SlackService do
 
         slack_service.post_hangout_notification(missing_url_hangout, slack_client, gitter_client)
       end
+    end
 
+    context 'Premium Mob Members Events' do
+      let(:premium_mob_hangout) { mock_model EventInstance, title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user, project: websiteone_project, for: 'Premium Mob Members' }
+      let(:general_channel_id) { 'C0TLAE1MH' }
+      let(:pairing_notifications_channel_id) { 'C29J3DPGW' }
+      let(:websiteone_channel_id) { 'C29J4QQ9W' }
+      
+      it 'posts hangout url to appropriate private channels' do
+        expect(slack_client).to receive(:chat_postMessage).with(post_premium_mob_notification_post_args).once
+        expect(slack_client).to receive(:chat_postMessage).with(post_premium_mob_trialists_notification_post_args).once
+        
+        slack_service.post_hangout_notification(premium_mob_hangout, slack_client)
+      end
+
+      it 'does not post hangout url to public channels' do
+        expect(slack_client).not_to receive(:chat_postMessage).with hash_including(channel: general_channel_id)
+        expect(slack_client).not_to receive(:chat_postMessage).with hash_including(channel: pairing_notifications_channel_id)
+        expect(slack_client).not_to receive(:chat_postMessage).with hash_including(channel: websiteone_channel_id)
+        
+        slack_service.post_hangout_notification(premium_mob_hangout, slack_client)
+      end
     end
   end
-
+  
   describe '.post_yt_link' do
     let(:client) { spy(:slack_client) }
     let(:expected_post_args) do
       {
-          text: 'Video/Livestream: <https://youtu.be/mock_url|click to play>',
-          username: user.display_name,
-          icon_url: user.gravatar_url,
-          link_names: 1
+        text: 'Video/Livestream: <https://youtu.be/mock_url|click to play>',
+        username: user.display_name,
+        icon_url: user.gravatar_url,
+        link_names: 1
       }
     end
     context('PairProgramming') do
-      let(:hangout) { EventInstance.create(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user) }
-
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: "PairProgramming", hangout_url: "mock_url", user: user ) }
+      before { allow(hangout).to receive(:for).and_return '' }
+      
       it 'sends the correct slack message to the correct channels' do
         hangout.yt_video_id = 'mock_url'
         hangout.project = Project.create(title: 'WebSiteOne', description: 'hmmm', status: 'active')
-
+        
         slack_service.post_yt_link(hangout, client)
-
+        
         expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
         expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C29J4QQ9W'))
       end
-
+      
       it 'does not ping slack when the project is cs169' do
         hangout.yt_video_id = 'mock_url'
         hangout.project = Project.create(title: 'cs169', description: 'hmmm', status: 'active')
-
+        
         slack_service.post_yt_link(hangout, client)
-
+        
         expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
         expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C29J4QQ9W'))
       end
-
+      
       it 'does not post youtube video link if yt_video_id is blank' do
         hangout.yt_video_id = '    '
         slack_service.post_yt_link(hangout, client)
-
+        
         expect(client).not_to have_received(:chat_postMessage)
       end
     end
-
+    
     context('Scrums') do
-      let(:hangout) { EventInstance.create(title: 'MockEvent', category: 'Scrum', hangout_url: 'mock_url', user: user) }
-
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: 'Scrum', hangout_url: 'mock_url', user: user ) }
+      before { allow(hangout).to receive(:for).and_return '' }
+      
       it 'sends the correct slack message to the correct channels' do
         hangout.yt_video_id = 'mock_url'
         hangout.project = Project.create(slug: 'websiteone', title: 'WebSiteOne', description: 'hmmm', status: 'active')
-
+        
         slack_service.post_yt_link(hangout, client)
-
+        
         expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C0TLAE1MH'))
         expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: 'C29J4QQ9W'))
       end
-
+      
       it 'does not post youtube video link if yt_video_id is blank' do
         hangout.yt_video_id = '    '
         slack_service.post_yt_link(hangout, client)
-
+        
         expect(client).not_to have_received(:chat_postMessage)
+      end
+    end
+    
+    context 'Premium Mob Members Events' do
+      let(:hangout) { EventInstance.create(title: 'MockEvent', category: 'PairProgramming', hangout_url: 'mock_url', user: user ) }
+      let(:general_channel_id) { 'C0TLAE1MH' }
+      let(:pairing_notifications_channel_id) { 'C29J3DPGW' }
+      let(:websiteone_channel_id) { 'C29J4QQ9W' }
+      let(:premium_extra_channel_id) { 'C29J4QQ9M' }
+      let(:premium_mob_trialists_channel_id) { 'C29J4QQ9F' }
+      before { allow(hangout).to receive(:for).and_return 'Premium Mob Members' }
+
+      let(:default_post_args) do
+        {
+            username: user.display_name,
+            icon_url: user.gravatar_url,
+            link_names: 1
+        }
+      end
+      let(:message) { 'MockEvent: <mock_url|click to join>' }
+      let(:here_message) { "@here #{message}" }
+      let(:post_premium_mob_notification_post_args) do 
+        {
+          channel: 'C29J4QQ9M',
+          text: here_message,
+        }.merge!(default_post_args)
+      end
+      
+      it 'posts youtube url to appropriate private channels' do
+        hangout.yt_video_id = 'mock_url'
+        hangout.project = Project.create(slug: 'websiteone', title: 'WebSiteOne', description: 'hmmm', status: 'active')
+        
+        slack_service.post_yt_link(hangout, client)
+
+        expect(client).to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: premium_extra_channel_id))
+        expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: premium_mob_trialists_channel_id))
+        # expect(client).to have_received(:chat_postMessage).with(post_premium_mob_notification_post_args.merge!(channel: premium_extra_channel_id))
+        expect(client).to have_received(:chat_postMessage).with(hash_including(channel: premium_extra_channel_id)).once
+      end
+
+      it 'does not post youtube url to public channels' do
+        hangout.yt_video_id = 'mock_url'
+        hangout.project = Project.create(slug: 'websiteone', title: 'WebSiteOne', description: 'hmmm', status: 'active')
+        
+        slack_service.post_yt_link(hangout, client)
+
+        expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: general_channel_id))
+        expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: pairing_notifications_channel_id))
+        expect(client).not_to have_received(:chat_postMessage).with(expected_post_args.merge!(channel: websiteone_channel_id))
       end
     end
   end
