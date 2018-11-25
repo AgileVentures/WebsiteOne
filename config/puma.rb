9618195b461f074308898d7603f4eb978faed1ac
Matt Rider <mattwr18@gmail.com>
2018-11-25 17:25:29 -0200

    Configure puma with Rails 5.2

--- config/puma.rb
+++ config/puma.rb
@@ -1,28 +1,29 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-# threads_count = ENV.fetch("MAX_THREADS")
-# threads threads_count, threads_count
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+# workers Integer(ENV['PUMA_WORKERS'] || 3)
 
-preload_app!
+threads_count = ENV.fetch("MAX_THREADS") { 5 }
+threads threads_count, threads_count
+# threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+
+# preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ApplicationRecord.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    ApplicationRecord.establish_connection(config)
-  end
-end
+# on_worker_boot do
+#   # worker specific setup
+#   ActiveSupport.on_load(:active_record) do
+#     config = ApplicationRecord.configurations[Rails.env] ||
+#                 Rails.application.config.database_configuration[Rails.env]
+#     config['pool'] = ENV['MAX_THREADS'] || 16
+#     ApplicationRecord.establish_connection(config)
+#   end
+# end
 
-after_worker_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-  Vanity.playground.establish_connection unless ENV['SEMAPHORE'] == 'true'
-end
+# after_worker_fork do |server, worker|
+#   ActiveRecord::Base.establish_connection
+#   Vanity.playground.establish_connection unless ENV['SEMAPHORE'] == 'true'
+# end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
