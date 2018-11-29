d5a770f8766aa4e7e77223512fa258dd94fd4d34
Daniel Kukula <daniel.kuku@gmail.com>
2018-11-29 18:54:47 +0000

    yt api in progress

--- app/assets/javascripts/hangout_play_on_hover.js
+++ app/assets/javascripts/hangout_play_on_hover.js
@@ -1,11 +1,2 @@
 $(document).ready(function(){
-  var address = "none";
-  $('#hg-container').on("mouseenter", ".card", function(){
-    address = $(this).find('iframe').attr('src');
-    $(this).find('iframe').attr('src',address+'&autoplay=1');
-  })
-  $('#hg-container').on("mouseleave", ".card", function(){
-    address = $(this).find('iframe').attr('src');
-    $(this).find('iframe').attr('src',address.slice(0, -11));
-  });
 });
