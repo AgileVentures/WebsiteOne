18caf602aabdc43ea6b7d79fda29daf0cd6fda0b
Matt Rider <mattwr18@gmail.com>
2018-12-08 12:00:15 -0200

    Add endpoints for proejcts' languages, documents, followers

--- app/api/projects/api.rb
+++ app/api/projects/api.rb
@@ -15,9 +15,18 @@ module Projects
     end
 
     resource :projects do
-      desc 'Return the upcoming events.'
+      desc 'Return projects list.'
       get '/' do
-        Project.all
+        projects_languages_hash = {}
+        projects_followers_count = {}
+        projects_documents_count = {}
+        Project.all.each do |project|
+          projects_languages_hash.merge!("#{project.title}": project.languages)
+          projects_followers_count.merge!("#{project.title}": project.followers.count) 
+          projects_documents_count.merge!("#{project.title}": project.documents.count)
+        end
+        { projects: Project.all.to_json, languages: projects_languages_hash.to_json, 
+          followers: projects_followers_count.to_json, documents: projects_documents_count.to_json }
       end
     end
   end
