eb7c43775595d6bb2c324d0e98802d1775cbdac6
Daniel Kukula <daniel.kuku@gmail.com>
2018-12-10 03:04:30 +0000

    Implement pagination on event_instances api

--- app/api/event_instances/api.rb
+++ app/api/event_instances/api.rb
@@ -1,13 +1,25 @@
 module EventInstances
   class API < Grape::API
+    helpers do
+      params :pagination do
+        optional :page, type: Integer
+        optional :per_page, type: Integer
+        optional :live, type: String
+      end
+    end
     version 'v1', using: :path, vendor: 'agileventures'
     format :json
     prefix :api
 
     resource :event_instances do
       desc 'Return the upcoming events.'
+      params do 
+        use :pagination
+      end
       get '/' do
-        EventInstance.latest.limit(6)
+        relation = (params[:live] == 'true') ? EventInstance.live : EventInstance.latest
+        @event_instances = relation.paginate(page: params[:page], per_page: 6)
+        @event_instances
       end
     end
   end
