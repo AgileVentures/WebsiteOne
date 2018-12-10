c8813c8d997acc2d6eab4523ef3f73d20987d5bc
Daniel Kukula <daniel.kuku@gmail.com>
2018-12-10 03:29:14 +0000

    extracted required fields to variable

--- spec/requests/event_instances.rb
+++ spec/requests/event_instances.rb
@@ -6,6 +6,7 @@ describe "/api/v1" do
     before do
       FactoryBot.create_list(:event_instance, 4)
       FactoryBot.create_list(:event_instance, 3, updated: 1.hour.ago)
+      @required_fields =  [:id, :event_id, :title, :hangout_url, :category, :yt_video_id, :user_id]
     end
 
     context 'json first page' do
@@ -24,8 +25,7 @@ describe "/api/v1" do
         expect(hash_body).to be_an(Array)
         expect(hash_body.length).to eq(6)
         expect(hash_body).to all(be_a(Hash))
-        [:id, :event_id, :title, :hangout_url, :category,
-         :yt_video_id, :user_id].each do |key|
+        @required_fields.each do |key|
           expect(hash_body.first).to have_key(key.to_s)
         end
       end
@@ -47,8 +47,7 @@ describe "/api/v1" do
         expect(hash_body).to be_an(Array)
         expect(hash_body.length).to eq(1)
         expect(hash_body).to all(be_a(Hash))
-        [:id, :event_id, :title, :hangout_url, :category,
-         :yt_video_id, :user_id].each do |key|
+        @required_fields.each do |key|
           expect(hash_body.first).to have_key(key.to_s)
         end
       end
