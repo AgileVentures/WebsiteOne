96518c306f0682f837b9359a9f7681426f253dd6
Matt Rider <mattwr18@gmail.com>
2019-01-07 20:02:48 -0200

    Add users activity stats

--- app/api/users/api.rb
+++ app/api/users/api.rb
@@ -44,7 +44,12 @@ module Users
         users_contributions_hash = {}
         users_contributions_total_hash = {}
         users_videos_hash = {}
-        
+        users_hangouts_hosted_hash = {}
+        users_authentications_hash = {}
+        users_profile_completeness_hash = {}
+        users_membership_length_hash = {}
+        users_activity_hash = {}
+
         User.includes(:karma, :titles).order("karmas.total DESC").limit(500).each do |user|
           users_karma_total_hash.merge!("#{user.id}": user.karma_total)
           users_gravatar_url_hash.merge!("#{user.id}": user.gravatar_url(size: 250))
@@ -55,12 +60,19 @@ module Users
           users_contributions_hash.merge!("#{user.id}": contributions(user))
           users_contributions_total_hash.merge!("#{user.id}": user.commit_count_total)
           users_videos_hash.merge!("#{user.id}": videos(user))
+          users_hangouts_hosted_hash.merge!("#{user.id}": user.number_hangouts_started_with_more_than_one_participant)
+          users_authentications_hash.merge!("#{user.id}": user.authentications.count)
+          users_profile_completeness_hash.merge!("#{user.id}": user.profile_completeness)
+          users_membership_length_hash.merge!("#{user.id}": user.membership_length)
+          users_activity_hash.merge!("#{user.id}": user.activity)
         end
         { 
           users: ordered_users, karma_total: users_karma_total_hash, gravatar_url: users_gravatar_url_hash, 
           users_title: users_titles_hash, users_bio: users_bio_hash, users_skills: users_skill_list_hash, 
           users_projects: users_projects_list_hash, users_contributions: users_contributions_hash, users_videos: users_videos_hash,
-          users_commit_count_total: users_contributions_total_hash
+          users_commit_count_total: users_contributions_total_hash, users_hangouts: users_hangouts_hosted_hash, 
+          users_authentications: users_authentications_hash, users_profile: users_profile_completeness_hash, users_membership_length: users_membership_length_hash,
+          users_activity: users_activity_hash
         }
       end
     end
