558b2a3f8c19ebd17739e16e9172d0fa0a1f25a2
Matt Rider <mattwr18@gmail.com>
2019-01-07 12:33:38 -0200

    Remove contributions from user api

--- app/api/users/api.rb
+++ app/api/users/api.rb
@@ -29,7 +29,6 @@ module Users
         users_bio_hash = {}
         users_skill_list_hash = {}
         users_projects_list_hash = {}
-        users_contributions_hash = {}
         User.includes(:karma, :titles).order("karmas.total DESC").limit(500).each do |user|
           users_karma_total_hash.merge!("#{user.id}": user.karma_total)
           users_gravatar_url_hash.merge!("#{user.id}": user.gravatar_url(size: 250))
@@ -37,12 +36,11 @@ module Users
           users_bio_hash.merge!("#{user.id}": user.bio)
           users_skill_list_hash.merge!("#{user.id}": user.skill_list)
           users_projects_list_hash.merge!("#{user.id}": user.following_projects)
-          users_contributions_hash.merge!("#{user.id}": user.contributions)
         end
         { 
           users: ordered_users, karma_total: users_karma_total_hash, gravatar_url: users_gravatar_url_hash, 
           users_title: users_titles_hash, users_bio: users_bio_hash, users_skills: users_skill_list_hash, 
-          users_projects: users_projects_list_hash, users_contributions: users_contributions_hash
+          users_projects: users_projects_list_hash
         }
       end
     end
