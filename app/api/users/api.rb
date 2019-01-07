54ef35616e2cb078ef0acbd046b093a7cf963c6b
Matt Rider <mattwr18@gmail.com>
2019-01-07 12:55:39 -0200

    Add users contributions

--- app/api/users/api.rb
+++ app/api/users/api.rb
@@ -18,6 +18,12 @@ module Users
         .order("karmas.total DESC")
         .limit(500)
       end
+
+      def contributions(user) 
+        user.commit_counts.select do |commit_count|
+          commit_count.user.following? commit_count.project
+        end
+      end
     end
 
     resource :users do
@@ -29,6 +35,7 @@ module Users
         users_bio_hash = {}
         users_skill_list_hash = {}
         users_projects_list_hash = {}
+        users_contributions_hash = {}
         User.includes(:karma, :titles).order("karmas.total DESC").limit(500).each do |user|
           users_karma_total_hash.merge!("#{user.id}": user.karma_total)
           users_gravatar_url_hash.merge!("#{user.id}": user.gravatar_url(size: 250))
@@ -36,11 +43,12 @@ module Users
           users_bio_hash.merge!("#{user.id}": user.bio)
           users_skill_list_hash.merge!("#{user.id}": user.skill_list)
           users_projects_list_hash.merge!("#{user.id}": user.following_projects)
+          users_contributions_hash.merge!("#{user.id}": contributions(user))
         end
         { 
           users: ordered_users, karma_total: users_karma_total_hash, gravatar_url: users_gravatar_url_hash, 
           users_title: users_titles_hash, users_bio: users_bio_hash, users_skills: users_skill_list_hash, 
-          users_projects: users_projects_list_hash
+          users_projects: users_projects_list_hash, users_contributions: users_contributions_hash
         }
       end
     end
