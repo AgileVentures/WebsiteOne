2202484db43fa4443b5410dc496ba4818c7db669
Matt Rider <mattwr18@gmail.com>
2019-01-16 02:44:29 -0200

    Update step definitions, helper method

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,6 +1,6 @@
 module SubscriptionsHelper
   def paypal_return_url
-    return "#{root_url.chomp('/')}#{subscriptions_paypal_redirect_path}"
+    return "#{root_url.chomp('/')}#{subscriptions_paypal_redirect_path}?item_number=#{current_user.slug}&item_name=#{@plan.name}&auth=#{cookies['_WebsiteOne_session']}"
   end
 
   def action_text
