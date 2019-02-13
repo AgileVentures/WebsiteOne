145144d5ec46d3c760882b26df8174e730b4818c
Matt Rider <mattwr18@gmail.com>
2019-02-13 17:14:06 -0200

    Modify users api endpoint
    - provide /users/:id path as well

--- app/api/users/api.rb
+++ app/api/users/api.rb
@@ -5,16 +5,8 @@ module Users
     prefix :api
 
     helpers do
-      def current_user
-        @current_user ||= User.authorize!(env)
-      end
-
-      def authenticate!
-        error!('401 Unauthorized', 401) unless current_user
-      end
-
       def ordered_users 
-        User.where.not(id: -1).includes(:karma, :titles)
+        User.where.not(id: -1).includes(:karma)
         .order("karmas.total DESC")
         .limit(100)
       end
@@ -37,43 +29,41 @@ module Users
       get '/' do
         users_karma_total_hash = {}
         users_gravatar_url_hash = {}
-        users_titles_hash = {}
-        users_bio_hash = {}
-        users_skill_list_hash = {}
-        users_projects_list_hash = {}
-        users_contributions_hash = {}
-        users_contributions_total_hash = {}
-        users_videos_hash = {}
-        users_hangouts_hosted_hash = {}
-        users_authentications_hash = {}
-        users_profile_completeness_hash = {}
-        users_membership_length_hash = {}
-        users_activity_hash = {}
 
-        User.includes(:karma, :titles).order("karmas.total DESC").limit(100).each do |user|
+        User.includes(:karma).order("karmas.total DESC").limit(100).each do |user|
           users_karma_total_hash.merge!("#{user.id}": user.karma_total)
           users_gravatar_url_hash.merge!("#{user.id}": user.gravatar_url(size: 250))
-          users_titles_hash.merge!("#{user.id}": user.titles.pluck(:name))
-          users_bio_hash.merge!("#{user.id}": user.bio)
-          users_skill_list_hash.merge!("#{user.id}": user.skill_list)
-          users_projects_list_hash.merge!("#{user.id}": user.following_projects)
-          users_contributions_hash.merge!("#{user.id}": contributions(user))
-          users_contributions_total_hash.merge!("#{user.id}": user.commit_count_total)
-          users_videos_hash.merge!("#{user.id}": videos(user))
-          users_hangouts_hosted_hash.merge!("#{user.id}": user.number_hangouts_started_with_more_than_one_participant)
-          users_authentications_hash.merge!("#{user.id}": user.authentications.count)
-          users_profile_completeness_hash.merge!("#{user.id}": user.profile_completeness)
-          users_membership_length_hash.merge!("#{user.id}": user.membership_length)
-          users_activity_hash.merge!("#{user.id}": user.activity)
         end
         { 
-          users: ordered_users, karma_total: users_karma_total_hash, gravatar_url: users_gravatar_url_hash, 
-          users_title: users_titles_hash, users_bio: users_bio_hash, users_skills: users_skill_list_hash, 
-          users_projects: users_projects_list_hash, users_contributions: users_contributions_hash, users_videos: users_videos_hash,
-          users_commit_count_total: users_contributions_total_hash, users_hangouts: users_hangouts_hosted_hash, 
-          users_authentications: users_authentications_hash, users_profile: users_profile_completeness_hash, users_membership_length: users_membership_length_hash,
-          users_activity: users_activity_hash
+          users: ordered_users, 
+          karma_total: users_karma_total_hash, 
+          gravatar_url: users_gravatar_url_hash,
         }
+      end
+  
+      desc 'Return a user show page info'
+      params do
+        requires :id, type: Integer, desc: 'User id.'
+      end
+      route_param :id do
+        get do
+          user = User.find(params[:id])
+          {
+            user: user,
+            karmaTotal: user.karma_total, 
+            gravatarUrl: user.gravatar_url(size: 250),
+            bio: user.bio, 
+            projects: user.following_projects, 
+            contributions: contributions(user), 
+            commitCountTotal: user.commit_count_total, 
+            videos: videos(user),
+            hangouts: user.number_hangouts_started_with_more_than_one_participant, 
+            authentications: user.authentications.count, 
+            profile: user.profile_completeness, 
+            membershipLength: user.membership_length,
+            activity: user.activity
+          }
+        end
       end
     end
   end
