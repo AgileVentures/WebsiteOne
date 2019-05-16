d1d7ae94bfae25dce6619dee1a7086089771ea96
Piotr Brych <pbrych@gmail.com>
2019-05-16 20:17:04 +0200

    Adjust passwords endpoint

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,4 +2,13 @@ class PasswordsController < Devise::PasswordsController
   respond_to :json
 
 
+  private
+
+  def respond_with(resource, _opts = {})
+    respond_to do |format|
+      puts resource, _opts
+      format.html { render :new }
+      format.json { render json: resource }
+    end
+  end
 end
