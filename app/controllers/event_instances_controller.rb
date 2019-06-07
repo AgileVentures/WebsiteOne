c09202a7fd679c43b3234cd9fe835b46b3fe7820
Nick Schimek <cfme00@gmail.com>
2019-06-07 10:04:56 -0700

    Remove cors pre flight check
    We're no longer receiving telemetry from google.

--- app/controllers/event_instances_controller.rb
+++ app/controllers/event_instances_controller.rb
@@ -1,6 +1,5 @@
 class EventInstancesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :cors_preflight_check, except: [:index, :edit, :update_link]
   before_action :authenticate_user!, only: [:edit, :update_link, :update]
 
   def update
@@ -84,28 +83,10 @@ class EventInstancesController < ApplicationController
     params.require(:event_instance).permit(:yt_video_id, :hoa_status)
   end
 
-  def cors_preflight_check
-    head :bad_request and return unless (allowed? || local_request?)
-
-    set_cors_headers
-    head :ok and return if request.method == 'OPTIONS'
-  end
-
-  def allowed?
-    allowed_sites = %w(a-hangout-opensocial.googleusercontent.com)
-    origin = request.env['HTTP_ORIGIN']
-    allowed_sites.any? { |url| origin =~ /#{url}/ }
-  end
-
   def local_request?
     request.env['HTTP_ORIGIN'].include?(request.env['HTTP_HOST'])
   rescue
     true
-  end
-
-  def set_cors_headers
-    response.headers['Access-Control-Allow-Origin'] = request.env['HTTP_ORIGIN']
-    response.headers['Access-Control-Allow-Methods'] = 'PUT'
   end
 
   def check_and_transform_params(event_instance)
