1dde9257d991d485cf555ea5eb42d8e087fdcf56
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-06-09 12:33:52 +0100

    remove destroy action project controller

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Rails.application.routes.draw do
   resources :event_instances, :only => [:edit]
   patch '/event_instances/:id', to: 'event_instances#update_link'
 
-  resources :projects, :format => false do
+  resources :projects, except: [:destroy], :format => false do
     member do
       put :mercury_update
       get :mercury_saved
