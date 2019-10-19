a3f6467ad38a0a3132f3b61cecbdee091b87f517
abdellani <abdellani@gmail.com>
2019-10-19 17:39:18 +0100

    wrap new code inside try/catch block

--- app/assets/javascripts/events/edit_event_form.js
+++ app/assets/javascripts/events/edit_event_form.js
@@ -19,23 +19,25 @@ var editEventForm = {
         start_time.val(local_next_date_time.format("hh:mm A"));
 
         var daysOfWeek = document.querySelectorAll('#daysOfWeek>label>input')
-        if (daysOfWeek.length === 0) {
-            return
-        }
-        var arrayOfdays = []
-        daysOfWeek.forEach(function (checkBox) { arrayOfdays.push(checkBox.checked) })
-        if (normalized_start_date_time.toDate().getUTCDate() !== normalized_start_date_time.toDate().getDate()) {
-            var tmp;
-            if (normalized_start_date_time._offset > 0) {
-                tmp = arrayOfdays.pop()
-                arrayOfdays.unshift(tmp)
-            } else {
-                tmp = arrayOfdays.shift()
-                arrayOfdays.push(tmp)
+        try {
 
+            var arrayOfdays = []
+            daysOfWeek.forEach(function (checkBox) { arrayOfdays.push(checkBox.checked) })
+            if (normalized_start_date_time.toDate().getUTCDate() !== normalized_start_date_time.toDate().getDate()) {
+                var tmp;
+                if (normalized_start_date_time._offset > 0) {
+                    tmp = arrayOfdays.pop()
+                    arrayOfdays.unshift(tmp)
+                } else {
+                    tmp = arrayOfdays.shift()
+                    arrayOfdays.push(tmp)
+
+                }
             }
+            daysOfWeek.forEach(function (checkBox, index) { checkBox.checked = arrayOfdays[index] })
+        } catch (error) {
+            console.log(error)
         }
-        daysOfWeek.forEach(function (checkBox, index) { checkBox.checked = arrayOfdays[index] })
     }
 };
 
