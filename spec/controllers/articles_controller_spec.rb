0cb3aa159d4ea1bdbc2e5a6f5e218771b9369b55
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-05-23 10:50:53 +0900

    Finish removing deprecated stub syntax
    Removed deprecated stub syntax which was causing failures on the CI.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe ArticlesController do
                  content: 'Content',
                  tag_list: 'Ruby on Rails' } }
   end
+  let(:articles) { instance_double(ActiveRecord::Relation) }
 
   describe 'GET index' do
-    let(:articles) { instance_double(ActiveRecord::Relation) }
-
     before { allow(articles).to receive_messages(order: articles, includes: articles) }
 
     context 'when tag is provided' do
@@ -33,32 +32,34 @@ RSpec.describe ArticlesController do
   end
 
   describe 'GET show' do
+    let(:article) { double('Article', friendly_id: 'friend', user: author) }
+    let(:author)  { double('User') }
+
     before(:each) do
-      @author = double('User')
-      @article = double('Article', friendly_id: 'friend', user: @author)
-      Article.stub_chain('friendly.find').and_return(@article)
+      allow(Article).to receive(:friendly).and_return articles
+      allow(articles).to receive(:find).and_return article
     end
 
     it 'is expected to render the show template' do
-      get :show, params: { id: @article.friendly_id }
+      get :show, params: { id: article.friendly_id }
       expect(response).to render_template :show
     end
 
     it 'is expected to search the database using the friendly id' do
-      dummy = Object.new
-      expect(Article).to receive(:friendly).and_return(dummy)
-      expect(dummy).to receive(:find).with(@article.friendly_id).and_return(@article)
-      get :show, params: { id: @article.friendly_id }
+      get :show, params: { id: article.friendly_id }
+      expect(Article).to have_received(:friendly)
+      expect(articles).to have_received(:find).with article.friendly_id
+      expect(article).to have_received(:user)
     end
 
-    it 'is expected to assign the requested article to @article' do
-      get :show, params: { id: @article.friendly_id }
-      expect(assigns(:article)).to eq @article
+    it 'is expected to assign the requested article to article' do
+      get :show, params: { id: article.friendly_id }
+      expect(assigns(:article)).to eq article
     end
 
-    it 'is expected to assign the requested article author to @author' do
-      get :show, params: { id: @article.friendly_id }
-      expect(assigns(:author)).to eq @author
+    it 'is expected to assign the requested article author to author' do
+      get :show, params: { id: article.friendly_id }
+      expect(assigns(:author)).to eq author
     end
   end
 
@@ -91,38 +92,42 @@ RSpec.describe ArticlesController do
   end
 
   describe 'GET edit' do
+    let(:article) { double('Article', friendly_id: 'friend') }
+
     before(:each) do
       controller.stub(:authenticate_user!).and_return(true)
-      @article = double('Article', friendly_id: 'friend')
-      Article.stub_chain('friendly.find').and_return(@article)
+      allow(Article).to receive(:friendly).and_return articles
+      allow(articles).to receive(:find).and_return article
     end
 
     it 'is expected to require authentication' do
       expect(controller).to receive(:authenticate_user!)
-      get :edit, params: { id: @article.friendly_id }
+      get :edit, params: { id: article.friendly_id }
     end
 
     it 'is expected to search for the article using friendly ids' do
-      dummy = Object.new
-      expect(Article).to receive(:friendly).and_return(dummy)
-      expect(dummy).to receive(:find).with(@article.friendly_id)
-      get :edit, params: { id: @article.friendly_id }
+      get :edit, params: { id: article.friendly_id }
+      expect(Article).to have_received(:friendly)
+      expect(articles).to have_received(:find).with article.friendly_id
     end
 
-    it 'is expected to assign the article to be edited to @article' do
-      get :edit, params: { id: @article.friendly_id }
-      expect(assigns(:article)).to eq @article
+    it 'is expected to assign the article to be edited to article' do
+      get :edit, params: { id: article.friendly_id }
+      expect(assigns(:article)).to eq article
     end
   end
 
   describe 'POST create' do
+    let(:user)    { double('User', id: 1) }
+    let(:article) { build(:article, title: 'my title', slug: 'friend') }
+    let(:save)    { true }
+
     before(:each) do
       controller.stub(:authenticate_user!).and_return(true)
-      @article = build(:article, title: 'my title', slug: 'friend')
-      @user = double('User', id: 1)
-      allow(controller).to receive(:current_user).and_return(@user)
-      allow(@article).to receive(:create_activity)
-      @user.stub_chain('articles.build').and_return(@article)
+      allow(controller).to receive(:current_user).and_return(user)
+      allow(article).to receive(:create_activity)
+      allow(user).to receive_message_chain(:articles, :build).and_return article
+      allow(article).to receive(:save).and_return save
     end
 
     it 'is expected to require authentication' do
@@ -130,33 +135,36 @@ RSpec.describe ArticlesController do
       post :create, params: valid_params
     end
 
-    it 'is expected to create the article through the current user' do
-      expect(controller).to receive(:current_user)
-      dummy = Object.new
-      expect(@user).to receive(:articles).and_return(dummy)
-      expect(dummy).to receive(:build).and_return(@article)
-      post :create, params: valid_params
+    context 'when successful' do
+      before { allow(article).to receive(:create_activity) }
+
+      it 'creates activity' do
+        post :create, params: valid_params
+        expect(article).to have_received(:create_activity).with(:create, { owner: user })
+      end
+
+      it 'is responds with success message', :aggregate_failures do
+        post :create, params: valid_params
+        expect(flash[:notice]).to match /^Successfully created the article/
+        expect(response).to redirect_to article_path(article)
+      end
     end
 
-    it 'is expected to be successful for requests with valid params' do
-      post :create, params: valid_params
-      expect(flash[:notice]).to match /^Successfully created the article/
-      # Bryan: assumes title is equal to the friendly id
-      expect(response).to redirect_to article_path(@article)
-    end
+    context 'when unsuccessful' do
+      let(:save)          { false }
+      let(:error_message) { 'invalid form entry' }
 
-    it 'is expected to receive :create_activity with :create' do
-      post :create, params: valid_params
-      expect(@article).to have_received(:create_activity).with(:create, { owner: @user })
-    end
+      before do
+        allow(article).to(
+          receive_message_chain(:errors, :full_messages, :join).and_return error_message
+        )
+      end
 
-    it 'is expected to render the new template with error messages if unsuccessful' do
-      expect(@article).to receive(:save).and_return(false)
-      error_message = 'error!'
-      @article.stub_chain('errors.full_messages.join').and_return(error_message)
-      post :create, params: valid_params
-      expect(flash.now[:alert]).to eq error_message
-      expect(response).to render_template :new
+      it 'responds with error message' do
+        post :create, params: valid_params
+        expect(flash.now[:alert]).to eq error_message
+        expect(response).to render_template :new
+      end
     end
   end
 
