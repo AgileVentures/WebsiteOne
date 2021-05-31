073f0c5f27f0e064428e96313b376af21fd1c483
Thomas <thomas@craftacademy.se>
2021-05-31 21:30:43 +0200

    regenerates casettes and fixtures adds condition to cucumber initializer

--- config/initializers/cucumber.rb
+++ config/initializers/cucumber.rb
@@ -3,10 +3,12 @@
 # Based on https://github.com/cucumber/cucumber-ruby/issues/1432
 # HACK: this method was available in cucumber 3.1 and VCR relies on it to
 # generate cassette names.
-Cucumber::Core::Test::Case.class_eval do
-  def feature
-    string = File.read(location.file)
-    document = ::Gherkin::Parser.new.parse(string)
-    document[:feature]
+if Rails.env.test?
+  Cucumber::Core::Test::Case.class_eval do
+    def feature
+      string = File.read(location.file)
+      document = ::Gherkin::Parser.new.parse(string)
+      document[:feature]
+    end
   end
 end
