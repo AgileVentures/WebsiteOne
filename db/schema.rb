92ac33a821a00ff1d20be5687147f606cbc1d6c8
Thomas <thomas@craftacademy.se>
2021-07-02 19:30:28 +0200

    adds migration to remove vanity tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_143519) do
+ActiveRecord::Schema.define(version: 2021_07_02_172212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
