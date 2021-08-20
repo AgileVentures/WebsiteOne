93003d19efa2e1bc36518c6ddd32be20f6938372
MAC <user@MACs-MacBook-Pro.local>
2021-08-20 11:01:38 +0100

    create emails to send to creator on project creation and project approval

--- spec/mailers/project_mailer_spec.rb
+++ spec/mailers/project_mailer_spec.rb
@@ -20,6 +20,63 @@ RSpec.describe ProjectMailer, type: :mailer do
     end
   end
 
+
+  describe '#project_creator_alert' do
+    context 'websiteone project creator recieves email notifying of creation and pending approval' do
+      before(:each) do
+        @project_creator = FactoryBot.create(:user, first_name: 'Sam', last_name: 'Satterfield', email: 'sam@sam.com')
+        @project = FactoryBot.create(:project, title: 'WebsiteOne', user: @project_creator)
+        @email = ProjectMailer.with(project: @project, project_creator: @project_creator).alert_project_creator_about_new_project_created.deliver_now
+      end
+      it 'queues mailer for delivery' do
+        assert !ActionMailer::Base.deliveries.empty?
+      end
+
+      it 'sends an email to the new project creator' do
+        assert_equal [@project_creator.email], @email.to 
+      end
+
+      it 'displays the project title in the subject' do
+        assert_equal 'WebsiteOne project created pending approval.', @email.subject
+      end
+
+      it 'sends an email with a text part' do
+        #assert_equal read_fixture('project_creation_notification_text').join, @email.text_part.body.to_s
+      end
+
+      it 'sends an email with an html part' do
+        #assert_equal read_fixture('project_creation_notification_html').join, @email.html_part.body.to_s
+      end
+    end
+
+    context 'websiteone project creator recieves email notifying of project approval' do
+      before(:each) do
+        @project_creator = FactoryBot.create(:user, first_name: 'Sam', last_name: 'Satterfield')
+        @project = FactoryBot.create(:project, title: 'WebsiteOne', user: @project_creator)
+        @email = ProjectMailer.with(project: @project, project_creator: @project_creator).alert_project_creator_about_project_approval.deliver_now
+      end
+      it 'queues mailer for delivery' do
+        assert !ActionMailer::Base.deliveries.empty?
+      end
+
+      it 'sends an email to the new project creator' do
+        assert_equal [@project_creator.email], @email.to 
+      end
+
+      it 'displays the project title in the subject' do
+        assert_equal 'WebsiteOne project approved.', @email.subject
+      end
+
+      it 'sends an email with a text part' do
+       # assert_equal read_fixture('project_creator_approval_text').join, @email.text_part.body.to_s
+      end
+
+      it 'sends an email with an html part' do
+        #assert_equal read_fixture('project_creator_approval_html').join, @email.html_part.body.to_s
+      end
+    end
+  end
+
   describe '#welcome_project_joinee' do
     context 'websiteone project' do
       before(:each) do
