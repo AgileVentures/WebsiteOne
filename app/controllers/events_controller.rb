a37bb33eb6b651b2b523cdadc62dd3bf75dae224
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-01-28 10:49:15 -0500

    fix meet buttons

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,7 @@ class EventsController < ApplicationController
     @recent_hangout = @event.recent_hangouts.first
     @event_instances = EventInstance.where(event_id: @event.id).where.not(yt_video_id: nil)
                                     .order(created_at: :desc).limit(5)
+    @project = @event.project
     render partial: 'hangouts_management' if request.xhr?
   end
 
