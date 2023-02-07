1afe391583658ed7bb70063205056c87c9b4eeae
Florence-Njeri <flonsie17@gmail.com>
2023-02-07 12:07:43 +0300

    revert app:update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,10 @@
-# Be sure to restart your server when you modify this file.
+# frozen_string_literal: true
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
-
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.configure do
+    # ensure svg assets are compiled in production
+    config.assets.precompile += %w(jquery-1.7.js subscriptions.css lolex.js jasmine-jquery.js .svg
+                                   hangout_play_on_hover.js)
+    config.assets.paths << Rails.root.join('node_modules')
+    # Version of your assets, change this if you want to expire all your assets.
+    config.assets.version = '1.0'
+  end
