2fe1ac4e39491f7bfd5e7d66404a3ec031d6edb9
Florence-Njeri <flonsie17@gmail.com>
2023-02-15 22:56:34 +0300

    revert Ruby upgrade

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 Bundler.require(:default, Rails.env)
 
 module WebsiteOne
