649dd59d35dbf2d3898937d309e912b654d244ba
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-03-02 13:50:15 -0500

    Add courses model

---
+++ app/models/course.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Course < ApplicationRecord
+  extend FriendlyId
+  include Filterable
+  friendly_id :title, use: %i(slugged history)
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { :minimum => 5 }
+  validates :status, presence: true
+
+  belongs_to :user, optional: true
+  include UserNullable
+
+  # acts_as_followable
+
+  scope :active, -> { where('status ILIKE ?', 'active').order(:title) }
+
+  def slack_channel
+    "https://agileventures.slack.com/app_redirect?channel=#{slack_channel_name}"
+  end
+
+end
