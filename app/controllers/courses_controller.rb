30486c8cc13737575fe699322cdb7e47cf1004b7
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-03-03 07:27:29 -0500

    rubocop fixes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CoursesController < ApplicationController
   def create
-    Course.create(course_params)
+    @course = Course.create(course_params)
   end
 
   private
 
   def course_params
-    params.require(:user).permit(:title, :description, :status, :user_id, :slack_channel_name)
+    params.require(:course).permit(:title, :description, :status, :user_id, :slack_channel_name)
   end
 end
