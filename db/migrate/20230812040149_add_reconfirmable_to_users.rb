965546f9525b530eecfe157edde9d369babbfc30
wordhouse66 <wordhouse66@yahoo.com>
2023-08-21 14:22:52 +0100

    delete migration files for reconfirmable

--- db/migrate/20230812040149_add_reconfirmable_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddReconfirmableToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :reconfirmable, :string
-  end
-end
--- db/migrate/20230812061445_remove_reconfirmable_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReconfirmableFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :reconfirmable, :string
-  end
-end
