dcdb22884aec527a9deda12bdce10776c38f3b16
mac <mac@macs-MacBook-Pro.local>
2023-12-07 11:01:46 +0100

    added frozen string literal to cookie controller and helper

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class CookiesController < ApplicationController
   def index
     session[:cookies_accepted] = params[:cookies] if params[:cookies]
-    #session[:cookies_accepted] = nil
   end
 end
