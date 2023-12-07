405efaa82da1abfb1f3d245c344ad3aac6df0283
mac <mac@macs-MacBook-Pro.local>
2023-12-07 05:29:54 +0100

    implemented cookies banner

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,5 +1,6 @@
 class CookiesController < ApplicationController
   def index
     session[:cookies_accepted] = params[:cookies] if params[:cookies]
+    #session[:cookies_accepted] = nil
   end
 end
